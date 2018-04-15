66687dc5cb80dd0893f7d5c9fb9afb85e20c849e
Julian9527 <lucien777@show.one>
2018-04-15 22:10:03 +0800

    增加消息类型

---
+++ ShowLive/Class/Home/model/SLMessageInfo.m
@@ -0,0 +1,13 @@
+//
+//  SLMessageInfo.m
+//  ShowLive
+//
+//  Created by zhangxinggong on 2018/4/13.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "SLMessageInfo.h"
+
+@implementation SLMessageInfo
+
+@end
