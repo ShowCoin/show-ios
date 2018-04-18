5509de5663e3ccff8dc22b35ebb562427b78a1c0
Julian9527 <lucien777@show.one>
2018-04-18 21:01:31 +0800

    聊天室相关

---
+++ ShowLive/Class/Home/SLChatRoomView/ChatRoomMessage.h
@@ -0,0 +1,22 @@
+//
+//  BKChatRoomMessage.h
+//  show-ios
+//
+//  Created by mac on 18/4/15.
+//  Copyright © 2019年 SouYu. All rights reserved.
+//
+
+#import <RongIMLib/RongIMLib.h>
+/*!
+ 文本消息的类型名
+ */
+#define ChatRoomMessageTypeIdentifier @"chatRoomMsg"
+@interface ChatRoomMessage : RCMessageContent
+/*!
+ 文本消息的内容
+ */
+@property(nonatomic, strong) NSString *content;
+
++ (instancetype)messageWithContent:(NSString *)content;
+
+@end
---
+++ ShowLive/Class/Home/SLChatRoomView/ChatRoomMessage.m
@@ -0,0 +1,88 @@
+//
+//  BKChatRoomMessage.m
+//  showLive
+//
+//  Created by mac on 16/5/30.
+//  Copyright © 2016年 SouYu. All rights reserved.
+//
+
+#import "ChatRoomMessage.h"
+
+@implementation ChatRoomMessage
+///初始化
++ (instancetype)messageWithContent:(NSString *)content {
+    ChatRoomMessage *text = [[ChatRoomMessage alloc] init];
+    if (text) {
+        text.content = content;
+    }
+    return text;
+}
+
+///消息是否存储，是否计入未读数
++ (RCMessagePersistent)persistentFlag {
+    return (MessagePersistent_ISPERSISTED | MessagePersistent_ISCOUNTED);
+}
+
+/// NSCoding
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super init];
+    if (self) {
+        self.content = [aDecoder decodeObjectForKey:@"content"];
+    }
+    return self;
+}
+
+/// NSCoding
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    [aCoder encodeObject:self.content forKey:@"content"];
+}
+
+///将消息内容编码成json
+- (NSData *)encode {
+    NSMutableDictionary *dataDict = [NSMutableDictionary dictionary];
+    [dataDict setObject:self.content forKey:@"content"];
+    
+    
+    if (self.senderUserInfo) {
+        NSMutableDictionary *userInfoDic = [[NSMutableDictionary alloc] init];
+        if (self.senderUserInfo.name) {
+            [userInfoDic setObject:self.senderUserInfo.name forKeyedSubscript:@"name"];
+        }
+        if (self.senderUserInfo.portraitUri) {
+            [userInfoDic setObject:self.senderUserInfo.portraitUri forKeyedSubscript:@"icon"];
+        }
+        if (self.senderUserInfo.userId) {
+            [userInfoDic setObject:self.senderUserInfo.userId forKeyedSubscript:@"id"];
+        }
+        [dataDict setObject:userInfoDic forKey:@"user"];
+    }
+    NSData *data = [NSJSONSerialization dataWithJSONObject:dataDict options:kNilOptions error:nil];
+    return data;
+}
+///将json解码生成消息内容
+- (void)decodeWithData:(NSData *)data {
+    if (data) {
+        __autoreleasing NSError *error = nil;
+        
+        NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+        
+        if (dictionary) {
+            self.content = dictionary[@"content"];
+            
+            NSDictionary *userinfoDic = dictionary[@"user"];
+            [self decodeUserInfo:userinfoDic];
+        }
+    }
+}
+
+/// 会话列表中显示的摘要
+- (NSString *)conversationDigest {
+    return self.content;
+}
+
+///消息的类型名
++ (NSString *)getObjectName {
+    return ChatRoomMessageTypeIdentifier;
+}
+
+@end
