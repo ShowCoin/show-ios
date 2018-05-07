b0beb76f3eb99495876be3328955fadc6efc41e3
“Vninggit config --global user.email “790068808@qq.com <790068808@qq.comgit config --global user.name Vning>
2018-05-08 01:38:33 +0800

    榜单接口 计算逻辑

---
+++ requestAction/SLUrgeListAction.m
@@ -0,0 +1,15 @@
+//
+//  SLUrgeListAction.m
+//  ShowLive
+//
+//  Created by vning on 2018/4/22.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "SLUrgeListAction.h"
+
+@implementation SLUrgeListAction
+- (NSString *)interface{
+    return  @"rank/urgeList";
+}
+@end
