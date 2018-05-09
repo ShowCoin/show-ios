f56a14e3e70270a8f110461fe18f68de894355b4
chouYagami <zhouhua@show.one>
2018-05-10 00:38:08 +0800

    Add login function, support WeChat, mobile phone, email, microblog...

---
+++ requestAction/ShowReqAction.m
@@ -0,0 +1,18 @@
+//
+//  ShowReqAction.m
+//  ShowLive
+//
+//  Created by iori_chou on 2018/3/29.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "ShowReqAction.h"
+
+@implementation ShowReqAction
+
+
+-(NSString *)interface{
+    return @"reg";
+}
+
+@end
