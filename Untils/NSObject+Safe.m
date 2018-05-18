f046c5beef36c9c520d6bc4d027cea87c2a93383
JokeSmileZhang <lucien777@show.one>
2018-05-18 23:51:36 +0800

    安全模式

--- Untils/NSObject+Safe.m
+++ Untils/NSObject+Safe.m
@@ -10,28 +10,28 @@
 
 @implementation NSObject (Safe)
 
-
+//安全的字典样式
 - (instancetype)safeDictionary {
     if (![self isKindOfClass:[NSDictionary class]]) {
         return nil;
     }
     return self;
 }
-
+//安全的数组样式
 - (instancetype)safeArray {
     if (![self isKindOfClass:[NSArray class]]) {
         return nil;
     }
     return self;
 }
-
+//安全的字符串样式
 - (instancetype)safeString {
     if (![self isKindOfClass:[NSString class]]) {
         return nil;
     }
     return self;
 }
-
+//安全的Number样式
 - (instancetype)safeNumber {
     if (![self isKindOfClass:[NSNumber class]]) {
         return nil;
