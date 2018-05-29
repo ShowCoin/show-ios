4c381c4cb6fac0682e68d06a7736d1def64daa44
Vning <790068808@qq.com>
2018-05-30 03:14:02 +0800

    网络诊断

---
+++ Classes/UserCenter/SLNetDiagnoViewController.m
@@ -0,0 +1,136 @@
+//
+//  SLNetDiagnoViewController.m
+//  ShowLive
+//
+//  Created by iori_chou on 2018/4/21.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "SLNetDiagnoViewController.h"
+#import "LDNetDiagnoService.h"
+
+@interface SLNetDiagnoViewController () <LDNetDiagnoServiceDelegate, UITextFieldDelegate> {
+    
+}
+@property (nonatomic,strong)UITextView *txtView_log;
+@property (nonatomic,strong)UIActivityIndicatorView *indicatorView;
+@property (nonatomic,copy)NSString *logInfo;
+@property (nonatomic,strong)LDNetDiagnoService *netDiagnoService;
+@property (nonatomic,assign)BOOL isRunning;
+
+
+@end
+
+@implementation SLNetDiagnoViewController
+- (void)dealloc
+{
+    
+}
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:YES];
+    _isRunning = !_isRunning;
+    [_netDiagnoService stopNetDialogsis];
+}
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:YES];
+    _txtView_log.text = @"";
+    _logInfo = @"";
+    _isRunning = !_isRunning;
+    [_netDiagnoService startNetDiagnosis];
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.navigationBarView setNavigationLeftBarStyle:NavigationBarLeftDefault];
+    [self.navigationBarView setNavigationTitle:@"网络诊断"];
+    [self.navigationBarView setNavigationColor:NavigationColorBlack];
+
+    
+    _txtView_log = [[UITextView alloc] initWithFrame:CGRectZero];
+    _txtView_log.backgroundColor = kBlackThemeBGColor;
+    _txtView_log.font = [UIFont systemFontOfSize:10.0f];
+    _txtView_log.textAlignment = NSTextAlignmentLeft;
+    _txtView_log.scrollEnabled = YES;
+    _txtView_log.textColor = kBlackThemetextColor;
+    _txtView_log.editable = NO;
+    _txtView_log.frame =CGRectMake(0.0f, KNaviBarHeight, kMainScreenWidth, kMainScreenHeight - KNaviBarHeight);
+    [self.view addSubview:_txtView_log];
+    
+    // Do any additional setup after loading the view, typically from a nib.
+    _netDiagnoService = [[LDNetDiagnoService alloc] initWithAppCode:@"NetWork Diagnosis"
+                                                            appName:[SLUtils appName]
+                                                         appVersion:[SLUtils appVersion]
+                                                             userID:@"kefu01@showlive.one"
+                                                           deviceID:nil
+                                                            dormain:@"www.baidu.com"
+                                                        carrierName:nil
+                                                     ISOCountryCode:nil
+                                                  MobileCountryCode:nil
+                                                      MobileNetCode:nil];
+    _netDiagnoService.delegate = self;
+    _isRunning = NO;
+
+    // Do any additional setup after loading the view.
+}
+#pragma mark NetDiagnosisDelegate
+- (void)netDiagnosisDidStarted
+{
+    NSLog(@"开始诊断～～～");
+}
+
+- (void)netDiagnosisStepInfo:(NSString *)stepInfo
+{
+    NSLog(@"%@", stepInfo);
+    _logInfo = [_logInfo stringByAppendingString:stepInfo];
+    @weakify(self)
+    dispatch_async(dispatch_get_main_queue(), ^{
+        @strongify(self)
+        self.txtView_log.text = self.logInfo;
+    });
+}
+
+
+- (void)netDiagnosisDidEnd:(NSString *)allLogInfo;
+{
+    NSLog(@"logInfo>>>>>\n%@", allLogInfo);
+    //可以保存到文件，也可以通过邮件发送回来
+    @weakify(self)
+    dispatch_async(dispatch_get_main_queue(), ^{
+        @strongify(self)
+        [self.indicatorView stopAnimating];
+        self.isRunning = NO;
+    });
+}
+
+- (void)emailLogInfo
+{
+    [_netDiagnoService printLogInfo];
+}
+
+
+#pragma mark -
+#pragma mark - textFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
