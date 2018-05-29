5839526186e9511f6c9dfd1fb39c8880aa1acd43
Gxshowone <gongxin@show.one>
2018-05-30 00:50:00 +0800

    finish model

---
+++ Classes/Model/SLFinishMode.h
@@ -0,0 +1,13 @@
+//
+//  SLFinishModel.h
+//  ShowLive
+//
+//  Created by show gx on 2018/4/20.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@interface SLFinishModel : BaseModel
+
+@end
