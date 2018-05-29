97ddd9a8acf75f58d0eda6d65269e02f1ac41679
Gxshowone <gongxin@show.one>
2018-05-29 14:55:17 +0800

    media enum

--- Classes/Model/SLAppMediaPerssion.h
+++ Classes/Model/SLAppMediaPerssion.h
@@ -7,7 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
-
+//硬件权限枚举
+typedef NS_ENUM(NSInteger, SLDeviceErrorStatus) {
+    AVDeviceNOErrorStatus  =0,//都开了
+    AudioDeviceErrorStatus = 1,//麦克风没开,相机开
+    VedioDeviceErrorStatus,//相机没开，麦克风开
+    AVDeviceErrorStatus, //都没开
+    
+};
 @interface SLAppMediaPerssion : NSObject
 
 
