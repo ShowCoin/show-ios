705da85107ceef1d54252cccef5a89af63662994
Gxshowone <gongxin@show.one>
2018-05-29 15:00:00 +0800

    mediaperssionm

---
+++ Classes/Model/SLAppMediaPerssion.m
@@ -0,0 +1,15 @@
+//
+//  SLAppMediaPerssion.m
+//  ShowLive
+//
+//  Created by show gx on 2018/4/12.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "SLAppMediaPerssion.h"
+#import <AVFoundation/AVFoundation.h>
+@implementation SLAppMediaPerssion
+
+
+@end
+
