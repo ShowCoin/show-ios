b1f5e2a82251f93acb39d6fe3736f25de6f8a37f
Vning <790068808@qq.com>
2018-05-29 17:59:24 +0800

    设置功能-三级页面-关于我们

---
+++ Classes/UserCenter/ShowForUsViewController.m
@@ -0,0 +1,70 @@
+//
+//  ShowForUsViewController.m
+//  ShowLive
+//
+//  Created by iori_chou on 2018/4/6.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "ShowForUsViewController.h"
+#import "ShowForUsView.h"
+
+@interface ShowForUsViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property(nonatomic,strong)UITableView * tableView;
+
+@end
+
+@implementation ShowForUsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self.navigationBarView setNavigationLeftBarStyle:NavigationBarLeftDefault];
+    [self.navigationBarView setNavigationTitle:@"关于我们"];
+    [self.navigationBarView setNavigationColor:NavigationColorBlack];
+    self.view.backgroundColor = kBlackThemeBGColor;
+    [self.view addSubview:self.tableView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+-(UITableView *)tableView
+{
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, KNaviBarHeight, KScreenWidth, KScreenHeight-KNaviBarHeight) style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.scrollEnabled = NO;
+        _tableView.backgroundColor =kBlackThemeBGColor;
+        _tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+        _tableView.separatorColor = kBlackThemeColor;
+        _tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+
+        if (@available(iOS 11.0, *)) {
+            _tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+            _tableView.estimatedRowHeight = 0;
+            _tableView.estimatedSectionHeaderHeight = 0;
+            _tableView.estimatedSectionFooterHeight = 0;
+        } else {
+            
+        }
+        
+    }
+    return _tableView;
+    
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
