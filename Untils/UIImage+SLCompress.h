9ba024d2014cc60013e5c54cbb11222725c2cb7d
Gxshowone <gongxin@show.one>
2018-05-30 23:38:28 +0800

    image category

---
+++ Untils/UIImage+SLCompress.h
@@ -0,0 +1,18 @@
+//
+//  UIImage+SLCompress.h
+//  ShowLive
+//
+//  Created by showgx on 2018/5/30.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#define MAX_IMAGEPIX 1000.0 // max pix 200.0px
+#define MAX_IMAGEDATA_LEN 1000000.0 // max data length 300k
+
+@interface UIImage (SLCompress)
+//图像质量压缩
+-(UIImage *)compressedImage;
+
+
+@end
