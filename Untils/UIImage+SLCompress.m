2d08bac08ab2e0fc62c1d2e971fcd18e9b0bde52
Gxshowone <gongxin@show.one>
2018-05-31 14:00:18 +0800

    image new size

--- Untils/UIImage+SLCompress.m
+++ Untils/UIImage+SLCompress.m
@@ -72,6 +72,24 @@
     UIGraphicsEndImageContext();
     return newImage;
 }
+-(UIImage*)imageWithImage:(UIImage*)image scaledToSize:(CGSize)newSize
+{
+    // Create a graphics image context
+    UIGraphicsBeginImageContext(newSize);
+    
+    // Tell the old image to draw in this new context, with the desired
+    // new size
+    [image drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
+    
+    // Get the new image from the context
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    // End the context
+    UIGraphicsEndImageContext();
+    
+    // Return the new image.
+    return newImage;
+}
 
 
 @end
