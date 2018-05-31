177af4194ebdd0c8ee24db09ad7cae9e5d1da954
Vning <790068808@qq.com>
2018-05-31 21:35:24 +0800

    getUser_UI

--- Classes/UserCenter/Controller/UserCenterViewController.m
+++ Classes/UserCenter/Controller/UserCenterViewController.m
@@ -66,6 +66,92 @@
     [super viewDidAppear:YES];
     [PageMgr setRootScrollEnabled:NO];
 }
+- (void)getUserInfo
+{
+    self.action = [SLGetUserInfoAction action];
+    //    self.action.modelClass = AccountModel.self;
+    if (!_IsMe) {
+        self.action.uid = _userModel.uid;
+    }
+    @weakify(self);
+    self.action.finishedBlock = ^(NSDictionary * dic) {
+        @strongify(self);
+        if (self.IsMe) {
+            AccountModel * reponseModel = [AccountModel mj_objectWithKeyValues:dic];
+            AccountModel *model = [AccountModel shared];
+            reponseModel.token = model.token;
+            model.state = UserAccountState_Login;
+            [model updateInfo:reponseModel];
+            [model save];
+        }
+        self.userModel = [ShowUserModel mj_objectWithKeyValues:dic];
+        //计算个性签名高度
+        [self fitDescriptionsHeight];
+        [self.mainCollectionView reloadData];
+    };
+    self.action.failedBlock = ^(NSError *err) {
+    };
+    [self.action start];
+    
+}
+
+- (void)fitDescriptionsHeight
+{
+    if (IsStrEmpty(self.userModel.descriptions)) {
+        self.userModel.descriptions = @"此人很懒，还没有签名哦~";
+    }
+    self.testStr = self.userModel.descriptions;
+    NSMutableParagraphStyle *paraStyle = [[NSMutableParagraphStyle alloc] init];
+    paraStyle.lineBreakMode = NSLineBreakByCharWrapping;
+    paraStyle.alignment = NSTextAlignmentCenter;
+    paraStyle.lineSpacing = 0; //设置行间距
+    paraStyle.hyphenationFactor = 1.0;
+    paraStyle.firstLineHeadIndent = 0.0;
+    paraStyle.paragraphSpacingBefore = 0.0;
+    paraStyle.headIndent = 0;
+    paraStyle.tailIndent = 0;
+    //    设置字间距 NSKernAttributeName:@1.5f
+    NSDictionary *strdic = @{NSFontAttributeName:Font_Regular(14*Proportion375), NSParagraphStyleAttributeName:paraStyle, NSKernAttributeName:@0.5f
+                             };
+    if (IsStrEmpty(self.testStr)) {
+        return;
+    }
+    NSAttributedString *attributeStr = [[NSAttributedString alloc] initWithString:self.testStr attributes:strdic];
+    
+    CGSize size = CGSizeMake(kMainScreenWidth-40, CGFLOAT_MAX);
+    YYTextLayout *layout = [YYTextLayout layoutWithContainerSize:size text:attributeStr];
+    self.wordsHeight = layout.textBoundingSize.height;
+    NSLog(@"==========%f",self.wordsHeight);
+    
+    if (!self.dataSource.count) {
+        [self.tableView setContentSize:CGSizeMake(kMainScreenWidth, HeaderHeightWithoutWords + self.wordsHeight + 100*Proportion375)];
+    }
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self.navigationBarView setNavigationColor:NavigationColorBlack];
+    self.navigationBarView.hidden = YES;
+    self.view.backgroundColor = kThemeWhiteColor;
+    self.dataModelList = [NSMutableArray array];
+    self.dataSource = [NSMutableArray array];
+    [self.view addSubview:self.mainCollectionView];
+    [self.view addSubview:self.floatView];
+    //    _nodataLab = [[UILabel alloc] initWithFrame:CGRectMake(0, HeaderHeightWithoutWords + self.wordsHeight + 70*Proportion375, kMainScreenWidth, 17)];
+    
+    [self resetParameter];
+    [self requestWithMore:NO];
+    @weakify(self);
+    [[[NSNotificationCenter defaultCenter] rac_addObserverForName:kNotificationLogout object:nil] subscribeNext:^(id x) {
+        @strongify(self);
+        [self.dataSource removeAllObjects];
+        [self.mainCollectionView reloadData];
+        self.cursor= @"0";
+    }];
+}
+
 
 
 
