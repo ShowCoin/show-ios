143f328b4905f06d3205287e2b6a41fd0ffcfd78
Gxshowone <gongxin@show.one>
2018-05-31 14:05:31 +0800

    cut image

--- Untils/UIImage+SLCompress.m
+++ Untils/UIImage+SLCompress.m
@@ -296,5 +296,21 @@
     return image;
     
 }
+/** 设置圆形图片(放到分类中使用) */
+- (UIImage *)cutCircleImage {
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0);
+    // 获取上下文
+    CGContextRef ctr = UIGraphicsGetCurrentContext();
+    // 设置圆形
+    CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
+    CGContextAddEllipseInRect(ctr, rect);
+    // 裁剪
+    CGContextClip(ctr);
+    // 将图片画上去
+    [self drawInRect:rect];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
 
 @end
