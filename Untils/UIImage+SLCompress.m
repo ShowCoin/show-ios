bcdc4d9113338dc1de4ffa8bae7b0cbd7edde1f7
Gxshowone <gongxin@show.one>
2018-05-31 14:01:21 +0800

    commpresse image

--- Untils/UIImage+SLCompress.m
+++ Untils/UIImage+SLCompress.m
@@ -90,6 +90,50 @@
     // Return the new image.
     return newImage;
 }
+-(UIImage *)compressedImage {
+    CGSize imageSize = self.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    
+    if (width <= MAX_IMAGEPIX && height <= MAX_IMAGEPIX) {
+        // no need to compress.
+        return self;
+    }
+    
+    if (width == 0 || height == 0) {
+        // void zero exception
+        return self;
+    }
+    
+    UIImage *newImage = nil;
+    CGFloat widthFactor = MAX_IMAGEPIX / width;
+    CGFloat heightFactor = MAX_IMAGEPIX / height;
+    CGFloat scaleFactor = 0.0;
+    
+    if (widthFactor > heightFactor)
+        scaleFactor = heightFactor; // scale to fit height
+    else
+        scaleFactor = widthFactor; // scale to fit width
+    
+    CGFloat scaledWidth = width * scaleFactor;
+    CGFloat scaledHeight = height * scaleFactor;
+    CGSize targetSize = CGSizeMake(scaledWidth, scaledHeight);
+    
+    UIGraphicsBeginImageContext(targetSize); // this will crop
+    
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.size.width = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    
+    [self drawInRect:thumbnailRect];
+    
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    //pop the context to get back to the default
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
 
 
 @end
