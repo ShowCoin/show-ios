40fa4632ca89280bf703db3192ea71595c1050c4
Gxshowone <gongxin@show.one>
2018-06-01 12:53:52 +0800

    add static

--- Untils/UIView+SLComOpenThreadOTScreenshotHelperStatusBarReference.m
+++ Untils/UIView+SLComOpenThreadOTScreenshotHelperStatusBarReference.m
@@ -5,7 +5,7 @@
 //  Created by showgx on 2018/5/30.
 //  Copyright © 2018年 vning. All rights reserved.
 //
-
+static UIView *statusBarInstance = nil;
 
 #import "SLComOpenThreadOTScreenshotHelperSwizzleHelper.h"
 #import "UIView+SLComOpenThreadOTScreenshotHelperStatusBarReference.h"
