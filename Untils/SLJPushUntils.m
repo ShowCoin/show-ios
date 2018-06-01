4f296eb00d0e08ff6776ff0fa8684d6bdc9a4ec4
Gxshowone <gongxin@show.one>
2018-06-01 13:19:35 +0800

    jpush util .m

---
+++ Untils/SLJPushUntils.m
@@ -0,0 +1,14 @@
+//
+//  SLJPushUntils.m
+//  ShowLive
+//
+//  Created by iori_chou on 2018/3/21.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "SLJPushUntils.h"
+#import "JPUSHService.h"
+@implementation SLJPushUntils
+
+
+@end
