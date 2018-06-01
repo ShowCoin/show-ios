c13e9b64e48c27d02624378d995bb6e187547629
Gxshowone <gongxin@show.one>
2018-06-01 12:51:54 +0800

    remove static

---
+++ Untils/UIView+SLComOpenThreadOTScreenshotHelperStatusBarReference.m
@@ -0,0 +1,18 @@
+//
+//  UIView+SLComOpenThreadOTScreenshotHelperStatusBarReference.m
+//  ShowLive
+//
+//  Created by showgx on 2018/5/30.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+
+#import "SLComOpenThreadOTScreenshotHelperSwizzleHelper.h"
+#import "UIView+SLComOpenThreadOTScreenshotHelperStatusBarReference.h"
+
+@implementation UIView (SLComOpenThreadOTScreenshotHelperStatusBarReference)
+
+
+
+
+@end
