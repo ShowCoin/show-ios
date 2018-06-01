5a57320f2c1a217eed76de81805763f0e483dc0c
Gxshowone <gongxin@show.one>
2018-06-01 17:53:46 +0800

    window category m

---
+++ Untils/UIWindow+SLMotion.m
@@ -0,0 +1,18 @@
+//
+//  UIWindow+SLMotion.m
+//  ShowLive
+//
+//  Created by show gx on 2018/4/18.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "UIWindow+SLMotion.h"
+//#import "SULogger.h"
+
+#define UIEventSubtypeMotionShakeNotification @"UIEventSubtypeMotionShakeNotification"
+
+@implementation UIWindow (SLMotion)
+
+
+
+@end
