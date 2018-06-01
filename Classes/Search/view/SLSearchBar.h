aa553104a836bc0067692fd23ea7ba32d0f5c160
chouYagami <zhouhua@show.one>
2018-06-01 17:33:17 +0800

    Add comments

--- Classes/Search/view/SLSearchBar.h
+++ Classes/Search/view/SLSearchBar.h
@@ -34,12 +34,14 @@ NS_ASSUME_NONNULL_BEGIN
 @property(nullable,nonatomic,strong) UIFont  *font;
 @property(nullable,nonatomic,strong) UIColor *placeholderColor;
 
+/* Allow placement of an input accessory view to the keyboard for the search bar
+ */
 @property (nullable,nonatomic,readwrite,strong) UIView *inputAccessoryView;
-@property (assign, nonatomic) BOOL canHideCancelButton;
-@property (assign, nonatomic) NSInteger leadingOrTailMargin;
-@property (assign, nonatomic) NSInteger searchHeight;
-@property (assign, nonatomic) NSInteger hiddenCancelbutton;
-@property (strong, nonatomic,nullable) UIColor * backColor; 
+@property (assign, nonatomic) BOOL canHideCancelButton;// 是否可以隐藏取消按钮 默认NO,该模式下是icon中间布局
+@property (assign, nonatomic) NSInteger leadingOrTailMargin; // textfiled距离左边、右边的距离 默认15
+@property (assign, nonatomic) NSInteger searchHeight; // textfiled高
+@property (assign, nonatomic) NSInteger hiddenCancelbutton; // 隐藏取消键
+@property (strong, nonatomic,nullable) UIColor * backColor; // 颜色
 
 - (BOOL)becomeFirstResponder;
 - (BOOL)resignFirstResponder;
