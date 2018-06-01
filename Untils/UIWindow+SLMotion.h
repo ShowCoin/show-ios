8025a92acf512913a84b78f6ba37b0a8cd67fed2
Gxshowone <gongxin@show.one>
2018-06-01 17:51:00 +0800

    window category

---
+++ Untils/UIWindow+SLMotion.h
@@ -0,0 +1,15 @@
+//
+//  UIWindow+SLMotion.h
+//  ShowLive
+//
+//  Created by show gx on 2018/4/18.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWindow (SLMotion)
+
+
+
+@end
