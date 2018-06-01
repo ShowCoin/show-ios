13bac43e53b73896c1b882980cb9d27b47dae01f
Gxshowone <gongxin@show.one>
2018-06-01 17:02:52 +0800

    textview +rect.h

---
+++ Untils/UITextView+Rect.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Extent.h
+//  QinChat
+//
+//  Created by show on 2017/2/22.
+//  Copyright © 2017年 zero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextView (Rect)
+
+@end
