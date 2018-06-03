dd70acdafee50e9f03b912af14f2ec7136308b22
Vning <790068808@qq.com>
2018-06-03 17:37:04 +0800

    可播放视频，gif，图片，倒计时

--- Classes/ADView/SLADView.m
+++ Classes/ADView/SLADView.m
@@ -82,6 +82,98 @@
     return self;
 }
 
+-(void)setupView{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationDidBecomeActiveNotification:)
+                                                 name:UIApplicationDidBecomeActiveNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationWillResignActiveNotification:)
+                                                 name:UIApplicationDidEnterBackgroundNotification
+                                               object:nil];
+
+    NSURL * url = [NSURL URLWithString:_admodel.thumbnail];
+    UIImage * adImage= __IphoneX__? [UIImage imageNamed:@"login_bg"]: [UIImage imageNamed:@"login_bg"];
+    if (_movieURL) {
+//        CALayer *backLayer = [CALayer layer];
+//        backLayer.frame = [UIScreen mainScreen].bounds;
+//        backLayer.contents = (__bridge id _Nullable)(adImage.CGImage);//[UIImage getLaunchImage]
+//        [self.layer addSublayer:backLayer];
+        self.backgroundColor =kBlackThemeBGColor;
+//        [self addSubview:self.moviePlayer.view];
+//        [self.moviePlayer play];
+
+        AVPlayer *player = [[AVPlayer alloc] initWithURL:_movieURL];
+        self.player = player;
+        AVPlayerLayer *playerLayer = [AVPlayerLayer playerLayerWithPlayer:player];
+        self.playerLayer = playerLayer;
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        playerLayer.frame = [UIScreen mainScreen].bounds;
+        [self.layer addSublayer:playerLayer];
+        [self.player play];
+
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playbackFinished:) name:AVPlayerItemDidPlayToEndTimeNotification object:nil];
+
+    }
+    
+   else if (_admodel) {
+        
+        [self addSubview:self.adImageView];
+        [self addSubview:self.progressButtonView];
+        self.progressButtonView.hidden = YES;
+        //        UIImage * adImage= __IphoneX__? [UIImage imageNamed:@"adimage_iphoneX"]: [UIImage imageNamed:@"adimage"];
+        __weak typeof(self) weaks = self;
+        [self.adImageView yy_setImageWithURL:url placeholder:adImage options:YYWebImageOptionSetImageWithFadeAnimation completion:^(UIImage * _Nullable image, NSURL * _Nonnull url, YYWebImageFromType from, YYWebImageStage stage, NSError * _Nullable error) {
+            weaks.progressButtonView.hidden = NO;
+        }];
+    }else{
+        [self addSubview:self.adImageView];
+        self.adImageView.image = adImage;
+        self.backgroundColor = [UIColor whiteColor];
+        
+        [self addSubview:self.iconImageView];
+        [self addSubview:self.nameLabel];
+        [self addSubview:self.descLabel];
+        
+        UIImage *image = [[YYWebImageManager sharedManager].cache getImageForKey:AccountUserInfoModel.avatar];
+        if (image == nil && AccountUserInfoModel.avatar.length >0) {
+            [[YYWebImageManager sharedManager] requestImageWithURL:[NSURL URLWithString:AccountUserInfoModel.avatar] options:0 progress:nil transform:nil completion:nil];
+        }
+        image = image == nil? [UIImage imageNamed:@"userhome_admin_Img"] : image;
+        if (image) {
+            [self.iconImageView setRoundStyle:YES imageUrl:@"" imageHeight:90 vip:NO attestation:NO];
+            self.iconImageView.imageView.image = image;
+        }
+        
+        self.nameLabel.text = [NSString stringWithFormat:@"Hi %@",AccountUserInfoModel.nickname?AccountUserInfoModel.nickname:@"老铁"];
+        self.descLabel.text =  @"欢迎回来";
+        
+        self.iconImageView.alpha = 0;
+        [UIView animateWithDuration:0.5 delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+            self.iconImageView.transform = CGAffineTransformMakeTranslation(0, -20 *HScale);
+            self.iconImageView.alpha = 1;
+        } completion:^(BOOL finished) {
+            
+        }];
+        
+        self.nameLabel.alpha = 0;
+        [UIView animateWithDuration:0.5 delay:0.5 options:UIViewAnimationOptionCurveEaseOut animations:^{
+            self.nameLabel.transform = CGAffineTransformMakeTranslation(0, -20 *HScale);
+            self.nameLabel.alpha = 1;
+        } completion:^(BOOL finished) {
+            
+        }];
+        
+        self.descLabel.alpha = 0;
+        [UIView animateWithDuration:0.5 delay:1 options:UIViewAnimationOptionCurveEaseOut animations:^{
+            self.descLabel.transform = CGAffineTransformMakeTranslation(0, -20 *HScale);
+            self.descLabel.alpha = 1;
+        } completion:^(BOOL finished) {
+            
+        }];
+    }
+}
 
 
 /*
