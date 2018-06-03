0ae7d8bc4339a7064d7e6e9c48239769de07d8b3
chenyh <chenyinghao@show.one>
2018-06-03 17:42:32 +0800

    note -5

--- Classes/Live/Controller/SLToolViewController/SLPlayerMoreController.m
+++ Classes/Live/Controller/SLToolViewController/SLPlayerMoreController.m
@@ -79,7 +79,7 @@ static dispatch_once_t onceToken;
 /**
  animateTransition
 
- @param transitionContext transitionContext2
+ @param transitionContext transitionContext22
  */
 - (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext {
     CGFloat h = UIScreen.mainScreen.bounds.size.height;
