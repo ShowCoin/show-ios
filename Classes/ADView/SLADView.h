c358858026081287304a0ef8a64de6a37d5c9cc9
Vning <790068808@qq.com>
2018-06-03 17:33:45 +0800

    启动页  广告页设计

---
+++ Classes/ADView/SLADView.h
@@ -0,0 +1,20 @@
+//
+//  SLADView.h
+//  ShowLive
+//
+//  Created by iori_chou on 2018/4/17.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@protocol SLADViewDelegate <NSObject>
+
+- (void)ADViewWillDisappear;
+
+@end
+
+@interface SLADView : UIView
+@property (nonatomic, weak) id<SLADViewDelegate> delegate;
+@property (nonatomic,strong) NSURL *movieURL;
+- (instancetype) initWithFrame:(CGRect)frame withMovieUrl:(NSURL *)movieURL;
+@end
