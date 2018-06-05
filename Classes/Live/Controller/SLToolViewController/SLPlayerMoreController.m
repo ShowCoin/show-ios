e9ab04256a06757378764c1ca43194936be3ec6e
chenyh <chenyinghao@show.one>
2018-06-05 18:50:56 +0800

    authorizationStatusAuthorized

--- Classes/Live/Controller/SLToolViewController/SLPlayerMoreController.m
+++ Classes/Live/Controller/SLToolViewController/SLPlayerMoreController.m
@@ -110,4 +110,16 @@ static dispatch_once_t onceToken;
 
 @implementation PAPhotoAuthorized
 
++ (BOOL)authorizationStatusAuthorized {
+    NSInteger status = [self.class authorizationStatus];
+    if (status == 0) {
+        /**
+         * 当某些情况下AuthorizationStatus == AuthorizationStatusNotDetermined时，无法弹出系统首次使用的授权alertView，系统应用设置里亦没有相册的设置，此时将无法使用，故作以下操作，弹出系统首次使用的授权alertView
+         */
+        [self requestAuthorizationWithCompletion:nil];
+    }
+    
+    return status == 3;
+}
+
 @end
