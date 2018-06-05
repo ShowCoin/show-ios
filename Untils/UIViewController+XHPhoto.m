0c8613a90bf9ecbb823f3403087be3c5d9b090ca
Gxshowone <gongxin@show.one>
2018-06-05 11:33:52 +0800

    import

--- Untils/UIViewController+XHPhoto.m
+++ Untils/UIViewController+XHPhoto.m
@@ -7,5 +7,10 @@
 //  Copyright © 2017年 frank. All rights reserved.
 
 #import "UIViewController+XHPhoto.h"
+#import "objc/runtime.h"
+#import <AssetsLibrary/ALAssetsLibrary.h>
+#import "DeviceInfo.h"
+#import <Photos/Photos.h>
+
 
 @end
