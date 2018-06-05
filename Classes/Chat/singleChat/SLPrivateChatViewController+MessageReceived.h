796e787007043d179ecb05fce9374fb83c8b886a
chouYagami <zhouhua@show.one>
2018-06-05 14:49:57 +0800

    Add the chat message delivery management extension

---
+++ Classes/Chat/singleChat/SLPrivateChatViewController+MessageReceived.h
@@ -0,0 +1,14 @@
+//
+//  SLPrivateChatViewController+MessageReceived.h
+//  ShowLive
+//
+//  Created by iori_chou on 2018/4/11.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "SLPrivateChatViewController.h"
+
+@interface SLPrivateChatViewController (MessageReceived)
+- (void)addMessageReceivedObservers;
+
+@end
