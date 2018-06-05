8092f172424624a4d7b80effdfc098601cb80a66
Vning <790068808@qq.com>
2018-06-05 17:04:17 +0800

    用户等级控件

---
+++ Common/SLLevelView/SLLevelMarkView.h
@@ -0,0 +1,20 @@
+//
+//  SLLevelView.h
+//  ShowLive
+//
+//  Created by iori_chou on 2018/4/25.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+typedef enum : NSUInteger {
+    LevelType_ShowCoin,        //  财富等级
+    LevelType_Host,        //  主播等级
+} LevelType;
+
+@interface SLLevelMarkView : UIView
+@property (nonatomic , assign) LevelType type;
+@property (nonatomic , copy) NSString * level;
+
+- (id)initWithFrame:(CGRect)frame withType:(LevelType)type;
+@end
