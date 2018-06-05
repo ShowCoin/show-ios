818dac3d010120ba617af98f1b391bec7b6aab43
Gxshowone <gongxin@show.one>
2018-06-05 11:33:14 +0800

    xhphoto.m

---
+++ Untils/UIViewController+XHPhoto.m
@@ -0,0 +1,11 @@
+//
+//  UIViewController+XHPhoto.m
+
+//  XHPhotoExample
+//
+//  Created by frank on 17/2/1.
+//  Copyright © 2017年 frank. All rights reserved.
+
+#import "UIViewController+XHPhoto.h"
+
+@end
