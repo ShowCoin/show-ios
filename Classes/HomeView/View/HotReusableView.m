de546ac25511ccb71b804642fc893ceef6a4c669
Vning <790068808@qq.com>
2018-06-07 15:27:39 +0800

    banner定时刷新

---
+++ Classes/HomeView/View/HotReusableView.m
@@ -0,0 +1,20 @@
+//
+//  HotReusableView.m
+//  ShowLive
+//
+//  Created by vning on 2018/4/7.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "HotReusableView.h"
+
+@implementation HotReusableView
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor greenColor];
+    }
+    return self;
+}
+@end
