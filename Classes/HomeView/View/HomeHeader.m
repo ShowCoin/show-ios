3336824e2832f4dbc8a75960a9c492bdf50abad8
Vning <790068808@qq.com>
2018-06-07 15:25:57 +0800

    homeheader

--- Classes/HomeView/View/HomeHeader.m
+++ Classes/HomeView/View/HomeHeader.m
@@ -61,6 +61,100 @@ static BOOL isHot = NO;
     }
 }
 
+- (void)updateTotalUnreadCount:(NSNotification *)notification
+{
+    NSString * count=notification.object;
+    self.rightBtn.hasMessage = (count.integerValue>0)?YES:NO;
+}
+
+- (SLMessageButton *)rightBtn {
+    if (!_rightBtn) {
+        _rightBtn = [SLMessageButton buttonWithType:UIButtonTypeCustom];
+        _rightBtn.frame = CGRectMake(kMainScreenWidth - 50, 17+KTopHeight, 50, 50);
+        [_rightBtn setImage:[UIImage imageNamed:@"live_bottom_more"] forState:UIControlStateNormal];
+        @weakify(self);
+        [[_rightBtn rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
+            @strongify(self);
+            NSLog(@"%s", __func__);
+            if ([self.delegate respondsToSelector:@selector(rightBtnClick:)]) {
+                [self.delegate rightBtnClick:self.rightBtn];
+            }
+        }];
+    }
+    return _rightBtn;
+}
+
+-(UIImageView *)grayBg
+{
+    if (!_grayBg) {
+        _grayBg = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, kMainScreenWidth, KNaviBarHeight)];
+        [_grayBg setImage:[UIImage imageNamed:@"home_header_bg"]];
+    }
+    return _grayBg;
+}
+
+-(UIButton *)navBtnA
+{
+    if (!_navBtnA) {
+        _navBtnA=[UIButton buttonWithType:UIButtonTypeCustom];
+        _navBtnA.frame=CGRectMake(self.navBtnB.left - 85*Proportion375, KTopHeight + 20, 85*Proportion375, 43);
+        [_navBtnA setTitle:STRING_HOME_TITLECONCER_74 forState:UIControlStateNormal];
+        [_navBtnA setTitleColor:kThemeAlphWhiteColor forState:UIControlStateNormal];
+        [_navBtnA.titleLabel setFont:Font_Medium(18*Proportion375)];
+         @weakify(self);
+        [[_navBtnA rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
+            @strongify(self);
+            if (self.delegate && [self.delegate respondsToSelector:@selector(navTabAaction:)]) {
+                [self.delegate navTabAaction:self.navBtnA];
+            }
+            [self rightButtonHidden:YES];
+        }];
+    }
+    return _navBtnA;
+}
+
+-(UIButton *)navBtnB
+{
+    if (!_navBtnB) {
+        _navBtnB=[UIButton buttonWithType:UIButtonTypeCustom];
+        _navBtnB.frame=CGRectMake(85*Proportion375, KTopHeight + 20, 85*Proportion375, 43);
+        _navBtnB.centerX = kMainScreenWidth/2;
+        [_navBtnB setTitle:STRING_HOME_TITLEHOT_75 forState:UIControlStateNormal];
+        [_navBtnB setTitleColor:kThemeWhiteColor forState:UIControlStateNormal];
+        [_navBtnB.titleLabel setFont:Font_Medium(20*Proportion375)];
+         @weakify(self);
+        [[_navBtnB rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
+            @strongify(self);
+            if (self.delegate && [self.delegate respondsToSelector:@selector(navTabBaction:)]) {
+                [self.delegate navTabBaction:self.navBtnB];
+            }
+            [self rightButtonHidden:NO];
+        }];
+    }
+    return _navBtnB;
+}
+
+-(UIButton *)navBtnC
+{
+    if (!_navBtnC) {
+        _navBtnC=[UIButton buttonWithType:UIButtonTypeCustom];
+        _navBtnC.frame=CGRectMake(self.navBtnB.right, KTopHeight + 20, 85*Proportion375, 43);
+        [_navBtnC setTitle:STRING_HOME_TITLENEW_76 forState:UIControlStateNormal];
+        [_navBtnC setTitleColor:kThemeAlphWhiteColor forState:UIControlStateNormal];
+        [_navBtnC.titleLabel setFont:Font_Medium(18*Proportion375)];
+        @weakify(self);
+        [[_navBtnC rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
+            @strongify(self);
+            if (self.delegate && [self.delegate respondsToSelector:@selector(navTabBaction:)]) {
+                [self.delegate navTabCaction:self.navBtnC];
+            }
+            [self rightButtonHidden:YES];
+        }];
+
+    }
+    return _navBtnC;
+}
+
 
 
 @end
