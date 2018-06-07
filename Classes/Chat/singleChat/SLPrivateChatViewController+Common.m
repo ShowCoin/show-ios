19fb143f43789057bb1a2ec9c277d814c4193e00
chouYagami <zhouhua@show.one>
2018-06-07 16:39:33 +0800

    Improve the implementation of the chat Common extension method

---
+++ Classes/Chat/singleChat/SLPrivateChatViewController+Common.m
@@ -0,0 +1,17 @@
+//
+//  SLPrivateChatViewController+Common.m
+//  ShowLive
+//
+//  Created by iori_chou on 2018/4/11.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "SLPrivateChatViewController+Common.h"
+#import "ShowWaringView.h"
+
+@implementation SLPrivateChatViewController (Common)
+#pragma mark - Toast
+
+
+
+@end
