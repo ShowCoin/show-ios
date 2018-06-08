952de90dc1f99505ad4f5481ecba0d6043b0fa9c
Gxshowone <gongxin@show.one>
2018-06-08 18:36:56 +0800

    add finish item

---
+++ Classes/View/SLLiveFinishView/SLLiveFinishItem.h
@@ -0,0 +1,14 @@
+//
+//  SLLiveFinishItem.h
+//  ShowLive
+//
+//  Created by gongxin on 2018/4/14.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLLiveFinishItem : UIView
+
+
+@end
---
+++ Classes/View/SLLiveFinishView/SLLiveFinishItem.m
@@ -0,0 +1,17 @@
+//
+//  SLLiveFinishItem.m
+//  ShowLive
+//
+//  Created by gongxin on 2018/4/14.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "SLLiveFinishItem.h"
+
+@interface SLLiveFinishItem ()
+
+@end
+
+@implementation SLLiveFinishItem
+
+@end
