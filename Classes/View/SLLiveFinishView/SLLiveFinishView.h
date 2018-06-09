9761881487bfe337cb68621f0298094ad456a526
Gxshowone <gongxin@show.one>
2018-06-09 16:13:20 +0800

    live finish view add

---
+++ Classes/View/SLLiveFinishView/SLLiveFinishView.h
@@ -0,0 +1,17 @@
+//
+//  SLLiveFinishView.h
+//  ShowLive
+//
+//  Created by gongxin on 2018/4/14.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SLLiveStartModel.h"
+#import "SLLiveListModel.h"
+#import "SLFinishModel.h"
+
+@interface SLLiveFinishView : UIView
+
+
+@end
---
+++ Classes/View/SLLiveFinishView/SLLiveFinishView.m
@@ -0,0 +1,28 @@
+//
+//  SLLiveFinishView.m
+//  ShowLive
+//
+//  Created by gongxin on 2018/4/14.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "SLLiveFinishView.h"
+#import "SLLiveStopAction.h"
+#import "SLLiveStopModel.h"
+#import "SLLiveFinishItem.h"
+#import "SLHeadPortrait.h"
+#import "SLShadowLabel.h"
+#import <Photos/Photos.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+#import "SLLiveDelete.h"
+
+@interface SLLiveFinishView ()
+
+
+@end
+
+@implementation SLLiveFinishView
+
+
+
+@end
