189f5d6bb97c8321a9626068850bbe1edead3b74
chenyh <chenyinghao@show.one>
2018-06-09 16:04:38 +0800

    add request

--- Classes/Live/Controller/SLToolViewController/SLPlayerMoreController.m
+++ Classes/Live/Controller/SLToolViewController/SLPlayerMoreController.m
@@ -351,7 +351,7 @@ static dispatch_once_t onceToken;
 
 /**
  request Authorization
- // note 2
+ // note 3
  @param completion block
  */
 + (void)requestAuthorizationWithCompletion:(void (^)(void))completion {
