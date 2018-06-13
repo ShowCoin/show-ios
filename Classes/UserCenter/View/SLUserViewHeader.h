b7dbf38fabf7364703525d29b9a70e9658c00215
chenyh <chenyinghao@show.one>
2018-06-13 20:15:33 +0800

    css f

--- Classes/UserCenter/View/SLUserViewHeader.h
+++ Classes/UserCenter/View/SLUserViewHeader.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+
 @protocol SLUserViewHeaderDelegate <NSObject>
 
 -(void)SLUserViewHeaderConcernActionDelegateWithModel:(ShowUserModel *)model;
@@ -16,83 +17,85 @@
 @optional
 
 @end
+
 @interface SLUserViewHeader : UICollectionReusableView<HeadPortraitDelegate>{
     NSShadow * _shadowColor;
 }
 //活跃
-@property (nonatomic,strong)UILabel * navLab;
+@property (nonatomic, strong) UILabel * navLab;
 //左边的按钮
-@property (nonatomic,strong)UIButton * leftBtn;
+@property (nonatomic, strong) UIButton * leftBtn;
 //列表按钮
-@property (nonatomic,strong)UIButton * listBtn;
+@property (nonatomic, strong) UIButton * listBtn;
 //设置按钮
-@property (nonatomic,strong)UIButton * settingBtn;
+@property (nonatomic, strong) UIButton * settingBtn;
 //礼物按钮
-@property (nonatomic,strong)UIButton * giftStoreBtn;
+@property (nonatomic, strong) UIButton * giftStoreBtn;
 //头像
-@property (nonatomic,strong)SLHeadPortrait * headPortrait;
+@property (nonatomic, strong) SLHeadPortrait * headPortrait;
 //名称label
-@property (nonatomic,strong)UILabel * nickLab;
+@property (nonatomic, strong) UILabel * nickLab;
 
 //主播等级
-@property (nonatomic,strong)SLLevelMarkView *  masterLevel;
+@property (nonatomic, strong) SLLevelMarkView *  masterLevel;
 //show等级
-@property (nonatomic,strong)SLLevelMarkView *  showLevel;
+@property (nonatomic, strong) SLLevelMarkView *  showLevel;
 
 //性别view
-@property (nonatomic,strong)UIView * sexbg;
+@property (nonatomic, strong) UIView * sexbg;
 //性别image
-@property (nonatomic,strong)UIImageView * sexImg;
+@property (nonatomic, strong) UIImageView * sexImg;
 //性别label
-@property (nonatomic,strong)UILabel * sexlab;
+@property (nonatomic, strong) UILabel * sexlab;
 //城市label
-@property (nonatomic,strong)UILabel * cityLab;
+@property (nonatomic, strong) UILabel * cityLab;
 //自我介绍按钮
-@property (nonatomic,strong)UILabel * constellationLab;
+@property (nonatomic, strong) UILabel * constellationLab;
 
 //@property (nonatomic,strong)UILabel * idPreLab;
-@property (nonatomic,strong)UILabel * idLab;
+@property (nonatomic, strong) UILabel * idLab;
 //词label
-@property (nonatomic,strong)UILabel * wordsLab;
+@property (nonatomic, strong) UILabel * wordsLab;
 //底部视图
-@property (nonatomic,strong)UIView * bottomWhiteView;
+@property (nonatomic, strong) UIView * bottomWhiteView;
 //粉丝按钮
-@property (nonatomic,strong)UIButton * fansBtn;
+@property (nonatomic, strong) UIButton * fansBtn;
 //关注按钮
-@property (nonatomic,strong)UIButton * concerBtn;
+@property (nonatomic, strong) UIButton * concerBtn;
 //钱包按钮
-@property (nonatomic,strong)UIButton * walletBtn;
+@property (nonatomic, strong) UIButton * walletBtn;
 //分享按钮
-@property (nonatomic,strong)UIButton * shareBtn;
+@property (nonatomic, strong) UIButton * shareBtn;
 //跳转关注按钮
-@property (nonatomic,strong)UIButton * toConcerBtn;
+@property (nonatomic, strong) UIButton * toConcerBtn;
 //跳转发送按钮
-@property (nonatomic,strong)UIButton * tosendMessageBtn;
+@property (nonatomic, strong) UIButton * tosendMessageBtn;
 //头部视图
-@property (nonatomic,strong)UIImageView * headImgView;
+@property (nonatomic, strong) UIImageView * headImgView;
 //磨砂颜色
-@property (nonatomic,strong)UIView * effColorView;
+@property (nonatomic, strong) UIView * effColorView;
 //磨砂视图
-@property (nonatomic,strong)UIVisualEffectView * effectview;
+@property (nonatomic, strong) UIVisualEffectView * effectview;
 //活跃
-@property (nonatomic,strong)UIButton * worksBtn;
+@property (nonatomic, strong) UIButton * worksBtn;
 //喜欢
-@property (nonatomic,strong)UIButton * likesBtn;
+@property (nonatomic, strong) UIButton * likesBtn;
 //底部的线
-@property (nonatomic,strong)UIView * bottomAnimationLine;
+@property (nonatomic, strong) UIView * bottomAnimationLine;
 
 //用户模型
-@property (nonatomic,strong)ShowUserModel * userModel;
+@property (nonatomic, strong) ShowUserModel * userModel;
 //本人的表识
-@property(nonatomic,assign)BOOL isMe;
+@property (nonatomic, assign) BOOL isMe;
 //迷你卡
-@property(nonatomic,assign)BOOL isMiniCard;
+@property (nonatomic, assign) BOOL isMiniCard;
 //文字高度
-@property(nonatomic,assign)CGFloat labelHeight;
+@property( nonatomic, assign) CGFloat labelHeight;
 //代理
 @property (nonatomic, weak) id<SLUserViewHeaderDelegate> delegate;
 //控制器
-@property(nonatomic,strong)BaseViewController * Controller;
+@property (nonatomic, strong) BaseViewController * Controller;
 //滚动视图
-@property(nonatomic, strong)UIScrollView *scrollerContentView;
+@property (nonatomic, strong) UIScrollView *scrollerContentView;
+
 @end
