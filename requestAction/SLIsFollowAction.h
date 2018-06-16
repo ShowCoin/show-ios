54fa2533735353771f9df0596eb33fe7af9b4fd6
chouYagami <zhouhua@show.one>
2018-06-16 11:18:31 +0800

    Add follow status request

---
+++ requestAction/SLIsFollowAction.h
@@ -0,0 +1,15 @@
+//
+//  SLIsFollowAction.h
+//  ShowLive
+//
+//  Created by gongxin on 2018/5/13.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "ShowAction.h"
+
+@interface SLIsFollowAction : ShowAction
+
+@property(nonatomic,copy)NSString * uid;
+
+@end
