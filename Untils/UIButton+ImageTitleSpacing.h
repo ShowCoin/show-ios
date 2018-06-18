9f2221a262c912079507da0d281a59b6fd5159d2
Gxshowone <gongxin@show.one>
2018-06-18 16:15:03 +0800

    layoutButton

--- Untils/UIButton+ImageTitleSpacing.h
+++ Untils/UIButton+ImageTitleSpacing.h
@@ -14,5 +14,15 @@ typedef NS_ENUM(NSUInteger, SLButtonEdgeInsetsStyle) {
     SLButtonEdgeInsetsStyleRight // image在右，label在左
 };
 
+@interface UIButton (ImageTitleSpacing)
+
+/**
+ *  设置button的titleLabel和imageView的布局样式，及间距
+ *
+ *  @param style titleLabel和imageView的布局样式
+ *  @param space titleLabel和imageView的间距
+ */
+- (void)layoutButtonWithEdgeInsetsStyle:(SLButtonEdgeInsetsStyle)style
+                        imageTitleSpace:(CGFloat)space;
 
 @end
