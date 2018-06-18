95d4c138d75668abe8ce0b80f0c145e08f5229ab
Gxshowone <gongxin@show.one>
2018-06-18 16:14:47 +0800

    ENUM

--- Untils/UIButton+ImageTitleSpacing.h
+++ Untils/UIButton+ImageTitleSpacing.h
@@ -7,6 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+typedef NS_ENUM(NSUInteger, SLButtonEdgeInsetsStyle) {
+    SLButtonEdgeInsetsStyleTop, // image在上，label在下
+    SLButtonEdgeInsetsStyleLeft, // image在左，label在右
+    SLButtonEdgeInsetsStyleBottom, // image在下，label在上
+    SLButtonEdgeInsetsStyleRight // image在右，label在左
+};
 
 
 @end
