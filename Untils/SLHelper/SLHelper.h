bda58137be9ae868a7ce1d2599fab582e51a3a15
chouYagami <zhouhua@show.one>
2018-06-18 17:04:33 +0800

    Improve SLHelper functionality: time format conversion

--- Untils/SLHelper/SLHelper.h
+++ Untils/SLHelper/SLHelper.h
@@ -122,5 +122,51 @@
  *@param size 限制的最大尺寸
  */
 + (CGSize)sizeForLabelWithString:(NSString *)string withFont:(UIFont *)font constrainedToSize:(CGSize)size;
+
+/**
+ *@brief 保存图片到相册
+ */
 +(NSString *)saveImageDocuments:(UIImage *)image;
+
+#pragma mark 时间格式转换
+/**
+ *@brief 获取当前时间戳，并转化为字符串
+ **/
++(NSString *)getTimeStamp;
+/**
+ *@brief 将时间格式字符串按照format格式转化为需要的时间格式字符串
+ */
++ (NSString *)formatDateWithString:(NSString *)dateString format:(NSString *)format;
+/**
+ *@brief 将date按照format格式转化为字符串
+ */
++ (NSString *)formatDateWithDate:(NSDate *)date format:(NSString *)format;
+/**
+ *@brief 将时间戳按照format格式化为字符串
+ *@param timeInterval 1970开始的时间戳
+ */
++ (NSString *)formatTimeInterval:(NSTimeInterval)timeInterval format:(NSString *)format;
+/**
+ *@brief 将时间格式字符串转化为date
+ */
++ (NSDate *)dateValueWithString:(NSString *)dateStr ByFormatter:(NSString *)formatter;
+/**
+ *@brief 给出date，返回这个时间点是星期几
+ */
++ (NSString *)weekdayStringValue:(NSDate*)date;
+
+/**
+ *@brief 将时间戳转换成时分秒
+ */
++(NSString *)getTimeIntervalWithTime:(NSTimeInterval)timeInterval;
+/**
+ *  时间补0
+ *
+ *  @param str str description
+ *
+ *  @return return value description
+ */
++ (NSString *)fillZeroWithString:(NSString *)str;
+
++(NSString *)getTwoCharTimeIntervalWithTime:(NSInteger)timeInterval formatStr:(NSString *)formatStr;
 @end
