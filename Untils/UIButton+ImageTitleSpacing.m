8fa5e59c66a8b0309f626ba3c743762149e80e3d
Gxshowone <gongxin@show.one>
2018-06-18 16:17:28 +0800

    label. image insets

--- Untils/UIButton+ImageTitleSpacing.m
+++ Untils/UIButton+ImageTitleSpacing.m
@@ -17,6 +17,21 @@
     CGFloat imageWith = self.imageView.frame.size.width;
     CGFloat imageHeight = self.imageView.frame.size.height;
     
+    CGFloat labelWidth = 0.0;
+    CGFloat labelHeight = 0.0;
+    if ([UIDevice currentDevice].systemVersion.floatValue >= 8.0) {
+        // 由于iOS8中titleLabel的size为0，用下面的这种设置
+        labelWidth = self.titleLabel.intrinsicContentSize.width;
+        labelHeight = self.titleLabel.intrinsicContentSize.height;
+    } else {
+        labelWidth = self.titleLabel.frame.size.width;
+        labelHeight = self.titleLabel.frame.size.height;
+    }
+    
+    // 2. 声明全局的imageEdgeInsets和labelEdgeInsets
+    UIEdgeInsets imageEdgeInsets = UIEdgeInsetsZero;
+    UIEdgeInsets labelEdgeInsets = UIEdgeInsetsZero;
+    
   
 }
 
