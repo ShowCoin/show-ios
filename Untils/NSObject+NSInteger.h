464733aa5aa9efc76c409164106e92446d0fb1da
Gxshowone <gongxin@show.one>
2018-06-20 11:21:50 +0800

    integer category

---
+++ Untils/NSObject+NSInteger.h
@@ -0,0 +1,14 @@
+//
+//  NSObject+NSInteger.h
+//  Show
+//
+//  Created by show gx on 16/8/25.
+//  Copyright © show gx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (NSInteger)
+
+  
+@end
