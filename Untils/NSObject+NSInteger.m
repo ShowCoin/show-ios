a5001d9d5856545ddfdd85e863da79f90986b1e6
Gxshowone <gongxin@show.one>
2018-06-20 11:23:28 +0800

    show gx

---
+++ Untils/NSObject+NSInteger.m
@@ -0,0 +1,14 @@
+//
+//  NSObject+NSInteger.m
+//  Show
+//
+//  Created by show  gx on 16/8/25.
+//  Copyright © Show. All rights reserved.
+//
+
+#import "NSObject+NSInteger.h"
+
+@implementation NSObject (NSInteger)
+
+
+@end
