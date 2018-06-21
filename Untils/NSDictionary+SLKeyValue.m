1421b6ffeebf31b9f7c806c2dd3998e1977c561a
Gxshowone <gongxin@show.one>
2018-06-21 13:18:57 +0800

    show

--- Untils/NSDictionary+SLKeyValue.m
+++ Untils/NSDictionary+SLKeyValue.m
@@ -1,24 +1,14 @@
 //
-//  NSDictionary+DLKeyValue.m
-//  Dreamer-ios-client
+//  NSDictionary+SLKeyValue.m
+//  Show
 //
-//  Created by lei on 17/3/27.
-//  Copyright © 2017年 Beijing Dreamer. All rights reserved.
+//  Created by gx on 17/3/27.
+//  Copyright © Show All rights reserved.
 //
 
 #import "NSDictionary+DLKeyValue.h"
 
 @implementation NSDictionary (DLKeyValue)
 
-- (NSString *)dl_safeStringForKey:(id)key
-{
-    id string = [self objectForKey:key];
-    if (string && [string isKindOfClass:NSString.class]) {
-        return string;
-    } else if ([string respondsToSelector:@selector(stringValue)]) {
-        return [string stringValue];
-    }
-    return @"";
-}
 
 @end
