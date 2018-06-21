7147073ef561d64b0f24c63e443591c2945b9b70
Vning <790068808@qq.com>
2018-06-21 22:52:42 +0800

    621-代码整理  超链接Label  手势冲突

--- Common/SLLinkLabel/SLLinkLabel.m
+++ Common/SLLinkLabel/SLLinkLabel.m
@@ -24,36 +24,7 @@
     [super awakeFromNib];
     [self addCopyForLabel];
 }
-#pragma mark -- 可复制
 
-- (void)addCopyForLabel
-{
-    self.userInteractionEnabled = YES;
-    UILongPressGestureRecognizer *longTouch = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longTouchAction:)];
-    longTouch.minimumPressDuration = 1;
-    [self addGestureRecognizer:longTouch];
-}
-
-- (BOOL)canBecomeFirstResponder {
-    return YES;
-}
-
-- (BOOL)canPerformAction:(SEL)action withSender:(id)sender {
-    return action == @selector(copyTextInThisLabel:);
-}
-
-- (void)copyTextInThisLabel:(id)sender {
-    UIPasteboard *pasteBoard = [UIPasteboard generalPasteboard];
-    pasteBoard.string = self.text;
-}
-
-- (void)longTouchAction:(UIGestureRecognizer *)recognizer {
-    [self becomeFirstResponder];
-    UIMenuItem *copyItem = [[UIMenuItem alloc] initWithTitle:@"复制" action:@selector(copyTextInThisLabel:)];
-    [[UIMenuController sharedMenuController] setMenuItems:[NSArray arrayWithObjects:copyItem, nil]];
-    [[UIMenuController sharedMenuController] setTargetRect:self.frame inView:self.superview];
-    [[UIMenuController sharedMenuController] setMenuVisible:YES animated:YES];
-}
 
 
 
