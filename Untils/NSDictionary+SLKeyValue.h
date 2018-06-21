03d747d76a23d3902c7da101cce0f071ae6725f5
Gxshowone <gongxin@show.one>
2018-06-21 13:11:40 +0800

    key value

---
+++ Untils/NSDictionary+SLKeyValue.h
@@ -0,0 +1,15 @@
+//
+//  NSDictionary+SLKeyValue.h
+//  show gx
+//
+//  Created by show gx on 17/3/27.
+//  Copyright © 2017年 Show. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (SLKeyValue)
+
+
+
+@end
