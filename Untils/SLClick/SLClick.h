b54bb5b6060591928d6a085b81cd841b32a70723
chenyh <chenyinghao@show.one>
2018-06-25 17:29:03 +0800

    SLClick add

---
+++ Untils/SLClick/SLClick.h
@@ -0,0 +1,13 @@
+//
+//  SLClick.h
+//  ShowLive
+//
+//  Created by chenyh on 2018/6/22.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLClick : NSObject
+
+@end
