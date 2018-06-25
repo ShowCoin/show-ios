004582745eee9e2a6010b3278e31fe62c56610d8
chouYagami <zhouhua@show.one>
2018-06-25 18:47:49 +0800

    SLHelper method implementation

--- Untils/SLHelper/SLHelper.m
+++ Untils/SLHelper/SLHelper.m
@@ -361,4 +361,20 @@
         return NO;
     }
 }
+
++ (BOOL) isBlankString:(NSString *)string {
+    if (![string isKindOfClass:[NSString class]]) {
+        return YES;
+    }
+    if (string == nil || string == NULL) {
+        return YES;
+    }
+    if ([string isKindOfClass:[NSNull class]]) {
+        return YES;
+    }
+    if ([[string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] length]==0) {
+        return YES;
+    }
+    return NO;
+}
 @end
