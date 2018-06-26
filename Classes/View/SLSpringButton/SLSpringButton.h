782006d7f40e96cebfab8ea18019c1612112b107
chenyh <chenyinghao@show.one>
2018-06-26 20:54:57 +0800

    Spring button add

---
+++ Classes/View/SLSpringButton/SLSpringButton.h
@@ -0,0 +1,13 @@
+//
+//  SLSpringBUtton.h
+//  test
+//
+//  Created by chenyh on 2018/6/26.
+//  Copyright © 2018年 chuxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLSpringButton : UIButton
+
+@end
