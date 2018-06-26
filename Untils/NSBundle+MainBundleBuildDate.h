686752c37a0cb5d7e47a8aaefae7e67833720531
Gxshowone <gongxin@show.one>
2018-06-26 12:24:00 +0800

    top class change

--- Untils/NSBundle+MainBundleBuildDate.h
+++ Untils/NSBundle+MainBundleBuildDate.h
@@ -1,18 +1,14 @@
 //
 //  NSBundle+MainBundleBuildDate.h
-//  Dreamer
+//  Show
 //
-//  Created by Ant on 16/10/21.
-//  Copyright © 2016年 Beijing Dreamer. All rights reserved.
+//  Created by Show on 16/10/21.
+//  Copyright © 2016年 Show. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface NSBundle (MainBundleBuildDate)
 
-/*
- * Retrieves the build date from the main bundle after diverting to the BuildDate.plist.
- */
-+ (NSString *)mainBundleBuildDate;
 
 @end
