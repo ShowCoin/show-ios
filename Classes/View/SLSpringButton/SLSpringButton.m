7f7bbdee6499b4840ec3363f406b9828bb9e5335
chenyh <chenyinghao@show.one>
2018-06-26 20:56:56 +0800

    animations

--- Classes/View/SLSpringButton/SLSpringButton.m
+++ Classes/View/SLSpringButton/SLSpringButton.m
@@ -42,7 +42,11 @@
     // usingSpringWithDamping的范围为0.0f到1.0f，数值越小「弹簧」的振动效果越明显
     // initialSpringVelocity则表示初始的速度，数值越大一开始移动越快
     [UIView animateWithDuration:0.5 delay:0.05 usingSpringWithDamping:0.45 initialSpringVelocity:1.9 options:UIViewAnimationOptionCurveEaseOut animations:^{
-        
+        if (selected) {
+            self.transform = CGAffineTransformIdentity;
+        } else {
+            self.transform = CGAffineTransformScale(CGAffineTransformIdentity, scale, scale);
+        }
     } completion:^(BOOL finished) {
         self.userInteractionEnabled = YES;
     }];
