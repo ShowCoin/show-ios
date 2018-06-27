67d2ff40c2914df72e69f992d6ad551972328420
Gxshowone <gongxin@show.one>
2018-06-27 12:08:29 +0800

    realzation

--- Untils/NSString+SLPictureChoice.m
+++ Untils/NSString+SLPictureChoice.m
@@ -14,7 +14,30 @@
 {
     if (IsStrEmpty(str)) return self;
     
-
+    if (!self) {
+        return nil;
+    }
+    
+    NSArray *array = [self componentsSeparatedByString:@"."];
+    
+    if (!array) return nil;
+    
+    NSString *subUrl = [array lastObject];
+    
+    if ([subUrl isEqualToString:@""]||[subUrl isEqualToString:@"<null>"]|| !subUrl || [subUrl isEqualToString:@"(null)"] || [subUrl isKindOfClass:NSNull.class]) { // 没有后缀 或者 地址为空
+        return nil;
+    }
+    
+    NSRange range = [self rangeOfString:[NSString stringWithFormat:@".%@",subUrl]];
+    NSUInteger avatarlength = range.location + range.length;
+    if (avatarlength>[self length])//加保护 避免越界情况
+    {
+        
+        return self;
+    }
+    
+    return [self stringByReplacingCharactersInRange:range
+                                         withString:[NSString stringWithFormat:@"_%@.%@",str,subUrl]];
     
 }
 
