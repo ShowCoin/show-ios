aee47e792f199c3be90c6bcbdffcc609ae57748f
Gxshowone <gongxin@show.one>
2018-06-29 21:10:41 +0800

    get method

--- Untils/UIButton+EnlargeTouchArea.m
+++ Untils/UIButton+EnlargeTouchArea.m
@@ -23,6 +23,34 @@ static char leftNameKey;
     objc_setAssociatedObject(self, &leftNameKey, [NSNumber numberWithFloat:left], OBJC_ASSOCIATION_COPY_NONATOMIC);
 }
 
+- (CGRect) enlargedRect
+{
+    NSNumber* topEdge = objc_getAssociatedObject(self, &topNameKey);
+    NSNumber* rightEdge = objc_getAssociatedObject(self, &rightNameKey);
+    NSNumber* bottomEdge = objc_getAssociatedObject(self, &bottomNameKey);
+    NSNumber* leftEdge = objc_getAssociatedObject(self, &leftNameKey);
+    if (topEdge && rightEdge && bottomEdge && leftEdge)
+    {
+        return CGRectMake(self.bounds.origin.x - leftEdge.floatValue,
+                          self.bounds.origin.y - topEdge.floatValue,
+                          self.bounds.size.width + leftEdge.floatValue + rightEdge.floatValue,
+                          self.bounds.size.height + topEdge.floatValue + bottomEdge.floatValue);
+    }
+    else
+    {
+        return self.bounds;
+    }
+}
+
+- (UIView*) hitTest:(CGPoint) point withEvent:(UIEvent*) event
+{
+    CGRect rect = [self enlargedRect];
+    if (CGRectEqualToRect(rect, self.bounds))
+    {
+        return [super hitTest:point withEvent:event];
+    }
+    return CGRectContainsPoint(rect, point) ? self : nil;
+}
 
 
 @end
