7b0d32cd7beecff6fb079b54cf357485b04fa675
Gxshowone <gongxin@show.one>
2018-06-29 21:06:48 +0800

    add file

---
+++ Untils/UIButton+EnlargeTouchArea.h
@@ -0,0 +1,15 @@
+//
+//  UIButton+EnlargeTouchArea.h
+//  Dreamer-ios-client
+//
+//  Created by 巩鑫 on 2017/4/28.
+//  Copyright © 2017年 Beijing Dreamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (EnlargeTouchArea)
+
+- (void)setEnlargeEdgeWithTop:(CGFloat) top right:(CGFloat) right bottom:(CGFloat) bottom left:(CGFloat) left;
+
+@end
