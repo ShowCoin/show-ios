2a9cdac40cf8be814b70042b3ae2ccc8d3cc5380
Vning <790068808@qq.com>
2018-06-29 21:10:39 +0800

    完善个人中心

--- Classes/UserCenter/View/setting/ShowForUsView.m
+++ Classes/UserCenter/View/setting/ShowForUsView.m
@@ -37,7 +37,35 @@
     }
     return _versionLabel;
 }
+-(UILabel*)introduceLabel
+{
+    if (!_introduceLabel) {
+        if (@available(iOS 8.2, *)) {
+            _introduceLabel = [UILabel labelWithFrame:CGRectMake(30*Proportion375, _versionLabel.bottom+10*Proportion375, self.width-60*Proportion375, 80*Proportion375) text:@"秀币(ShowCoin)是世界首个基于区块链和智能合约的点对点网络技术基础，针对直播类数字娱乐内容分发服务和产品而设计的数字加密虚拟货币。" textColor:kGrayWith999999 font:[UIFont systemFontOfSize:14*Proportion375 weight:UIFontWeightThin]   backgroundColor:[UIColor clearColor] alignment:NSTextAlignmentLeft];
+        } else {
+            // Fallback on earlier versions
+        }if (@available(iOS 8.2, *)) {
+            _introduceLabel = [UILabel labelWithFrame:CGRectMake(30*Proportion375, _versionLabel.bottom+10*Proportion375, self.width-60*Proportion375, 80*Proportion375) text:@"秀币(ShowCoin)是世界首个基于区块链和智能合约的点对点网络技术基础，针对直播类数字娱乐内容分发服务和产品而设计的数字加密虚拟货币。" textColor:kGrayWith999999 font:[UIFont systemFontOfSize:14*Proportion375 weight:UIFontWeightThin]   backgroundColor:[UIColor clearColor] alignment:NSTextAlignmentLeft];
 
+        } else {
+            // Fallback on earlier versions
+        }
+        _introduceLabel.numberOfLines=3;
+        
+    }
+    return _introduceLabel;
+}
+
+- (UIImageView*)showImage{
+    if (!_showImage) {
+        _showImage = [[UIImageView alloc]initWithFrame:CGRectMake(KScreenWidth/2-70*Proportion375/2, 25*Proportion375, 70*Proportion375, 70*Proportion375)];
+        
+        _showImage.image = [UIImage imageNamed:@"usLogo"];
+        [_showImage roundStyle];
+        _showImage.backgroundColor = [UIColor clearColor];
+    }
+    return _showImage;
+}
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
