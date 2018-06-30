95f8e6608d45fa8b5cfbf3af4b78737ca924a941
Gxshowone <gongxin@show.one>
2018-06-30 12:05:35 +0800

    color category file

---
+++ Untils/UIColor+Category.h
@@ -0,0 +1,17 @@
+//
+//  UIColor+Category.h
+//  kangxiaobai
+//
+//  Created by 武春鹏 on 15/9/2.
+//  Copyright (c) 2015年 kangxiaobai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Category)
+
+//颜色转换
+
++ (UIColor *)customColorWithString:(NSString *)string;
+
+@end
