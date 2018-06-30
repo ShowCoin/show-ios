2ac6b3e646ca963b044a061a0146c933df707a27
Gxshowone <gongxin@show.one>
2018-06-30 12:06:13 +0800

    change something

--- Untils/UIColor+Category.h
+++ Untils/UIColor+Category.h
@@ -1,17 +1,14 @@
 //
 //  UIColor+Category.h
-//  kangxiaobai
+//  show gx
 //
-//  Created by 武春鹏 on 15/9/2.
-//  Copyright (c) 2015年 kangxiaobai. All rights reserved.
+//  Created by show gx on 15/9/2.
+//  Copyright (c) 2015年 Show gx. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface UIColor (Category)
 
-//颜色转换
-
-+ (UIColor *)customColorWithString:(NSString *)string;
 
 @end
