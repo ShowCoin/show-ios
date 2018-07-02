9737f6ca9c0861dceac87e5bd54051b186426367
Gxshowone <gongxin@show.one>
2018-07-02 12:18:42 +0800

    validation nick

--- Untils/NSString+Validation.m
+++ Untils/NSString+Validation.m
@@ -9,6 +9,16 @@
 #import "NSString+Validation.h"
 
 @implementation NSString (Validation)
+/*判断输入的是否是昵称*/
+-(BOOL)isValidNickName;
+{
+    NSString *nicknameRegex = @"^[\u4e00-\u9fa5]{4,8}$";
+    
+    NSPredicate *passWordPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",nicknameRegex];
+    
+    return [passWordPredicate evaluateWithObject:self];
+    
+}
 
 
 @end
