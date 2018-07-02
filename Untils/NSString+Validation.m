7e95f71731ae9c37aaaef3dde392d6e56d5e9eff
Gxshowone <gongxin@show.one>
2018-07-02 12:19:00 +0800

    valid phone

--- Untils/NSString+Validation.m
+++ Untils/NSString+Validation.m
@@ -20,5 +20,14 @@
     
 }
 
+/*判断输入的是否是手机号码*/
+-(BOOL)isValidPhone
+{
+    NSString *stricterFilterString = @"\\b(1)[34578][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]\\b";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", stricterFilterString];
+    return [emailTest evaluateWithObject:self];
+}
+
+
 
 @end
