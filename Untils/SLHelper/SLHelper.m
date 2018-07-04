1f1aee7a405af51e9350c7461c8e491995b0ba04
chouYagami <zhouhua@show.one>
2018-07-04 18:30:13 +0800

    SLHelper method implementation

--- Untils/SLHelper/SLHelper.m
+++ Untils/SLHelper/SLHelper.m
@@ -697,5 +697,84 @@
     
     return object;
 }
++(void)synchronizedToCalendarTitle:(NSString *)title location:(NSString *)location{
+    //事件市场
+    EKEventStore *eventStore = [[EKEventStore alloc] init];
+    //6.0及以上通过下面方式写入事件
+    if ([eventStore respondsToSelector:@selector(requestAccessToEntityType:completion:)])
+    {
+        // the selector is available, so we must be on iOS 6 or newer
+        [eventStore requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError *error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (error)
+                {
+                    
+                    // display error message here
+                }
+                else if (!granted)
+                {
+                    //被用户拒绝，不允许访问日历
+                    // display access denied error message here
+                }
+                else
+                {
+                    // access granted
+                    // ***** do the important stuff here *****
+                    
+                    //事件保存到日历
+                    
+                    
+                    //创建事件
+                    EKEvent *event  = [EKEvent eventWithEventStore:eventStore];
+                    event.title     = title;
+                    event.location = location;
+                    
+                    NSDateFormatter *tempFormatter = [[NSDateFormatter alloc]init];
+                    [tempFormatter setDateFormat:@"dd.MM.yyyy HH:mm"];
+                    
+                    event.startDate = [[NSDate alloc]init ];
+                    event.endDate   = [[NSDate alloc]init ];
+                    event.allDay = YES;
+                    
+                    //添加提醒
+                    [event addAlarm:[EKAlarm alarmWithRelativeOffset:60.0f * -60.0f * 24]];
+                    [event addAlarm:[EKAlarm alarmWithRelativeOffset:60.0f * -15.0f]];
+                    
+                    [event setCalendar:[eventStore defaultCalendarForNewEvents]];
+                    NSError *err;
+                    [eventStore saveEvent:event span:EKSpanThisEvent error:&err];
+                    
+                }
+            });
+        }];
+    }
+    else
+    {
+        //4.0和5.0通过下述方式添加
+        
+        //保存日历
+        EKEvent *event  = [EKEvent eventWithEventStore:eventStore];
+        event.title     = title;
+        event.location = location;
+        
+        NSDateFormatter *tempFormatter = [[NSDateFormatter alloc]init];
+        [tempFormatter setDateFormat:@"dd.MM.yyyy HH:mm"];
+        
+        event.startDate = [[NSDate alloc]init ];
+        event.endDate   = [[NSDate alloc]init ];
+        event.allDay = YES;
+        
+        
+        [event addAlarm:[EKAlarm alarmWithRelativeOffset:60.0f * -60.0f * 24]];
+        [event addAlarm:[EKAlarm alarmWithRelativeOffset:60.0f * -15.0f]];
+        
+        [event setCalendar:[eventStore defaultCalendarForNewEvents]];
+        NSError *err;
+        [eventStore saveEvent:event span:EKSpanThisEvent error:&err];
+        NSLog(@"保存成功");
+        
+    }
+    
+}
 
 @end
