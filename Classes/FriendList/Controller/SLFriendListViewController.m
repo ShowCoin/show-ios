04bd5a7fbe35c3e55a4a5e8dfbd66fe1e08dc44b
Vning <790068808@qq.com>
2018-07-04 19:38:20 +0800

    好友-完成  7-4-6

--- Classes/FriendList/Controller/SLFriendListViewController.m
+++ Classes/FriendList/Controller/SLFriendListViewController.m
@@ -390,5 +390,38 @@
     _vmFrieldList.searchKey=@"";
 }
 
+- (void)searchBarTextDidEndEditing:(SLSearchBar *)searchBar
+{
+    [self searchBarCancelButtonClicked:searchBar];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (UIView *)footView
+{
+    self.tableView.tableFooterView = nil;
+    
+    // footerView
+    CGFloat height = 0;
+    if (self.tableView.contentSize.height >= self.tableView.height) {
+        height = 40;
+    } else {
+        height = MAX(self.tableView.height - self.tableView.contentSize.height, 40);
+    }
+    
+    
+    UIView* footerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kMainScreenWidth, height)];
+    
+    self.lblFriendTotals=[footerView buildLabel:@"0个SHOW好友" withFrame:CGRectMake(0, height - 35,kMainScreenWidth,20) withFont:[UIFont systemFontOfSize:15] withTextColor:RGBCOLOR(156, 158, 171) withTextAlign:NSTextAlignmentCenter];
+    
+    if (self.friendTotal) {
+        self.lblFriendTotals.text = self.friendTotal;
+    }
+    
+    return footerView;
+}
 
 @end
