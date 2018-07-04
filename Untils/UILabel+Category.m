abefd167f46b0e860f004645f3b79ad897297e4c
Gxshowone <gongxin@show.one>
2018-07-04 20:43:35 +0800

    text height

--- Untils/UILabel+Category.m
+++ Untils/UILabel+Category.m
@@ -18,6 +18,19 @@
     return tmpRect.size.width;
 }
 
+//label自适应高度
++ (CGFloat)getLabelHeightWithText:(NSString *)text wordSize:(CGFloat)wordSize width:(CGFloat)width
+{
+    UIFont * fnt = [UIFont fontWithName:TextFontName size:wordSize];
+    
+    CGRect tmpRect = [text boundingRectWithSize:CGSizeMake(width, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin attributes:[NSDictionary dictionaryWithObjectsAndKeys:fnt,NSFontAttributeName, nil] context:nil];
+    return tmpRect.size.height;
+}
+
+
+
+
+
 
 
 
