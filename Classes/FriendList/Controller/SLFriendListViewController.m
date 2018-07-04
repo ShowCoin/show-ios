fa7f377790a831eb655b87ff78a43ffdcda63da4
Vning <790068808@qq.com>
2018-07-04 19:36:53 +0800

    7-4-4  好友数据

--- Classes/FriendList/Controller/SLFriendListViewController.m
+++ Classes/FriendList/Controller/SLFriendListViewController.m
@@ -188,5 +188,38 @@
     }];
 }
 
+- (void)onObserverNotify:(NSNotification*)notification {
+    if ([notification.name isEqualToString:kNotificationFriendRequestCount]) {
+        NSNumber* count=@0;
+        if (!notification.object) {
+            count=[UserDefaultsUtils valueWithKey:kNotificationFriendRequestCount];
+        }
+        else{
+            [UserDefaultsUtils saveValue:notification.object forKey:kNotificationFriendRequestCount];
+            count=notification.object;
+        }
+        
+        if ([count integerValue]>0) {
+            CGSize size=[[count stringValue] sizeWithAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:10]}];
+            size=CGSizeMake(size.width+6*Proportion375, size.height+6*Proportion375);
+            if (size.height<16*Proportion375) {
+                size.height=16*Proportion375;
+            }
+            if (size.width<size.height) {
+                size.width=size.height;
+            }
+            _lblFriendReqCount.text=[count stringValue];
+            _vwFriendReqCount.frame=CGRectMake(kMainScreenWidth-36*Proportion375-size.width-6*Proportion375, 27*Proportion375-3*Proportion375-size.height/2, size.width, size.height);
+            
+            [_vwFriendReqCount.layer setCornerRadius:size.height/2];
+            [_vwFriendReqCount.layer masksToBounds];
+            _lblFriendReqCount.frame=CGRectMake(0, 0, size.width, size.height);
+            [_vwFriendReqCount setHidden:NO];
+        }else{
+            [_vwFriendReqCount setHidden:YES];
+        }
+    }
+}
+
 
 @end
