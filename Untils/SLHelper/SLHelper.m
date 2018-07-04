0ad1ac3311a717f75bb0a6c084b63b5f5cd57d11
chouYagami <zhouhua@show.one>
2018-07-04 18:28:00 +0800

    SLHelper method implementation

--- Untils/SLHelper/SLHelper.m
+++ Untils/SLHelper/SLHelper.m
@@ -653,6 +653,26 @@
     
     return timeStr;
 }
-
++(NSString *)trimright0:(double )param
+{
+    NSString *str = [NSString stringWithFormat:@"%.2lf",param];
+    NSUInteger len = str.length;
+    for (int i = 0; i < len; i++)
+    {
+        if (![str  hasSuffix:@"0"])
+            break;
+        else
+            str = [str substringToIndex:[str length]-1];
+    }
+    if ([str hasSuffix:@"."])//避免像2.0000这样的被解析成2.
+    {
+        //        return [NSString stringWithFormat:@"%@0", str];
+        return  [str substringToIndex:[str length]-1];
+    }
+    else
+    {
+        return str;
+    }
+}
 
 @end
