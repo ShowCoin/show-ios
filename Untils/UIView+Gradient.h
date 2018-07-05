9eca499d3c955e27fc8a920f790434a2259f3044
Gxshowone <gongxin@show.one>
2018-07-05 12:21:48 +0800

    no message

---
+++ Untils/UIView+Gradient.h
@@ -0,0 +1,15 @@
+//
+//  UIView+Gradient.h
+//  show
+//
+//  Created by Show on 16/10/26.
+//  Copyright © 2016年 Beijing show. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Gradient)
+
+
+
+@end
