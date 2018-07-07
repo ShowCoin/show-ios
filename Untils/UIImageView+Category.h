c640461bf6cd3f74dd4cff001e714bcf4481003d
Gxshowone <gongxin@show.one>
2018-07-07 11:57:35 +0800

    gif imageview

--- Untils/UIImageView+Category.h
+++ Untils/UIImageView+Category.h
@@ -13,5 +13,8 @@
 //模糊加黑色蒙层在window 上
 +(UIImageView*)blurImageWithView:(UIView*)view;
 
+//生成gif图       名字  帧数
++(UIImageView*)imageViewWithGifFileName:(NSString*)name gifCount:(NSInteger)giftCount frame:(CGRect)frame;
+
 
 @end
