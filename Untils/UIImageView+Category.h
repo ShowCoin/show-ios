d383ce7c3b43497d8718bacbd109c0b2da35f838
Gxshowone <gongxin@show.one>
2018-07-07 11:57:00 +0800

    rmeove space

--- Untils/UIImageView+Category.h
+++ Untils/UIImageView+Category.h
@@ -10,19 +10,5 @@
 
 @interface UIImageView (Category)
 
-//模糊加黑色蒙层在window 上
-+(UIImageView*)blurImageWithView:(UIView*)view;
-//生成gif图       名字  帧数
-+(UIImageView*)imageViewWithGifFileName:(NSString*)name gifCount:(NSInteger)giftCount frame:(CGRect)frame;
-
-/**
- 序列帧动画生成 imageView
-
- @param imagesArray 序列帧数组
- @param duration 动画时间
- @param frame 坐标
- @return UIImageView
- */
-+(UIImageView*)imageWithAnimationImages:(NSArray*)imagesArray animationDuration:(double)duration frame:(CGRect)frame;
 
 @end
