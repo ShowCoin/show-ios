a1ce23accfef909b28d57b3b57d1472bd961dc4d
Gxshowone <gongxin@show.one>
2018-07-07 11:59:41 +0800

    method real

--- Untils/UIImageView+Category.m
+++ Untils/UIImageView+Category.m
@@ -13,5 +13,23 @@
 @implementation UIImageView (Category)
 
 +(UIImageView*)blurImageWithView:(UIView *)view
+{
+    
+    UIImage * screenImage=[view convertViewToImage];
+    //将图片模糊
+    UIImage * blurImage=[screenImage blurredImageWithRadius:25 iterations:1 tintColor:[UIColor blackColor]];
+    UIImageView * imageView=[[UIImageView alloc]initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight)];
+    imageView.userInteractionEnabled=YES;
+    imageView.image=blurImage;
+    
+    UIView * blackView=[[UIView alloc]initWithFrame:imageView.bounds];
+    blackView.backgroundColor=[UIColor blackColor];
+    blackView.alpha=0.2;
+    [imageView addSubview:blackView];
+    
+    
+    return imageView;
+    
+}
 
 @end
