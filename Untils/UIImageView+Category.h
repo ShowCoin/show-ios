bb7dbc4368a75377d82d0655c1dda241f43b5d04
Gxshowone <gongxin@show.one>
2018-07-07 11:57:49 +0800

    animation iamge

--- Untils/UIImageView+Category.h
+++ Untils/UIImageView+Category.h
@@ -16,5 +16,13 @@
 //生成gif图       名字  帧数
 +(UIImageView*)imageViewWithGifFileName:(NSString*)name gifCount:(NSInteger)giftCount frame:(CGRect)frame;
 
-
+/**
+ 序列帧动画生成 imageView
+ 
+ @param imagesArray 序列帧数组
+ @param duration 动画时间
+ @param frame 坐标
+ @return UIImageView
+ */
++(UIImageView*)imageWithAnimationImages:(NSArray*)imagesArray animationDuration:(double)duration frame:(CGRect)frame;
 @end
