7c0f89cfaacbbeb315e8676e212b1f7f683617ab
chouYagami <zhouhua@show.one>
2018-07-09 17:04:13 +0800

    SLHelper method implementation

--- Untils/SLHelper/SLHelper.m
+++ Untils/SLHelper/SLHelper.m
@@ -987,4 +987,20 @@
     return folderSize/(1024.0*1024.0);
     
 }
+
++ (NSString *) compareCurrentTime:(NSString *)str
+{
+    
+    //把字符串转为NSdate
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy/MM/dd HH:mm"];//:ss.SSS
+    NSDate *timeDate = [dateFormatter dateFromString:str];
+    
+    //得到与当前时间差
+    NSTimeInterval  timeInterval = [timeDate timeIntervalSinceNow];
+    timeInterval = -timeInterval;
+    //标准时间和北京时间差8个小时
+    timeInterval = timeInterval - 8*60*60;
+    long temp = 0;
+    NSString *result;
 @end
