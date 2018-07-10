397053933d6ebc222c3e7c7f3d9895b988a914e7
Gxshowone <gongxin@show.one>
2018-07-10 21:14:45 +0800

    代码整理

--- Untils/NSString+Date.h
+++ Untils/NSString+Date.h
@@ -11,40 +11,4 @@
 @interface NSString (Date)
 
 
-+(NSString *)dateWithIntervale:(long long)interval formateStyle:(NSString*)style;
--(long long)dateStringWithFormateStyle:(NSString*)style;
-
-+ (NSString *)dateTimeDifferenceWithStartTime:(NSString *)startTime endTime:(NSString *)endTime;
-
-
-+ (NSString *)minutesWithStartTime:(NSString *)startTime endTime:(NSString *)endTime;
-
-/**
- 获取时间差
- 
- @param date 时间
- 
- @return 格式化时间字符串
- */
-+ (NSString *)strintFromeDate:(NSDate *)date formate:(NSString *)formate;
-
-
-/**
- 获取当前时间的格式化字符串
- 
- @param date 时间
- 
- @return 格式化时间字符串
- */
-+(NSString*)getNowString;
-
-//获取当前时间戳
-+(NSString *)getNowTimeInterval;
-
-//时间转换时间戳
-+(NSString*)exchageTimeInterval:(NSString*)timeString;
-
-//获取pk当天日期
-+(NSString*)getPKString;
-
 @end
