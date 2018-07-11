c792262613107bc013d2967fa12beea3bdfdd19c
Gxshowone <gongxin@show.one>
2018-07-11 19:49:32 +0800

    return barbuttonitem

--- Untils/UIBarButtonItem+Extension.m
+++ Untils/UIBarButtonItem+Extension.m
@@ -13,6 +13,12 @@
 + (UIBarButtonItem *)itemWithTarget:(id)target action:(SEL)action image:(NSString *)image highImage:(NSString *)highImage
 {
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
-   
+    [btn addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
+    // 设置图片
+    [btn setBackgroundImage:[UIImage imageNamed:image] forState:UIControlStateNormal];
+    [btn setBackgroundImage:[UIImage imageNamed:highImage] forState:UIControlStateHighlighted];
+    // 设置尺寸
+    //btn.size = btn.currentBackgroundImage.size;
+    return [[UIBarButtonItem alloc] initWithCustomView:btn];
 }
 @end
