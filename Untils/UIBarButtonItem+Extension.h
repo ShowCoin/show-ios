07e59adfc00a8e0ee33800667ccb0f8e5609e4cd
Gxshowone <gongxin@show.one>
2018-07-11 19:48:31 +0800

    add method

--- Untils/UIBarButtonItem+Extension.h
+++ Untils/UIBarButtonItem+Extension.h
@@ -9,4 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface UIBarButtonItem (Extension)
++ (UIBarButtonItem *)itemWithTarget:(id)target action:(SEL)action image:(NSString *)image highImage:(NSString *)highImage;
 @end
