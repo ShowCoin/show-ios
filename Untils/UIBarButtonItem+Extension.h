afd086e20922357faa2fb91a2b3ef2362672e692
Gxshowone <gongxin@show.one>
2018-07-11 19:48:59 +0800

    clear title

--- Untils/UIBarButtonItem+Extension.h
+++ Untils/UIBarButtonItem+Extension.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface UIBarButtonItem (Extension)
+
 + (UIBarButtonItem *)itemWithTarget:(id)target action:(SEL)action image:(NSString *)image highImage:(NSString *)highImage;
+
 @end
