a5c08934cc5c7aab73cb379e2617261d4bc67446
chouYagami <zhouhua@show.one>
2018-07-12 14:06:18 +0800

    SLHelper method implementation

--- Untils/SLHelper/SLHelper.m
+++ Untils/SLHelper/SLHelper.m
@@ -1076,5 +1076,20 @@
     }
     return plainText;
 }
++(NSDictionary *)dictionaryWithJsonString:(NSString *)jsonString {
+    if (jsonString == nil)
+    {
+        return nil;
+    }
+    NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *err;
+    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&err];
+    if(err)
+    {
+        NSLog(@"json解析失败：%@",err);
+        return nil;
+    }
+    return dic;
+}
 
 @end
