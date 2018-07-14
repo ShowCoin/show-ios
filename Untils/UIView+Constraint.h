91d034d904304253082b01170434c829a39d3e4d
Gxshowone <gongxin@show.one>
2018-07-14 19:48:50 +0800

    sticky center

--- Untils/UIView+Constraint.h
+++ Untils/UIView+Constraint.h
@@ -20,41 +20,14 @@
 #import <UIKit/UIKit.h>
 
 typedef NS_ENUM(NSUInteger, JQconstraintType) {
+    //     ---------
+    //     |       |
+    //     |   o   |
+    //     |       |
+    //     ---------
+    constraintStickyTypeCenter,//中心点
+    
    
-    
-};
-typedef NS_ENUM(NSUInteger, JQconstraint2ViewType) {
-    //     _________         ________
-    //     |       |         |      |
-    //     |   *---|----0----|---*  |
-    //     |       |         |      |
-    //     ---------         --------
-    constraint2ViewTypeDivideDistance,//中心点距离等分
-    
-    //     _________
-    //     |       |
-    //     |   0   |
-    //     |   |   |
-    //     ---------
-    constraint2ViewTypeLowerEdge,//1view到2view的底边
-    //     _________
-    //     |   |   |
-    //     |   0   |
-    //     |       |
-    //     ---------
-    constraint2ViewTypeUpperEdge,//1view到2view的底边
-    //     _________
-    //     |       |
-    //     |---0   |
-    //     |       |
-    //     ---------
-    constraint2ViewTypeLeftEdge,//1view到2view的左边
-    //     _________
-    //     |       |
-    //     |   0---|
-    //     |       |
-    //     ---------
-    constraint2ViewTypeRightEdge,//1view到2view的右边
     
 };
 
