38a36ef64759bfc0bab57b5981a64f311baf1083
Gxshowone <gongxin@show.one>
2018-07-16 19:36:42 +0800

    add file

---
+++ Untils/UIView + SoftBodyAnimation.h
@@ -0,0 +1,17 @@
+//
+//  UIView + SoftBodyAnimation.h
+//  ButtonAnimation
+//
+//  Created by show on 2017/6/13.
+//  Copyright © 2017年 show. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (SoftBodyAnimation)
+#pragma private property 
+
+
+
+@end
+
