0e2a09d35b0bf341009ccc7a7d7a664ae2fac5af
chouYagami <zhouhua@show.one>
2018-07-16 14:01:49 +0800

    The NSString method extends the implementation

--- Untils/Exten/NSString+Additions.m
+++ Untils/Exten/NSString+Additions.m
@@ -11,5 +11,14 @@
     
     return [self stringByReplacingOccurrencesOfString:string withString:@""];
 }
++ (BOOL)isValidString:(NSString *)value
+{
+    return (value &&
+            (![@"" isEqualToString:value]) &&
+            (value.length > 0) &&
+            (![value isKindOfClass:[NSNull class]]) &&
+            (![@"(null)" isEqualToString:value])) &&
+    (([value isKindOfClass:[NSString class]] && [[value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] length]));
+}
 
 @end
