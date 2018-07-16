2c5c9123c611de97e3a41fce9b032c722a1c6ebe
Vning <790068808@qq.com>
2018-07-16 17:50:06 +0800

    card UI

--- Classes/UserCenter/View/SLUserViewHeader+SLMinCard.m
+++ Classes/UserCenter/View/SLUserViewHeader+SLMinCard.m
@@ -60,4 +60,81 @@
         [self addSubview:self.bottomAnimationLine];
         
         
+        [self.scrollerContentView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.equalTo(self);
+        }];
+        
+        [self.headImgView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.scrollerContentView);
+            make.left.equalTo(self.scrollerContentView);
+            make.width.equalTo(self.scrollerContentView);
+            make.height.equalTo(self.scrollerContentView).with.offset(-40*Proportion375 + KTopHeight);
+        }];
+        
+        [self.effectview mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.headImgView);
+            make.bottom.equalTo(self.headImgView);
+            make.size.mas_equalTo(self.headImgView);
+        }];
+    
+        [self.headPortrait mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.scrollerContentView).with.offset(54*Proportion375 + KTopHeight);
+            make.centerX.equalTo(self.scrollerContentView);
+            make.size.mas_equalTo(CGSizeMake(93*Proportion375, 93*Proportion375));
+        }];
+        
+        [self.nickLab mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.headPortrait.mas_bottom).with.offset(12*Proportion375);
+            make.left.equalTo(self.scrollerContentView);
+            make.size.mas_equalTo(CGSizeMake(kMainScreenWidth, 27*Proportion375));
+        }];
+
+        [self.idLab mas_makeConstraints:^(MASConstraintMaker *make) {
+
+            make.top.equalTo(self.nickLab.mas_bottom).with.offset(6*Proportion375);
+            make.centerX.equalTo(self.scrollerContentView);
+            make.height.equalTo(@(16*Proportion375));
+        }];
+ 
+        [self.masterLevel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.idLab.mas_bottom).with.offset(12*Proportion375);
+            make.right.equalTo(self.mas_centerX).with.offset(-4*Proportion375);
+            make.size.mas_equalTo(CGSizeMake(28*Proportion375, 15*Proportion375));
+        }];
+        [self.showLevel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.idLab.mas_bottom).with.offset(12*Proportion375);
+            make.left.equalTo(self.mas_centerX).with.offset(4*Proportion375);
+            make.size.mas_equalTo(CGSizeMake(28*Proportion375, 15*Proportion375));
+        }];
+        [self.cityLab mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.showLevel.mas_bottom).with.offset(6*Proportion375);
+            make.centerX.equalTo(self.scrollerContentView);
+            make.width.lessThanOrEqualTo(@(100*Proportion375));
+            make.width.greaterThanOrEqualTo(@(50*Proportion375));
+            make.height.equalTo(@(20*Proportion375));
+            
+        }];
+ 
+    return self;
+}
+
+
+
+- (void)adjustMiniCard{
+    self.listBtn.hidden = YES ;
+    self.giftStoreBtn.hidden = YES ;
+    self.settingBtn.hidden = YES;
+    self.shareBtn.hidden = YES ;
+    self.worksBtn.hidden = YES ;
+    self.likesBtn.hidden = YES ;
+    self.leftBtn.hidden = YES ;
+    
+    [self.headImgView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self);
+        make.left.equalTo(self);
+        make.width.equalTo(self);
+        make.height.equalTo(self);
+    }];
+}
+
 @end
