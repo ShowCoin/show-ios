8f249947b7ac333a68f9df597ba0567e7dcdb98b
Gxshowone <gongxin@show.one>
2018-07-17 15:34:04 +0800

    stop motion

--- Untils/UIView + SoftBodyAnimation.m
+++ Untils/UIView + SoftBodyAnimation.m
@@ -23,4 +23,43 @@ static const char *kisScaleXY = "kisScaleXY";
     [self softBodyMotion];
 }
 
+-(void)softBodyMotion{
+    __weak typeof(self) weakSelf = self;
+    [UIView animateWithDuration:1 delay:0 options:UIViewAnimationOptionCurveLinear|UIViewAnimationOptionAllowUserInteraction  animations:^{
+        if(weakSelf.isAnimationZoomX){
+            weakSelf.transform = CGAffineTransformMakeScale(1.04, 0.96);
+        }else{
+            weakSelf.transform = CGAffineTransformMakeScale(0.96, 1.04);
+        }
+        
+    } completion:^(BOOL finished) {
+        if(finished){
+            if(weakSelf.isPlayBodyAnimation == NO){
+                return;
+            }
+            weakSelf.isAnimationZoomX = !self.isAnimationZoomX;
+            [weakSelf softBodyMotion];
+        }
+        
+    }];
+}
+-(void)startScaleMotion{
+    self.isScaleXY  = YES;
+    [self scaleMotion];
+}
+-(void)scaleMotion{
+    __weak typeof(self) weakSelf = self;
+    [UIView animateWithDuration:1 delay:0 options:UIViewAnimationOptionCurveLinear|UIViewAnimationOptionAllowUserInteraction| UIViewAnimationOptionAutoreverse|UIViewAnimationOptionRepeat  animations:^{
+        if(weakSelf.isScaleXY == NO){
+            return;
+        }
+        weakSelf.transform = CGAffineTransformMakeScale(1.03, 1.03);
+    } completion:^(BOOL finished) {
+    }];
+}
+-(void)stopScaleMotion{
+    self.isScaleXY  = NO;
+}
+
+
 @end
