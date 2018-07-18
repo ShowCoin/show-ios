46d39f70f5bbee3b13e9c79f73f8dfdd9a9f8bfc
chenyh <chenyinghao@show.one>
2018-07-18 19:49:10 +0800

    righttextfield

---
+++ Classes/Login/View/SLRightTextField.h
@@ -0,0 +1,20 @@
+//
+//  SLRightTextField.h
+//  test
+//
+//  Created by chenyh on 2018/7/10.
+//  Copyright © 2018年 chuxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+UIKIT_EXTERN CGFloat const kSLRightTextFieldH;
+
+typedef NS_ENUM(NSUInteger, SLFieldRightType) {
+    SLFieldRightTypeNormal,
+    SLFieldRightTypePwd,
+    SLFieldRightTypeQR,
+    SLFieldRightTypeVCode,// phone register
+    SLFieldRightTypeEmailVCode,
+};
+
