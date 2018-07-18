330ce63ff4e7667fd9ee11aa9e4e3f1a1e22530a
Gxshowone <gongxin@show.one>
2018-07-18 14:29:01 +0800

    constraint

---
+++ Untils/UIView+Constraint.m
@@ -0,0 +1,17 @@
+//
+//  UIView+Constraint.m
+//  showgx
+//
+//  Created by showgx on 16/8/14.
+//  Copyright © 2016年 showgx. All rights reserved.
+//
+
+#import "UIView+Constraint.h"
+
+@implementation UIView (Constraint)
+
+- (void)constraintStickyWithView:(UIView*)view orientation:(JQconstraintType)orientation offset:(NSArray*)offsetArray
+{
+    
+}
+@end
