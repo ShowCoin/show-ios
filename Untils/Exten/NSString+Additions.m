c0869ed6060d5d7a3a8becadc5aec0c3206f79bb
chouYagami <zhouhua@show.one>
2018-07-18 14:04:15 +0800

    NSString extension methods

--- Untils/Exten/NSString+Additions.m
+++ Untils/Exten/NSString+Additions.m
@@ -120,6 +120,12 @@
     
     return [phoneTest evaluateWithObject:self];
 }
+- (BOOL)isValidateEmail {
+    
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES%@", emailRegex];
+    return [emailTest evaluateWithObject:self];
+}
 
 
 @end
