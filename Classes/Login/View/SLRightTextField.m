4892d4d6a020b0ea5a7a7b537c74f4f89f4b8e4f
chenyh <chenyinghao@show.one>
2018-07-18 19:50:59 +0800

    .m

---
+++ Classes/Login/View/SLRightTextField.m
@@ -0,0 +1,29 @@
+//
+//  SLRightTextField.m
+//  test
+//
+//  Created by chenyh on 2018/7/10.
+//  Copyright © 2018年 chuxia. All rights reserved.
+//
+
+#import "SLRightTextField.h"
+#import "ShowVerifycodeAction.h"
+
+CGFloat const kSLRightTextFieldH = 50;
+
+@interface SLRightTextField ()
+
+@end
+
+@implementation SLRightTextField {
+    SLVerificationButton *vButton__;
+}
+@end
+
+@interface SLVerificationButton ()
+
+@end
+
+@implementation SLVerificationButton
+
+@end
