f8a69c7a4c0c81a0c18cf2bd3174f90c52a5701b
Gxshowone <gongxin@show.one>
2018-07-19 20:42:33 +0800

    clear lib

--- Untils/UIViewController+XHPhoto.m
+++ Untils/UIViewController+XHPhoto.m
@@ -7,19 +7,6 @@
 //  Copyright © 2017年 showgx. All rights reserved.
 
 #import "UIViewController+XHPhoto.h"
-#import "objc/runtime.h"
-#import <AssetsLibrary/ALAssetsLibrary.h>
-#import "DeviceInfo.h"
-#import <Photos/Photos.h>
-
-#ifdef DEBUG
-#define debugLog(...)    NSLog(__VA_ARGS__)
-#else
-#define debugLog(...)
-#endif
-
-static  BOOL canEdit = YES;
-static  char blockKey;
 
 @interface UIViewController()<UIActionSheetDelegate,UINavigationControllerDelegate, UIImagePickerControllerDelegate,UIAlertViewDelegate>
 
