de98ad0948c5b21fa7e0c7419e6cde4d50751ff8
Gxshowone <gongxin@show.one>
2018-07-20 22:24:47 +0800

    return color

--- Untils/UIColor+color.m
+++ Untils/UIColor+color.m
@@ -12,7 +12,7 @@
     CGFloat r = arc4random_uniform(255.0) / 255.0;
     CGFloat g = arc4random_uniform(255.0) / 255.0;
     CGFloat b = arc4random_uniform(255.0) / 255.0;
-   
+    return [UIColor colorWithRed:r green:g blue:b alpha:1];
 }
 
 @end
