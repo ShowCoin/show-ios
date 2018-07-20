8108610f4766de50f5de32f204bc7f44fd89d56a
Gxshowone <gongxin@show.one>
2018-07-20 22:22:43 +0800

    uicolor

--- Untils/UIColor+color.m
+++ Untils/UIColor+color.m
@@ -7,14 +7,6 @@
 //
 
 #import "UIColor+color.h"
-
 @implementation UIColor (color)
-
-+ (UIColor *)sl_arc4randomColor {
-    CGFloat r = arc4random_uniform(255.0) / 255.0;
-    CGFloat g = arc4random_uniform(255.0) / 255.0;
-    CGFloat b = arc4random_uniform(255.0) / 255.0;
-    return [UIColor colorWithRed:r green:g blue:b alpha:1];
-}
 
 @end
