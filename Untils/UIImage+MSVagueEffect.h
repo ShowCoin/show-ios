a1c69e576e0ce7b79eddc607a44a20dc59c4d1c3
Gxshowone <gongxin@show.one>
2018-07-23 20:55:43 +0800

    title change

--- Untils/UIImage+MSVagueEffect.h
+++ Untils/UIImage+MSVagueEffect.h
@@ -2,20 +2,14 @@
 //  UIImage+MSVagueEffect.h
 //  MyShowFoundation
 //
-//  Created by JingZhongJie on 16/5/13.
-//  Copyright © 2016年 Beijing JinHangZhuoLe Network Technology Co., Ltd. All rights reserved.
+//  Created by show on 16/5/13.
+//  Copyright © 2016年 Beijing show Network Technology Co., Ltd. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface UIImage (MSVagueEffect)
 
-- (UIImage *)applyLightEffect;
-- (UIImage *)applyExtraLightEffect;
-- (UIImage *)applyDarkEffect;
-- (UIImage *)applyClearEffect;
-- (UIImage *)applyClear1Effect;
 
-- (UIImage *)applyTintEffectWithColor:(UIColor *)tintColor;
 
 @end
