b3002c9f1b3ac32b97a6893ee4e0f94a6edeb710
chouYagami <zhouhua@show.one>
2018-07-28 21:04:19 +0800

    NSString extension method implementation

--- Untils/Exten/NSString+Additions.m
+++ Untils/Exten/NSString+Additions.m
@@ -394,6 +394,29 @@
     return frame.size.height;
 }
 
+//data转换为十六进制的string
++ (NSString *)hexStringFromData:(NSData *)myD{
+    
+    Byte *bytes = (Byte *)[myD bytes];
+    //下面是Byte 转换为16进制。
+    NSString *hexStr=@"";
+    for(int i=0;i<[myD length];i++)
+        
+    {
+        NSString *newHexStr = [NSString stringWithFormat:@"%x",bytes[i]&0xff];///16进制数
+        
+        if([newHexStr length]==1)
+            
+            hexStr = [NSString stringWithFormat:@"%@0%@",hexStr,newHexStr];
+        
+        else
+            
+            hexStr = [NSString stringWithFormat:@"%@%@",hexStr,newHexStr];
+    }
+    NSLog(@"hex = %@",hexStr);
+    
+    return hexStr;
+}
 
 
 
