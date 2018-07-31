b6a205fede12af26c2ea092edfb42a73c25e3aa9
Gxshowone <gongxin@show.one>
2018-07-31 14:55:27 +0800

    frame applied affine

--- Untils/UIView+Additions.m
+++ Untils/UIView+Additions.m
@@ -83,6 +83,68 @@
 }
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (CGFloat)centerY {
+    return self.center.y;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)setCenterY:(CGFloat)centerY {
+    self.center = CGPointMake(self.center.x, centerY);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (CGFloat)width {
+    return self.frame.size.width;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)setWidth:(CGFloat)width {
+    CGRect frame = self.frame;
+    frame.size.width = width;
+    self.frame = frame;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (CGFloat)height {
+    return self.frame.size.height;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)setHeight:(CGFloat)height {
+    CGRect frame = self.frame;
+    frame.size.height = height;
+    self.frame = frame;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (CGFloat)ttScreenX {
+    CGFloat x = 0;
+    for (UIView* view = self; view; view = view.superview) {
+        x += view.left;
+    }
+    return x;
+}
+
+
+
+- (CGRect)frameAppliedAffineTransform
+{
+    CGRect frame = CGRectApplyAffineTransform(self.frame, self.transform);
+    
+    return frame;
+}
+
+- (CGFloat)widthAppliedAffineTransform
+{
+    return [self frameAppliedAffineTransform].size.width;
+}
 
 - (CGFloat)heightAppliedAffineTransform
 {
