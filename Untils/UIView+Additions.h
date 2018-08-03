29b27882fa71bc7e54a1fb4d315a3be7ab30e56b
Gxshowone <gongxin@show.one>
2018-08-03 12:21:42 +0800

    remove subview

--- Untils/UIView+Additions.h
+++ Untils/UIView+Additions.h
@@ -22,6 +22,44 @@
  */
 @property (nonatomic) CGFloat top;
 
+/**
+ * Shortcut for frame.origin.x + frame.size.width
+ *
+ * Sets frame.origin.x = right - frame.size.width
+ */
+@property (nonatomic) CGFloat right;
+
+/**
+ * Shortcut for frame.origin.y + frame.size.height
+ *
+ * Sets frame.origin.y = bottom - frame.size.height
+ */
+@property (nonatomic) CGFloat bottom;
+
+/**
+ * Shortcut for frame.size.width
+ *
+ * Sets frame.size.width = width
+ */
+@property (nonatomic) CGFloat width;
+
+/**
+ * Shortcut for frame.size.height
+ *
+ * Sets frame.size.height = height
+ */
+@property (nonatomic) CGFloat height;
+
+/**
+ * Shortcut for center.x
+ *
+ * Sets center.x = centerX
+ */
+
+/**
+ * Removes all subviews.
+ */
+- (void)removeAllSubviews;
 
 
 
