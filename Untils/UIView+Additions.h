0d22d1ab87d9284fb44edfa629b5ad3efe5ec70b
Gxshowone <gongxin@show.one>
2018-08-03 12:22:01 +0800

    self others

--- Untils/UIView+Additions.h
+++ Untils/UIView+Additions.h
@@ -55,6 +55,65 @@
  *
  * Sets center.x = centerX
  */
+@property (nonatomic) CGFloat centerX;
+
+/**
+ * Shortcut for center.y
+ *
+ * Sets center.y = centerY
+ */
+@property (nonatomic) CGFloat centerY;
+
+/**
+ * Return the x coordinate on the screen.
+ */
+@property (nonatomic, readonly) CGFloat ttScreenX;
+
+/**
+ * Return the y coordinate on the screen.
+ */
+@property (nonatomic, readonly) CGFloat ttScreenY;
+
+/**
+ * Return the x coordinate on the screen, taking into account scroll views.
+ */
+@property (nonatomic, readonly) CGFloat screenViewX;
+
+/**
+ * Return the y coordinate on the screen, taking into account scroll views.
+ */
+@property (nonatomic, readonly) CGFloat screenViewY;
+
+/**
+ * Return the view frame on the screen, taking into account scroll views.
+ */
+@property (nonatomic, readonly) CGRect screenFrame;
+
+/**
+ * Shortcut for frame.origin
+ */
+@property (nonatomic) CGPoint origin;
+
+/**
+ * Shortcut for frame.size
+ */
+@property (nonatomic) CGSize size;
+
+/**
+ * Return the width in portrait or the height in landscape.
+ */
+@property (nonatomic, readonly) CGFloat orientationWidth;
+
+/**
+ * Return the height in portrait or the width in landscape.
+ */
+@property (nonatomic, readonly) CGFloat orientationHeight;
+
+/**
+ * Finds the first descendant view (including this view) that is a member of a particular class.
+ */
+- (UIView*)descendantOrSelfWithClass:(Class)cls;
+
 
 /**
  * Removes all subviews.
