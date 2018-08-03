8bd41a272b0ee26e1509c93640ae126e35db83bf
Gxshowone <gongxin@show.one>
2018-08-03 12:19:26 +0800

    add file

---
+++ Untils/UIView+Additions.h
@@ -0,0 +1,10 @@
+//show
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#define UIViewAutoresizingFlexibleAll (UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight)
+
+@interface UIView (TTCategory)
+
+
+@end
