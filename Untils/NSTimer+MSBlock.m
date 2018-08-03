c6f32e6ea8e8c8949ced6ddd6a2d9b6cac9107ed
Gxshowone <gongxin@show.one>
2018-08-03 12:16:34 +0800

    realzation

--- Untils/NSTimer+MSBlock.m
+++ Untils/NSTimer+MSBlock.m
@@ -13,6 +13,12 @@
 +(NSTimer *)scheduledMSTimerWithTimeInterval:(NSTimeInterval)interval
                                        block:(void (^)(void))block
                                      repeats:(BOOL)repeats
-
+{
+    return [self scheduledTimerWithTimeInterval:interval
+                                         target:self
+                                       selector:@selector(blockInvoke:)
+                                       userInfo:[block copy]
+                                        repeats:repeats];
+}
 
 @end
