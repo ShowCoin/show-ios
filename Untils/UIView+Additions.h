5c15e60fc8121e78057776c2bf76c1060b02b35b
Gxshowone <gongxin@show.one>
2018-08-03 12:21:23 +0800

    top

--- Untils/UIView+Additions.h
+++ Untils/UIView+Additions.h
@@ -15,7 +15,36 @@
  */
 @property (nonatomic) CGFloat left;
 
+/**
+ * Shortcut for frame.origin.y
+ *
+ * Sets frame.origin.y = top
+ */
+@property (nonatomic) CGFloat top;
 
+
+
+
+@end
+
+
+@interface UIView (MarkBorderWithRandomColor)
+
+
+/* 你懂得~ 造福人类
+ */
+
+- (void)markBorderWithColor:(UIColor *)color;
+
+/*  mark view's layer border with random color
+ */
+- (void)markBorderWithRandomColor;
+
+/*   mark view's layer border with random color Recursive meam mark the all view tree
+ */
+- (void)markBorderWithRandomColorRecursive;
+
+- (void)markBorderWithColor:(UIColor *)color borderWidth:(CGFloat)borderWidth;
 
 @end
 
