bb2c7066e728d92f44e12c685512f78f2c83410f
Gxshowone <gongxin@show.one>
2018-08-06 12:27:53 +0800

    string range

--- Untils/UITextView+Rect.h
+++ Untils/UITextView+Rect.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface UITextView (Rect)
+- (NSArray<NSValue *>*)getAllCharacterRects;
 
+#pragma mark - 获取textView 每一个字符的frame
+- (CGRect)rectInTextViewWithStringRange:(NSRange)stringRange;
 @end
