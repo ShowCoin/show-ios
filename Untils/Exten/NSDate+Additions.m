0f2f684019b827f5b1d1ce6af8fcaf5017cd47eb
chouYagami <zhouhua@show.one>
2018-08-06 14:28:31 +0800

    NSDate+Additions method implementation

--- Untils/Exten/NSDate+Additions.m
+++ Untils/Exten/NSDate+Additions.m
@@ -45,6 +45,43 @@
     
     return formattedString;
 }
-
+//时间
+-(NSString *)timeDateFormattedString{
+    NSString *formattedString;
+    
+    NSTimeZone *zone = [NSTimeZone systemTimeZone];
+    NSInteger interval = [zone secondsFromGMTForDate:[NSDate date]];
+    NSDate *now = [[NSDate date] dateByAddingTimeInterval:interval];
+    
+    NSCalendar *cal = [NSCalendar currentCalendar];
+    unsigned int unitFlags = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit;
+    NSDateComponents *nowComponents = [cal components:unitFlags fromDate:now];
+    NSDateComponents *components = [cal components:unitFlags fromDate:self];
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyyy-MM-dd hh:mm:ss"];
+    [formatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
+    NSDate *today = [formatter dateFromString:[NSString stringWithFormat:@"%i-%i-%i 00:00:00", nowComponents.year, nowComponents.month, nowComponents.day]];
+    
+    int secDiffInterval = [now timeIntervalSinceDate:self];
+    int minDiffInterval = secDiffInterval / 60;
+    int hourDiffInterval = minDiffInterval / 60;
+    int secDiffIntervalSinceToday = [today timeIntervalSinceDate:self];
+    if (minDiffInterval < 1) {
+        //小于1分钟
+        formattedString = @"刚刚";
+    }
+    else if (hourDiffInterval < 1) {
+        //小于1小时
+        formattedString = [NSString stringWithFormat:@"%i分钟前", minDiffInterval];
+    }else {
+        
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateFormat:@"hh:mm"];
+        formattedString = [dateFormatter stringFromDate:self];
+    }
+    return formattedString;
+    
+}
 
 @end
