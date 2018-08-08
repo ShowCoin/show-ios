b8a8e349764a92f1d1a0f4d3c9da7f9c1fa63c78
chenyh <chenyinghao@show.one>
2018-08-08 13:49:29 +0800

    red packec add

---
+++ Classes/Live/View/SLRedPacketResult/SLRedPacketResultView.h
@@ -0,0 +1,15 @@
+//
+//  SLRedPacketResultView.h
+//  demoPro
+//
+//  Created by chenyh on 2018/7/31.
+//  Copyright © 2018年 chuxia. All rights reserved.
+//
+
+#import "SLBaseModalView.h"
+
+@interface SLRedPacketResultView : SLBaseModalView
+
++ (instancetype)shared;
+
+@end
