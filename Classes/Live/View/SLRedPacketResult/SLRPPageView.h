c8783d579d03ce307a109086baebaffe7b9f8412
chenyh <chenyinghao@show.one>
2018-08-08 13:50:17 +0800

    page view  add

---
+++ Classes/Live/View/SLRedPacketResult/SLRPPageView.h
@@ -0,0 +1,17 @@
+//
+//  SLRPPageView.h
+//  demoPro
+//
+//  Created by chenyh on 2018/8/1.
+//  Copyright © 2018年 chuxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+UIKIT_EXTERN CGFloat const kSLRPPageViewH;
+
+@interface SLRPPageView : UIView
+
+@property (nonatomic, strong) NSDictionary *info;
+
+@end
