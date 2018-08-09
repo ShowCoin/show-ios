dc217505de514b53b8e72480007dab16064cbd49
Gxshowone <gongxin@show.one>
2018-08-09 12:28:20 +0800

    chinese money

--- Untils/NSObject+NSInteger.m
+++ Untils/NSObject+NSInteger.m
@@ -43,23 +43,19 @@
     if (count < 0) {
         count = 0;
     }
- 
+    NSString * str;
+    if (count >= 1000) {
+        NSInteger count1 = count/100;
+        str = [NSString stringWithFormat:@"%ld.%ldk",count1/10,count1%10];
+        
+    }
+    else
+    {
+        str = [NSString stringWithFormat:@"%ld",count];
+    }
+    
+    return str;
 }
 
 +(NSString*)starDiamondFromChinayuan:(NSInteger)num
-{
-    
-    NSInteger star = num*10;
-    NSString * starStr = [NSString stringWithFormat:@"%ld",star];
-    return starStr;
-}
-
-
-+(NSString*)starDiamondFromDollar:(NSInteger)num
-{
-    NSInteger star = num*60;
-    NSString * starStr = [NSString stringWithFormat:@"%ld",star];
-    return starStr;
-    
-}
 @end
