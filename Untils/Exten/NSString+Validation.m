dbd18097933ae767398d5d79724949449faf5a0c
chouYagami <zhouhua@show.one>
2018-08-14 22:01:10 +0800

    NSString+Validation class method implementation

--- Untils/Exten/NSString+Validation.m
+++ Untils/Exten/NSString+Validation.m
@@ -53,4 +53,12 @@
     return [integerTest evaluateWithObject:self];
 }
 
+/*判断是否有效的正整数*/
+-(BOOL)isValidPositiveInteger {
+    NSString *stricterFilterString = @"^[0-9]*[1-9][0-9]*$";
+    NSPredicate *integerTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", stricterFilterString];
+    return [integerTest evaluateWithObject:self];
+}
+
+
 @end
