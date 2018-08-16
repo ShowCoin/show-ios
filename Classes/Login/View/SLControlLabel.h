ef26b95899a178618b23f6600c0a04f8d3560a82
chenyh <chenyinghao@show.one>
2018-08-16 14:49:58 +0800

    control label add

---
+++ Classes/Login/View/SLControlLabel.h
@@ -0,0 +1,15 @@
+//
+//  SLControlLabel.h
+//  Edu
+//
+//  Created by chenyh on 2018/8/15.
+//  Copyright © 2018年 chuxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLControlLabel : UIControl
+
+@property (nonatomic, strong) NSAttributedString *attributedText;
+
+@end
