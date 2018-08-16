298dd35e3f352482706fe5b4cf1428a701d5775f
chenyh <chenyinghao@show.one>
2018-08-16 14:50:15 +0800

    control label methad

---
+++ Classes/Login/View/SLControlLabel.m
@@ -0,0 +1,41 @@
+//
+//  SLControlLabel.m
+//  Edu
+//
+//  Created by chenyh on 2018/8/15.
+//  Copyright © 2018年 chuxia. All rights reserved.
+//
+
+#import "SLControlLabel.h"
+
+@implementation SLControlLabel {
+    UILabel *_textLabel;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        _textLabel = [[UILabel alloc] init];
+        _textLabel.numberOfLines = 0;
+        [self addSubview:_textLabel];
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    _textLabel.frame = self.bounds;
+}
+
+@synthesize attributedText = _attributedText;
+
+- (void)setAttributedText:(NSAttributedString *)attributedText {
+    _attributedText = attributedText;
+    _textLabel.attributedText = attributedText;
+}
+
+- (NSAttributedString *)attributedText {
+    return _textLabel.attributedText;
+}
+
+@end
