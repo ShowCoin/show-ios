b80215413c7170f8429bce091cabddc728a24391
chenyh <chenyinghao@show.one>
2018-08-16 14:52:27 +0800

    SLFuncInvitaAttributedString

--- Classes/Login/View/SLLoginInviteView.m
+++ Classes/Login/View/SLLoginInviteView.m
@@ -13,6 +13,37 @@
 #import "SLGetInviteInfoAction.h"
 #import "SLControlLabel.h"
 
+inline NSAttributedString *SLFuncInvitaAttributedString(NSString *text, NSString *r, BOOL isAlert) {
+    NSString *aText = [NSString stringWithFormat:@"邀请您注册SHOW 的用户名为: %@，使用此邀请码，意味着您在SHOW直播的收入的%@%%定期奖励给 “%@”", text, r, text];
+    NSString *bText = @"。不可更改不可取消。";
+    NSString *fText = [NSString stringWithFormat:@"%@%@", aText, bText];
+    
+    NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
+    style.lineSpacing = 4;
+    
+    UIColor *wColor = isAlert ? [UIColor blackColor] : kGrayWith676767;
+    UIColor *sColor = isAlert ? [UIColor blueColor] : kBlackThemetextColor;
+
+    UIFont *font = isAlert ? [UIFont systemFontOfSize:14] : [UIFont systemFontOfSize:12];
+    
+    NSDictionary *aDict = @{NSFontAttributeName : font,
+                            NSForegroundColorAttributeName : wColor,
+                            NSParagraphStyleAttributeName : style,
+                            };
+    NSMutableAttributedString *aAttr = [[NSMutableAttributedString alloc] initWithString:fText attributes:aDict];
+    
+    NSDictionary *nDict = @{NSFontAttributeName : font,
+                            NSForegroundColorAttributeName : sColor,
+                            NSUnderlineStyleAttributeName : [NSNumber numberWithInteger:NSUnderlineStyleSingle]
+                            };
+    NSUInteger len = text.length + 2;
+    NSUInteger loc = fText.length - bText.length - len;
+    [aAttr addAttributes:nDict range:NSMakeRange(loc, len)];
+    
+    return aAttr;
+}
+
+
 @interface SLLoginInviteView () <UITextFieldDelegate>
 
 @property (nonatomic, strong) UILabel *invLabel;
