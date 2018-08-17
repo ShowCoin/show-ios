02a1d5c18197ae8131a12c1fadcd37ff959f952b
Gxshowone <gongxin@show.one>
2018-08-17 15:04:31 +0800

    creat image

--- Untils/UIImage+Gradient.m
+++ Untils/UIImage+Gradient.m
@@ -2,13 +2,15 @@
 //  UIImage+Gradient.m
 //  SHOW
 //
-//  Created by SHOWGX on 2017/12/18.
-//  Copyright © SHOW. All rights reserved.
+//  Created by show on 2017/12/18.
+//  Copyright © 2017年 show All rights reserved.
 //
 
 #import "UIImage+Gradient.h"
 
 @implementation UIImage (Gradient)
+
+- (UIImage *)createImageWithSize:(CGSize)imageSize gradientColors:(NSArray *)colors percentage:(NSArray *)percents gradientType:(GradientType)gradientType {
 
 
 @end
