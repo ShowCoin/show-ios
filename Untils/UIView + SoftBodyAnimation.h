62c152c8a91677f958c855c9970669b596be1527
Gxshowone <gongxin@show.one>
2018-08-20 21:16:42 +0800

    remove

--- Untils/UIView + SoftBodyAnimation.h
+++ Untils/UIView + SoftBodyAnimation.h
@@ -9,41 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface UIView (SoftBodyAnimation)
-#pragma private property 
 
-
-/*
- 是否开启软体动画
- */
-@property(nonatomic,assign) BOOL isPlayBodyAnimation;
-/*
- 是否正在缩放X轴
- */
-@property(nonatomic,assign) BOOL isAnimationZoomX;
-
-@property(nonatomic,assign) BOOL isScaleXY;
-
-/*
- 开始软体动画
- */
--(void)startSoftBodyAnimation;
-
-/*
- 停止软体动画
- */
--(void)stopSoftBodyAnimation;
-
--(void)touchDownBodyAnimation;
-
-//正常等比放大
--(void)startScaleMotion;
--(void)stopScaleMotion;
-
-// =======================================================
-// 按钮跳动动效
-// =======================================================
-
-- (void)startSoftBodyAnimationWithView:(UIView *)view;
 
 
 @end
