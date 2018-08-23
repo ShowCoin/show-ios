13751c97ab866261d5001e61a982421c85f0831d
Gxshowone <gongxin@show.one>
2018-08-23 21:50:54 +0800

    rgb

--- Untils/UIColor+SLExtend.h
+++ Untils/UIColor+SLExtend.h
@@ -13,19 +13,6 @@
 /**
  *  将字符串颜色（RGB格式）转换成 UIColor
  *
- *  @param rgb RGB颜色格式: 234,234,234 或 #FF66CC
- *
- *  @return 返回 UIColor
- */
-+ (UIColor* _Nonnull)colorWithRGB:(NSString* _Nonnull)rgb;
 
-/**
- *  反回颜色的 RBG 格式字符串，如：#FFFFCC
- *
- *  @param color 颜色对象
- *
- *  @return 颜色字符串
- */
-+ (NSString* _Nonnull)rgbFromColor:(UIColor* _Nonnull)color;
 
 @end
