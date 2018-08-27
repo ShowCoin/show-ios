907eee46a0b7c4f370cfaf25d1ea8eaa0a684cd8
Gxshowone <gongxin@show.one>
2018-08-27 21:07:26 +0800

    add file

---
+++ Untils/UITableView+NIMScrollToBottom.h
@@ -0,0 +1,14 @@
+//
+//  UITableView+NTESScrollToBottom.h
+//  show
+//
+//  Created by show.
+//  Copyright (c) 2015年 show. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (NIMKit)
+
+- (void)nim_scrollToBottom:(BOOL)animation;
+@end
