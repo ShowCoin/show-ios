60b1dcd493cde4ee4fe7cb300b63a8fc81215d9c
Gxshowone <gongxin@show.one>
2018-09-03 21:26:01 +0800

    top name

--- Untils/UIButton+EnlargeTouchArea.m
+++ Untils/UIButton+EnlargeTouchArea.m
@@ -18,9 +18,7 @@ static char leftNameKey;
 - (void) setEnlargeEdgeWithTop:(CGFloat) top right:(CGFloat) right bottom:(CGFloat) bottom left:(CGFloat) left
 {
     objc_setAssociatedObject(self, &topNameKey, [NSNumber numberWithFloat:top], OBJC_ASSOCIATION_COPY_NONATOMIC);
-    objc_setAssociatedObject(self, &rightNameKey, [NSNumber numberWithFloat:right], OBJC_ASSOCIATION_COPY_NONATOMIC);
-    objc_setAssociatedObject(self, &bottomNameKey, [NSNumber numberWithFloat:bottom], OBJC_ASSOCIATION_COPY_NONATOMIC);
-    objc_setAssociatedObject(self, &leftNameKey, [NSNumber numberWithFloat:left], OBJC_ASSOCIATION_COPY_NONATOMIC);
+   
 }
 
 - (CGRect) enlargedRect
