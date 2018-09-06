b01212b87a01dc48a5bb0bba41dc087e6969ef59
Gxshowone <gongxin@show.one>
2018-09-06 20:54:04 +0800

    remove zhushi

--- Untils/UIImage+SLImage.h
+++ Untils/UIImage+SLImage.h
@@ -10,7 +10,7 @@
 #import <UIKit/UIKit.h>
 
 @interface UIImage (DLImage)
-// 高效创建任何背景图
+
 + (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size;
 + (UIImage *)imageWithColor:(UIColor *)color cornerRadius:(CGFloat)cornerRadius;
 + (UIImage *)cornerImageWithColor:(UIColor *)color cornerRadius:(CGFloat)cornerRadius lineWidth:(CGFloat)lineWidth;
