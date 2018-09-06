7914a0de0035fd7524c2f78efda6af2189637fcc
Gxshowone <gongxin@show.one>
2018-09-06 20:52:48 +0800

    title change

--- Untils/UIImage+SLImage.h
+++ Untils/UIImage+SLImage.h
@@ -3,19 +3,13 @@
 //  UIImage+DLImage.h
 //  videoTest
 //
-//  Created by lei on 17/3/14.
-//  Copyright © 2017年 lei. All rights reserved.
+//  Created by show on 17/3/14.
+//  Copyright © 2017年 show. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface UIImage (DLImage)
-// 高效创建任何背景图
-+ (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size;
-+ (UIImage *)imageWithColor:(UIColor *)color cornerRadius:(CGFloat)cornerRadius;
-+ (UIImage *)cornerImageWithColor:(UIColor *)color cornerRadius:(CGFloat)cornerRadius lineWidth:(CGFloat)lineWidth;
-- (UIImage *)createImageByRoundCornerRadius:(CGFloat)radius
-                                    corners:(UIRectCorner)corners
-                                borderWidth:(CGFloat)borderWidth;
+
 
 @end
