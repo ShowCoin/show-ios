046170abf3fb9b21e4ac6a5a365d65ea59dae00b
chenyh <chenyinghao@show.one>
2018-09-11 18:24:08 +0800

    ani add

--- Classes/Login/View/Help/SLLoginHelpButton.m
+++ Classes/Login/View/Help/SLLoginHelpButton.m
@@ -59,5 +59,58 @@
     return HexRGBAlpha(0x242424, 1);
 }
 
+- (void)beginAnimation {
+    [self setTitleColor:HexRGBAlpha(0x0c0c0c, 1) forState:UIControlStateNormal];
+    self.backgroundColor = HexRGBAlpha(0xf7f7f7, 1);
+    
+    CABasicAnimation *tcolorAni = [CABasicAnimation animationWithKeyPath:@"titleLabel.textColor"];
+    tcolorAni.fromValue = (id)HexRGBAlpha(0x676767, 1).CGColor;
+    tcolorAni.toValue = (id)HexRGBAlpha(0x0c0c0c, 1).CGColor;
+    
+    CABasicAnimation *scaleAni = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAni.fromValue = @1;
+    scaleAni.toValue = @1.2;
+    scaleAni.duration = 0.5;
+    scaleAni.repeatCount = 2;
+    scaleAni.autoreverses = YES;
+    scaleAni.removedOnCompletion = NO;
+    scaleAni.fillMode = kCAFillModeForwards;
+    scaleAni.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+    
+    [self.layer addAnimation:scaleAni forKey:nil];
+    
+//    CABasicAnimation *colorAni = [CABasicAnimation animationWithKeyPath:@"backgroundColor"];
+//    colorAni.fromValue = (id)[self blackColor].CGColor;
+//    colorAni.toValue = (id)[self highlightedColor].CGColor;
+//    
+//    CABasicAnimation *scaleAni = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+//    scaleAni.fromValue = @1;
+//    scaleAni.toValue = @1.1;
+//    
+//    CAAnimationGroup *group = [CAAnimationGroup animation];
+//    group.animations = @[colorAni, scaleAni];
+//    group.duration = 0.5;
+//    group.repeatCount = 2;
+//    group.removedOnCompletion = NO;
+//    group.autoreverses = YES;
+//    group.fillMode = kCAFillModeForwards;
+//    group.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+//    self.delegate = [[SLLHBAniDelegate alloc] init];
+//    __weak typeof (self) wself = self;
+//    self.delegate.aniblock = ^{
+//        [wself doColorAni];
+//    };
+//    group.delegate = self.delegate;
+//    [self.layer addAnimation:group forKey:nil];
+}
+
+- (void)doColorAni {
+    CABasicAnimation *colorAni = [CABasicAnimation animationWithKeyPath:@"backgroundColor"];
+    colorAni.fromValue = (id)[self blackColor].CGColor;
+    colorAni.toValue = (id)[self highlightedColor].CGColor;
+    colorAni.removedOnCompletion = NO;
+    colorAni.fillMode = kCAFillModeForwards;
+    [self.layer addAnimation:colorAni forKey:nil];
+}
 
 @end
