a99317d2764dc648bb911286e815270eb1d7709e
chenyh <chenyinghao@show.one>
2018-09-11 18:23:01 +0800

    help button

---
+++ Classes/Login/View/Help/SLLoginHelpButton.h
@@ -0,0 +1,15 @@
+//
+//  SLLoginHightButton.h
+//  Edu
+//
+//  Created by chenyh on 2018/9/7.
+//  Copyright © 2018年 chuxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLLoginHelpButton : UIButton
+
+- (void)beginAnimation;
+
+@end
