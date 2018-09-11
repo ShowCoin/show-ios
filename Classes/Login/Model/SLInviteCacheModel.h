4c2e489447412aade833d80a70e47f42da625f26
chenyh <chenyinghao@show.one>
2018-09-11 18:16:30 +0800

    inv cache model

---
+++ Classes/Login/Model/SLInviteCacheModel.h
@@ -0,0 +1,14 @@
+//
+//  SLInviteCacheModel.h
+//  ShowLive
+//
+//  Created by chenyh on 2018/9/7.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@interface SLInviteCacheModel : BaseModel
+
+
+@end
