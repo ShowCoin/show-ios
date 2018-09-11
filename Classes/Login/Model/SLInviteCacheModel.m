f5197907bc3edfae0114daa81f1957a3e648b52f
chenyh <chenyinghao@show.one>
2018-09-11 18:17:56 +0800

    cache mode .m add

---
+++ Classes/Login/Model/SLInviteCacheModel.m
@@ -0,0 +1,19 @@
+//
+//  SLInviteCacheModel.m
+//  ShowLive
+//
+//  Created by chenyh on 2018/9/7.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "SLInviteCacheModel.h"
+
+static NSString * const kSLInviteCacheModelKey = @"kSLInviteCacheModelKey";
+
+@interface SLInviteCacheModel ()
+
+@end
+
+@implementation SLInviteCacheModel
+
+@end
