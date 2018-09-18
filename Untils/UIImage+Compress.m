1064c5b43eb500871090967159bf90de5112126e
Gxshowone <gongxin@show.one>
2018-09-18 20:55:37 +0800

    image by scaling

---
+++ Untils/UIImage+Compress.m
@@ -0,0 +1,19 @@
+//
+//  compress.m
+//  show
+//
+//  Created by show coin on 13-7-16.
+//  Copyright (c) show. All rights reserved.
+//
+
+#import "UIImage+Compress.h"
+#import <Accelerate/Accelerate.h>
+
+
+@implementation UIImage (Compress)
+//图像尺寸压缩
+- (UIImage*)imageByScalingAndCroppingForSize:(CGSize)targetSize
+
+
+@end
+
