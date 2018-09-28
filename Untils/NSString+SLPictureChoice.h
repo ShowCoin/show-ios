92cf56713f922ded34f539631efdfa53a1eabe6d
Gxshowone <gongxin@show.one>
2018-09-28 21:14:36 +0800

    图片大小裁剪 函数

--- Untils/NSString+SLPictureChoice.h
+++ Untils/NSString+SLPictureChoice.h
@@ -10,13 +10,6 @@
 
 @interface NSString (SLPictureChoice)
 
-/**
- 根据需求大小获取图片资源代码
- 
- @param str 图片大小 比如 头像100-100  最新封面324-324  大厅关注封面800-800;
- 
- @return 拼接好的字符串
- */
 - (NSString *)stringWithSizeString:(NSString *)str;
 
 @end
