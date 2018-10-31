9bd61ee120e947c346223b4ea31021d372d92f56
Gxshowone <gongxin@show.one>
2018-10-31 15:47:17 +0800

    systemVersion

--- Untils/JKCategories/UIScreen+JKFrame.m
+++ Untils/JKCategories/UIScreen+JKFrame.m
@@ -33,6 +33,7 @@
     
     CGFloat systemVersion = [[[UIDevice currentDevice] systemVersion]
                              doubleValue];
+    
     BOOL isLand =   UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation);
     return (systemVersion>8.0 && isLand) ? jk_SizeSWAP([UIScreen jk_size]) : [UIScreen jk_size];
 }
