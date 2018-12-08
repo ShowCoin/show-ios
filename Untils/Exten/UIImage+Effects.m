9d2c393437988bbf0c569ac228ab586b9ac6bcd3
chouYagami <zhouhua@show.one>
2018-12-08 13:49:51 +0800

    Add method implementation

--- Untils/Exten/UIImage+Effects.m
+++ Untils/Exten/UIImage+Effects.m
@@ -231,6 +231,28 @@
 - (UIImage *)imageByApplyingTintEffectWithColor:(UIColor *)tintColor radius:(CGFloat)radius{
     return [self imageByApplyingBlurToImage:self withRadius:radius tintColor:tintColor saturationDeltaFactor:1.8 maskImage:nil];
 }
+#pragma mark -
+#pragma mark - Implementation
 
+//| ----------------------------------------------------------------------------
+//! Applies a blur, tint color, and saturation adjustment to @a inputImage,
+//! optionally within the area specified by @a maskImage.
+//!
+//! @param  inputImage
+//!         The source image.  A modified copy of this image will be returned.
+//! @param  blurRadius
+//!         The radius of the blur in points.
+//! @param  tintColor
+//!         An optional UIColor object that is uniformly blended with the
+//!         result of the blur and saturation operations.  The alpha channel
+//!         of this color determines how strong the tint is.
+//! @param  saturationDeltaFactor
+//!         A value of 1.0 produces no change in the resulting image.  Values
+//!         less than 1.0 will desaturation the resulting image while values
+//!         greater than 1.0 will have the opposite effect.
+//! @param  maskImage
+//!         If specified, @a inputImage is only modified in the area(s) defined
+//!         by this mask.  This must be an image mask or it must meet the
+//!         requirements of the mask parameter of CGContextClipToMask.
 @end
 
