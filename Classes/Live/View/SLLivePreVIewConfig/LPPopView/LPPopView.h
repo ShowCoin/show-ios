43f216404db958191acd3be9366ec9e55b5d8d32
chenyh <chenyinghao@show.one>
2018-12-10 21:15:53 +0800

    pop

---
+++ Classes/Live/View/SLLivePreVIewConfig/LPPopView/LPPopView.h
@@ -0,0 +1,18 @@
+//
+//  LPPopView.h
+//  Edu
+//
+//  Created by chenyh on 2018/9/20.
+//  Copyright © 2018年 chuxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LPConfigHeader.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LPPopView : LPView <LPAnimationProtocol>
+
+@end
+
+NS_ASSUME_NONNULL_END
