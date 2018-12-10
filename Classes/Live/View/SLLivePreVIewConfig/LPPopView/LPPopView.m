bebf1b421e80bfe5574a0c1aa3de4ebd03f659f9
chenyh <chenyinghao@show.one>
2018-12-10 21:16:13 +0800

    .m

---
+++ Classes/Live/View/SLLivePreVIewConfig/LPPopView/LPPopView.m
@@ -0,0 +1,27 @@
+//
+//  LPPopView.m
+//  Edu
+//
+//  Created by chenyh on 2018/9/20.
+//  Copyright © 2018年 chuxia. All rights reserved.
+//
+
+#import "LPPopView.h"
+
+@implementation LPPopView
+
+@synthesize animating = _animating;
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor clearColor];
+        self.alpha = 0;
+        self.layer.anchorPoint = CGPointMake(0.5, 0);        
+    }
+    return self;
+}
+
+
+@end
