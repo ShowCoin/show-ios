e675af9a3248ba5e64ebbebbad4357a926b6997e
chouYagami <zhouhua@show.one>
2018-12-11 14:03:28 +0800

    UIColor+HexString method implementation

--- Untils/Exten/UIColor+HexString.m
+++ Untils/Exten/UIColor+HexString.m
@@ -51,4 +51,19 @@
     }
     return [UIColor colorWithRed: red green: green blue: blue alpha: alpha];
 }
+
+- (NSString *)hexString
+{
+    const CGFloat *components = CGColorGetComponents(self.CGColor);
+    
+    CGFloat r = components[0];
+    CGFloat g = components[1];
+    CGFloat b = components[2];
+    
+    return [NSString stringWithFormat:@"#%02lX%02lX%02lX",
+            lroundf(r * 255),
+            lroundf(g * 255),
+            lroundf(b * 255)];
+}
+
 @end
