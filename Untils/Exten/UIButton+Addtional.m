2f7a3efdbbeb030454f0ba5495f621a90338d50e
chouYagami <zhouhua@show.one>
2018-12-11 14:05:47 +0800

    UIButton+Addtional method modification

--- Untils/Exten/UIButton+Addtional.m
+++ Untils/Exten/UIButton+Addtional.m
@@ -38,7 +38,19 @@
     button.backgroundColor = backgroundColor;
     return button;
 }
-
+- (void)xr_setButtonImageWithUrl:(NSString *)urlStr {
+    NSURL * url = [NSURL URLWithString:urlStr];
+    // 根据图片的url下载图片数据
+    dispatch_queue_t xrQueue = dispatch_queue_create("loadImage", NULL); // 创建GCD线程队列
+    dispatch_async(xrQueue, ^{
+        // 异步下载图片
+        UIImage * img = [UIImage imageWithData:[NSData dataWithContentsOfURL:url]];
+        // 主线程刷新UI
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self setImage:img forState:UIControlStateNormal];
+        });
+    });
+}
 + (instancetype)buttonWithImage:(UIImage *)image frame:(CGRect)frame{
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
     [btn setImage:image forState:UIControlStateNormal];
