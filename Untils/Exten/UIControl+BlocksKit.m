96b1998909926fda4dd4aefbf83233b80fd74bcf
chouYagami <zhouhua@show.one>
2018-12-12 16:58:25 +0800

    add UIControl+BlocksKit.m

---
+++ Untils/Exten/UIControl+BlocksKit.m
@@ -0,0 +1,37 @@
+//
+//  UIControl+BlocksKit.m
+//  BlocksKit
+//
+
+#import <objc/runtime.h>
+#import "UIControl+BlocksKit.h"
+
+static const void *BKControlHandlersKey = &BKControlHandlersKey;
+
+#pragma mark Private
+
+@interface BKControlWrapper : NSObject <NSCopying>
+
+- (id)initWithHandler:(void (^)(id sender))handler forControlEvents:(UIControlEvents)controlEvents;
+
+@property (nonatomic) UIControlEvents controlEvents;
+@property (nonatomic, copy) void (^handler)(id sender);
+
+@end
+
+@implementation BKControlWrapper
+
+- (id)initWithHandler:(void (^)(id sender))handler forControlEvents:(UIControlEvents)controlEvents
+{
+	self = [super init];
+	if (!self) return nil;
+
+	self.handler = handler;
+	self.controlEvents = controlEvents;
+
+	return self;
+}
+
+
+
+@end
