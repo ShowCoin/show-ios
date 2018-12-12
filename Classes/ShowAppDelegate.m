600d81c502e279e0e7d1b88de398bb5459eb87fd
chenyh <chenyinghao@show.one>
2018-12-12 22:07:00 +0800

    x

--- Classes/ShowAppDelegate.m
+++ Classes/ShowAppDelegate.m
@@ -46,7 +46,7 @@
 /**
  desp add
 
- @param application <#application description#>
+ @param application UIApplication 
  */
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
