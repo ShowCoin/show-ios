e56e72ed371574b1f8fdda48179373800148649c
Gxshowone <gongxin@show.one>
2018-12-13 14:31:27 +0800

    regi push

--- Untils/JKCategories/NSData+JKHash.h
+++ Untils/JKCategories/NSData+JKHash.h
@@ -26,5 +26,36 @@
  */
 @property (readonly) NSData *jk_sha512Data;
 
-
+/**
+ *  @brief  md5 NSData
+ *
+ *  @param key 密钥
+ *
+ *  @return 结果
+ */
+- (NSData *)jk_hmacMD5DataWithKey:(NSData *)key;
+/**
+ *  @brief  sha1Data NSData
+ *
+ *  @param key 密钥
+ *
+ *  @return 结果
+ */
+- (NSData *)jk_hmacSHA1DataWithKey:(NSData *)key;
+/**
+ *  @brief  sha256Data NSData
+ *
+ *  @param key 密钥
+ *
+ *  @return 结果
+ */
+- (NSData *)jk_hmacSHA256DataWithKey:(NSData *)key;
+/**
+ *  @brief  sha512Data NSData
+ *
+ *  @param key 密钥
+ *
+ *  @return 结果
+ */
+- (NSData *)jk_hmacSHA512DataWithKey:(NSData *)key;
 @end
