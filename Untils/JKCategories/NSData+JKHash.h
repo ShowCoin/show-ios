90459c430e825e8894b52c0a2a30ef814d52c3bd
Gxshowone <gongxin@show.one>
2018-12-13 14:29:29 +0800

    md5 data

--- Untils/JKCategories/NSData+JKHash.h
+++ Untils/JKCategories/NSData+JKHash.h
@@ -13,49 +13,5 @@
  *  @brief  md5 NSData
  */
 @property (readonly) NSData *jk_md5Data;
-/**
- *  @brief  sha1Data NSData
- */
-@property (readonly) NSData *jk_sha1Data;
-/**
- *  @brief  sha256Data NSData
- */
-@property (readonly) NSData *jk_sha256Data;
-/**
- *  @brief  sha512Data NSData
- */
-@property (readonly) NSData *jk_sha512Data;
 
-/**
- *  @brief  md5 NSData
- *
- *  @param key 密钥
- *
- *  @return 结果
- */
-- (NSData *)jk_hmacMD5DataWithKey:(NSData *)key;
-/**
- *  @brief  sha1Data NSData
- *
- *  @param key 密钥
- *
- *  @return 结果
- */
-- (NSData *)jk_hmacSHA1DataWithKey:(NSData *)key;
-/**
- *  @brief  sha256Data NSData
- *
- *  @param key 密钥
- *
- *  @return 结果
- */
-- (NSData *)jk_hmacSHA256DataWithKey:(NSData *)key;
-/**
- *  @brief  sha512Data NSData
- *
- *  @param key 密钥
- *
- *  @return 结果
- */
-- (NSData *)jk_hmacSHA512DataWithKey:(NSData *)key;
 @end
