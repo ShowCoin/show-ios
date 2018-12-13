9d986d3dcca82e6a23285dbb33a65a52144c076a
chenyh <chenyinghao@show.one>
2018-12-13 21:10:10 +0800

    layoutsubviews

--- Classes/Live/View/SLLivePreVIewConfig/LPCoverView/LPCoverView.m
+++ Classes/Live/View/SLLivePreVIewConfig/LPCoverView/LPCoverView.m
@@ -60,4 +60,70 @@ static CGFloat const kLPUserViewWH = 17;
     return self;
 }
 
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    CGFloat w = CGRectGetWidth(self.frame);
+    CGFloat h = CGRectGetHeight(self.frame);
+    
+    self.imageView.frame = self.bounds;
+    
+    self.addButton.bounds = CGRectMake(0, 0, w, 100);
+    self.addButton.center = CGPointMake(w / 2, h / 4);
+    
+    CGFloat leftMargin   = 10;
+    CGFloat bottomMargin = 10;
+    
+    CGFloat viewX = 0;
+    CGFloat viewH = 0;
+    CGFloat viewW = 0;
+    CGFloat viewY = 0;
+    
+    viewW = 80;//LPGetLabelWidth(self.preCoverButton.titleLabel) + 8;
+    viewY = 100;
+    viewX = (w - viewW) / 2;
+    viewH = 30;//self.preCoverButton.titleLabel.font.lineHeight + 6;
+    self.preCoverButton.frame = CGRectMake(viewX, viewY, viewW, viewH);
+    
+    CGRect userF = self.userView.frame;
+    userF.origin.x = leftMargin;
+    userF.origin.y = h - bottomMargin - userF.size.height - (isScale ? KTabbarSafeBottomMargin : 0);
+    self.userView.frame = userF;
+    
+    viewX = CGRectGetMaxX(self.userView.frame) + 5;
+    viewY = CGRectGetMinY(self.userView.frame);
+    viewW = w - viewX - leftMargin;
+    viewH = userF.size.height;
+    self.nameLabel.frame = CGRectMake(viewX, viewY, viewW, viewH);
+    
+    viewX = leftMargin;
+    viewW = w - 6;
+    if (isScale) {
+        viewW = w - leftMargin * 3 - 70;
+    }
+    viewH = SLFuncGetTextHeightFromLabel(self.textLabel, viewW);
+    CGFloat kMaxH = self.textLabel.font.lineHeight * 3;
+    if (viewH > kMaxH) {
+        viewH = kMaxH;
+    }
+    viewY = CGRectGetMinY(self.nameLabel.frame) - kMaxH - 5;
+    self.textLabel.frame = CGRectMake(viewX, viewY, viewW, viewH);
+    
+    CGFloat customMargin = self.ratioView.viewH * 1.5;
+    viewX = leftMargin;
+    viewW = LPGetLabelWidth(self.ratioView.textLabel) + customMargin;
+    viewH = self.ratioView.viewH;
+    viewY = CGRectGetMinY(self.textLabel.frame) - viewH - 5;
+    if (self.textLabel.frame.size.height == 0) {
+        viewY = CGRectGetMinY(self.nameLabel.frame) - viewH - 5;
+    }
+    self.ratioView.frame = CGRectMake(viewX, viewY, viewW, viewH);
+    
+    viewX = leftMargin;
+    viewW = LPGetLabelWidth(self.showView.textLabel) + customMargin;
+    viewH = CGRectGetHeight(self.ratioView.frame);
+    viewY = CGRectGetMinY(self.ratioView.frame) - viewH - 5;
+    self.showView.frame = CGRectMake(viewX, viewY, viewW, viewH);
+}
+
 @end
