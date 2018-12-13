9319ff03e777b037c5a527805aa511ba20a6ead1
chouYagami <zhouhua@show.one>
2018-12-13 21:16:35 +0800

    UIImage+Additional method implementation

--- Untils/Exten/UIImage+Additional.m
+++ Untils/Exten/UIImage+Additional.m
@@ -21,6 +21,30 @@ CGRect swapWidthAndHeight(CGRect rect)
 }
 
 @implementation UIImage (Additional)
++ (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size
+
+{
+    
+    @autoreleasepool {
+        
+        CGRect rect = CGRectMake(0, 0, size.width, size.height);
+        
+        UIGraphicsBeginImageContext(rect.size);
+        
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        
+        CGContextSetFillColorWithColor(context,
+                                       
+                                       color.CGColor);
+        
+        CGContextFillRect(context, rect);
+        
+        UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+        
+        UIGraphicsEndImageContext();
+        return img;
+    }
+}
 
 
 
