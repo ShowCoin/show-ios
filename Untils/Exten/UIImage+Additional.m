effdfbeb15ec4c2d8c6a9e59edc37c6d869f1b6c
chouYagami <zhouhua@show.one>
2018-12-14 22:21:42 +0800

    UIImage+Additional method implementation

--- Untils/Exten/UIImage+Additional.m
+++ Untils/Exten/UIImage+Additional.m
@@ -415,4 +415,14 @@ CGRect swapWidthAndHeight(CGRect rect)
     
     return copy;
 }
+
++(UIImage*) OriginImage:(UIImage *)image scaleToSize:(CGSize)size
+{
+    UIGraphicsBeginImageContext(size);  //size 为CGSize类型，即你所需要的图片尺寸
+    [image drawInRect:CGRectMake(0, 0, size.width, size.height)];
+    UIImage* scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return scaledImage;   //返回的就是已经改变的图片
+}
+
 @end
