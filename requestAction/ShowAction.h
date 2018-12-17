c254e461fed6724166c6aa635a0599788f84c4ce
Gxshowone <gongxin@show.one>
2018-12-17 20:13:15 +0800

    model class

--- requestAction/ShowAction.h
+++ requestAction/ShowAction.h
@@ -16,7 +16,14 @@ typedef void(^ShowActionFailedBlock)(NSError *error);
 typedef void(^ShowActionCancelledBlock)(void);
 
 @interface ShowAction : NSObject
-
+/**
+ * 创建并返回一个action
+ */
 + (instancetype)action;
+/**
+ *根据是否传入modelClass来决定返回类型
+ */
+@property(nonatomic,strong) Class modelClass ;
+
 
 @end
