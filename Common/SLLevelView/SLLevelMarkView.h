9c265a4aefc97b6fe07dcc16ba3c102b0cc1d659
Gxshowone <gongxin@show.one>
2018-12-18 18:43:04 +0800

    type d

--- Common/SLLevelView/SLLevelMarkView.h
+++ Common/SLLevelView/SLLevelMarkView.h
@@ -7,17 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-typedef enum : NSUInteger {
-    LevelType_ShowCoin,        //  财富等级
-    LevelType_Host,        //  主播等级
-} LevelType;
 
-@interface SLLevelMarkView : UIView
-//等级类型
-@property (nonatomic , assign) LevelType type;
-//等级
-@property (nonatomic , copy) NSString * level;
-//根据类型初始化
-- (id)initWithFrame:(CGRect)frame withType:(LevelType)type;
 
 @end
