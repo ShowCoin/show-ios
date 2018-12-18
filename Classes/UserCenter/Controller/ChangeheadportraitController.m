9373a5001aff7e8657f0ed33bba1cc37f0e6b393
Vning <790068808@qq.com>
2018-12-18 18:35:22 +0800

    拍照

--- Classes/UserCenter/Controller/ChangeheadportraitController.m
+++ Classes/UserCenter/Controller/ChangeheadportraitController.m
@@ -220,6 +220,116 @@
 //        [HDHud hideHUDInView:self.view];
     }];
 }
+- (void)openCamera
+{
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+        if (authStatus == AVAuthorizationStatusRestricted || authStatus ==AVAuthorizationStatusDenied)
+        {
+            UIAlertController *alertController =  [UIAlertController alertControllerWithTitle:@"提示" message:@"请在设备的设置-隐私-相机中允许访问相机! 若不允许，您将无法在SHOW中拍摄视频和照片" preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction *settingAction = [UIAlertAction actionWithTitle:@"设置" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+            }];
+            UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            }];
+            [alertController addAction:settingAction];
+            [alertController addAction:cancelAction];
+            [self presentViewController:alertController animated:YES completion:^{}];
+            
+        }else{
+            SLTakingViewController * tak=[SLTakingViewController initVC];
+            tak.isHead = YES;
+            [self.navigationController pushViewController:tak animated:YES];
+
+//            UIImagePickerController *imgpicker = [[UIImagePickerController alloc]init];
+//            imgpicker.sourceType=UIImagePickerControllerSourceTypeCamera;
+//            imgpicker.delegate = self;
+//            imgpicker.cameraDevice=UIImagePickerControllerCameraDeviceRear;
+//            imgpicker.navigationBar.backgroundColor = kthemeBlackColor;
+//            imgpicker.navigationBar.barTintColor = kthemeBlackColor;
+//            imgpicker.navigationBar.translucent=YES;
+//
+//            [self presentViewController:imgpicker animated:YES completion:nil];
+        }
+    }
+}
+
+- (void)openAlbum
+{
+//    [self.photoSelect startPhotoSelect:YHEPhotoSelectFromLibrary];
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    imagePicker.navigationBar.backgroundColor = kBlackWith17;
+    imagePicker.navigationBar.barTintColor = kBlackWith17;
+    imagePicker.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName:[UIColor whiteColor]}; // Title color
+    imagePicker.navigationBar.translucent=YES;
+    imagePicker.delegate = self;
+    if ([imagePicker.navigationBar respondsToSelector:@selector(setBarTintColor:)]) {
+        [imagePicker.navigationBar setTranslucent:YES];
+        [imagePicker.navigationBar setTintColor:kTextWithF7];
+    }
+    [self presentViewController:imagePicker animated:YES completion:^{
+        
+    }];
+
+//    SLImagePickerControllerV *imagePickController = [[SLImagePickerControllerV alloc] initWithMaxImagesCount:1 delegate:self];
+//    [self presentViewController:imagePickController animated:YES completion:nil];
+}
+#pragma mark - Delegates
+
+#pragma mark - TZImagePickerControllerDelegate
+- (void)imagePickerController:(TZImagePickerController *)picker didFinishPickingPhotos:(NSArray<UIImage *> *)photos sourceAssets:(NSArray *)assets isSelectOriginalPhoto:(BOOL)isSelectOriginalPhoto{
+    UIImage *image =photos.firstObject;
+    
+        NSData * imageData = UIImageJPEGRepresentation(image, 1);
+        if (image.size.width >720&&imageData.length/1000>100) {
+            self.uploadImage = image;
+            [self supportRetryUploadAvatar];
+        }
+        else
+        {
+            [ShowWaringView waringView:@"头像尺寸不符合规则,请重新选择" style:WaringStyleRed];
+            self.uploadImage = [UIImage imageNamed:@"userhome_avatar_image"];
+        }
+
+}
+- (void)notificationHandler: (NSNotification *)notification {
+    self.uploadSmallImage = notification.object[0];
+    NSData * imageData = UIImageJPEGRepresentation(self.uploadSmallImage, 1);
+    NSString * format = [SLHelper contentTypeForImageData:imageData];
+    if (format) {
+        [self supportRetryUploadAvatar];
+        [self.headerBtn setImage:self.uploadSmallImage];
+    }
+}
+
+- (void)dissmissPickerAction:(NSNotification *)notification{
+    [self.Imgpicker dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - UIImagePickerControllerDelegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    image = [UIImage fixOrientation:[info objectForKey:UIImagePickerControllerOriginalImage]];
+    
+    NSData * imageData = UIImageJPEGRepresentation(image, 1);
+    if (image.size.width >=720&&image.size.height >=1280&&imageData.length/1000>100) {
+        self.uploadImage = image;
+        SLCropImageViewController *cropImg = [[SLCropImageViewController alloc] initWithCropImage:image withShoot:NO];
+        self.Imgpicker = picker;
+        [self.Imgpicker presentViewController:cropImg animated:YES completion:^{
+            [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(dissmissPickerAction:) name:@"DISSMISSPICKER" object:nil];
+        }];
+    }
+    else
+    {
+        UIAlertView *alertview = [[UIAlertView alloc]initWithTitle:@"" message:@"欢迎上传高清的头像作为直播的封面，高清的头像才能开播哦。" delegate:self cancelButtonTitle:@"知道了" otherButtonTitles: nil];
+        [alertview show];
+    }
+
+}
 /*
 #pragma mark - Navigation
 
