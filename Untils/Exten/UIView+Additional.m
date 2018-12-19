5188badc1862a207df42987651c4be2a11578236
chouYagami <zhouhua@show.one>
2018-12-19 19:58:39 +0800

    UIView+Additional Method implementation

--- Untils/Exten/UIView+Additional.m
+++ Untils/Exten/UIView+Additional.m
@@ -18,5 +18,47 @@
     self.layer.shadowOpacity = 1;
     self.layer.rasterizationScale = kScreenScale;
 }
+-(void)addDefaultShadow1
+{
+    self.layer.shadowColor = [UIColor grayColor].CGColor;//shadowColor阴影颜色
+    self.layer.shadowOffset = CGSizeMake(0,0);//shadowOffset阴影偏移，默认(0, -3),这个跟shadowRadius配合使用
+    self.layer.shadowOpacity = 1;//阴影透明度，默认0
+    self.layer.shadowRadius = 3;//阴影半径，默认3
+    
+    //路径阴影
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    
+    float width = self.bounds.size.width;
+    float height = self.bounds.size.height;
+    float x = self.bounds.origin.x;
+    float y = self.bounds.origin.y;
+    float addWH = 10;
+    
+    CGPoint topLeft      = self.bounds.origin;
+    CGPoint topMiddle = CGPointMake(x+(width/2),y-addWH);
+    CGPoint topRight     = CGPointMake(x+width,y);
+    
+    CGPoint rightMiddle = CGPointMake(x+width+addWH,y+(height/2));
+    
+    CGPoint bottomRight  = CGPointMake(x+width,y+height);
+    CGPoint bottomMiddle = CGPointMake(x+(width/2),y+height+addWH);
+    CGPoint bottomLeft   = CGPointMake(x,y+height);
+    
+    
+    CGPoint leftMiddle = CGPointMake(x-addWH,y+(height/2));
+    
+    [path moveToPoint:topLeft];
+    //添加四个二元曲线
+    [path addQuadCurveToPoint:topRight
+                 controlPoint:topMiddle];
+    [path addQuadCurveToPoint:bottomRight
+                 controlPoint:rightMiddle];
+    [path addQuadCurveToPoint:bottomLeft
+                 controlPoint:bottomMiddle];
+    [path addQuadCurveToPoint:topLeft
+                 controlPoint:leftMiddle];
+    //设置阴影路径
+    self.layer.shadowPath = path.CGPath;
+}
 
 @end
