dc1bdff179130c390f3f9d5aeb07c2e13a825ee7
Gxshowone <gongxin@show.one>
2018-12-20 12:33:08 +0800

    time label

--- Common/SLTimerLabel/SLTimerLabel.h
+++ Common/SLTimerLabel/SLTimerLabel.h
@@ -13,6 +13,21 @@ typedef enum{
     SLTimerLabelTypeTimer
 }SLTimerLabelType;
 
+/**********************************************
+ Delegate Methods
+ @optional
+ 
+ - timerLabel:finshedCountDownTimerWithTimeWithTime:
+ ** SLTimerLabel Delegate method for finish of countdown timer
+ 
+ - timerLabel:countingTo:timertype:
+ ** SLTimerLabel Delegate method for monitering the current counting progress
+ 
+ - timerlabel:customTextToDisplayAtTime:
+ ** SLTimerLabel Delegate method for overriding the text displaying at the time, implement this for your very custom display formmat
+ **********************************************/
+
+@class SLTimerLabel;
 
 
 @end
