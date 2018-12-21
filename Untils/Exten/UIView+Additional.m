b507fdc57c4d219161284ef58935f6fa471e1ab3
chouYagami <zhouhua@show.one>
2018-12-21 21:14:20 +0800

    UIView+Additional method implementation

--- Untils/Exten/UIView+Additional.m
+++ Untils/Exten/UIView+Additional.m
@@ -259,5 +259,13 @@
 }
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)setSize:(CGSize)size {
+    CGRect frame = self.frame;
+    frame.size = size;
+    self.frame = frame;
+}
+
+
 
 @end
