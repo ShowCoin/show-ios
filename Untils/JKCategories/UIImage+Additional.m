fbdd7f5f10f99813c09bd6e84fea74a1dcd6e762
chouYagami <zhouhua@show.one>
2018-12-26 20:43:00 +0800

    UIImage+Additional method implementation

--- Untils/JKCategories/UIImage+Additional.m
+++ Untils/JKCategories/UIImage+Additional.m
@@ -46,6 +46,16 @@ CGRect swapWidthAndHeight(CGRect rect)
     }
 }
 
+-(UIImage *)resizableImageExtendWithCapInsets:(UIEdgeInsets)capInsets{
+    UIImage *newImage = nil;
+    if ([self respondsToSelector:@selector(resizableImageWithCapInsets:)]) {
+        newImage = [self resizableImageWithCapInsets:capInsets];
+    } else {
+        newImage = [self stretchableImageWithLeftCapWidth:capInsets.left topCapHeight:capInsets.top];
+    }
+    
+    return newImage;
+}
 
 
 @end
