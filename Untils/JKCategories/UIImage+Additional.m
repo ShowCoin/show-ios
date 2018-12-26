ea7e37842e971bd49c16baeee5f84e186f4c026d
chouYagami <zhouhua@show.one>
2018-12-26 20:43:35 +0800

    UIImage+Additional method implementation

--- Untils/JKCategories/UIImage+Additional.m
+++ Untils/JKCategories/UIImage+Additional.m
@@ -77,4 +77,20 @@ CGRect swapWidthAndHeight(CGRect rect)
 }
 
 
+
+- (UIImage*)scaleToSize:(CGSize)size {
+    UIGraphicsBeginImageContext(size);
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextTranslateCTM(context, 0.0, size.height);
+    CGContextScaleCTM(context, 1.0, -1.0);
+    
+    CGContextDrawImage(context, CGRectMake(0.0f, 0.0f, size.width, size.height), self.CGImage);
+    UIImage* scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return scaledImage;
+}
+
+
 @end
