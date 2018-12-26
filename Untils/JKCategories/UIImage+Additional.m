ba0e1f99be8c5639a403358c511fb8b14cc0ffed
chouYagami <zhouhua@show.one>
2018-12-26 20:43:15 +0800

    UIImage+Additional method implementation

--- Untils/JKCategories/UIImage+Additional.m
+++ Untils/JKCategories/UIImage+Additional.m
@@ -57,5 +57,24 @@ CGRect swapWidthAndHeight(CGRect rect)
     return newImage;
 }
 
+//对图片尺寸进行压缩--
+-(UIImage*)imageWithImage:(UIImage*)image scaledToSize:(CGSize)newSize{
+    // Create a graphics image context
+    UIGraphicsBeginImageContext(newSize);
+    
+    // Tell the old image to draw in this new context, with the desired
+    // new size
+    [image drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
+    
+    // Get the new image from the context
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    // End the context
+    UIGraphicsEndImageContext();
+    
+    // Return the new image.
+    return newImage;
+}
+
 
 @end
