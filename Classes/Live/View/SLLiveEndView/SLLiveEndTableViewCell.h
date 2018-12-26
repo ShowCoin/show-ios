c83daadeb8a5748ad74f691bc4c0c23b6c30e09f
chenyh <chenyinghao@show.one>
2018-12-26 20:58:21 +0800

    cell

---
+++ Classes/Live/View/SLLiveEndView/SLLiveEndTableViewCell.h
@@ -0,0 +1,20 @@
+//
+//  SLLiveEndTableViewCell.h
+//  ShowLive
+//
+//  Created by gongxin on 2018/9/28.
+//  Copyright © 2018 vning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SLLiveEndTableViewCell : UITableViewCell
+
+@property(nonatomic,assign)NSInteger line;
+@property(nonatomic,copy)NSString * info;
+
+@end
+
+NS_ASSUME_NONNULL_END
