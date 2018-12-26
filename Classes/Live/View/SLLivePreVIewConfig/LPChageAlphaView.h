64388e4a1716be11d6505b06af6af3af4790a102
chenyh <chenyinghao@show.one>
2018-12-26 21:00:07 +0800

    alpha

---
+++ Classes/Live/View/SLLivePreVIewConfig/LPChageAlphaView.h
@@ -0,0 +1,17 @@
+//
+//  LPChageAlphaView.h
+//  ShowLive
+//
+//  Created by chenyh on 2018/9/26.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LPChageAlphaView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
