18bf67c510ccfece89d5f732d9f17a87952047b5
chenyh <chenyinghao@show.one>
2018-12-26 20:56:32 +0800

    live end add

---
+++ Classes/Live/View/SLLiveEndView/SLLiveEvaluateView.h
@@ -0,0 +1,21 @@
+//
+//  SLLiveEvaluateView.h
+//  ShowLive
+//
+//  Created by gongxin on 2018/9/28.
+//  Copyright © 2018 vning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SLLiveEvaluateView : UITableView
+
+@property (nonatomic, strong) NSMutableArray * dataArray;
+
++ (CGFloat)viewH;
+
+@end
+
+NS_ASSUME_NONNULL_END
