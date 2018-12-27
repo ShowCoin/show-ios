576731a9c7e36577659679dddae8a9cb4b46b3df
Gxshowone <gongxin@show.one>
2018-12-27 14:22:25 +0800

    no message

--- requestAction/ShowAction.h
+++ requestAction/ShowAction.h
@@ -67,5 +67,42 @@ typedef void(^ShowActionCancelledBlock)(void);
  */
 - (NSString *)interface;
 
+/**
+ 数据解析，如果子类需要特殊的处理返回数据的话请重写这个方法
+ 
+ @param respDic 返回的数据字典
+ */
+- (void)parseModel:(NSDictionary *)respDic;
+/**
+ 抛出返回值
+ 
+ @param result 返回值
+ */
+- (void)throwResult:(id)result;
+/**
+ 抛出错误
+ 
+ @param error 返回的错误
+ */
+- (void)throwError:(NSError *)error;
+/**
+ 操作取消
+ */
+- (void)throwCancel;
 
+/**
+ 请求，对原来的请求的封住，可以用原来的方式进行请求，也可以用这种简便的方式进行
+ 
+ @param sucess 请求成功
+ @param faild 请求失败
+ */
+- (void)startRequestSucess:(ShowActionFinishedBlock)sucess FaildBlock:(ShowActionFailedBlock)faild;
+/**
+ 请求，对原来的请求的封住，可以用原来的方式进行请求，也可以用这种简便的方式进行
+ 
+ @param sucess 请求成功
+ @param faild 请求失败
+ @param cancelled 请求终止
+ */
+- (void)startRequestSucess:(ShowActionFinishedBlock)sucess FaildBlock:(ShowActionFailedBlock)faild cancellBlock:(ShowActionCancelledBlock)cancelled;
 @end
