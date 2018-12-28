854f29e5b08ad712831973ffe04fd566c8055a8f
chouYagami <zhouhua@show.one>
2018-12-28 13:36:26 +0800

    UIImage+Additional Method implementation

--- Untils/JKCategories/UIImage+Additional.m
+++ Untils/JKCategories/UIImage+Additional.m
@@ -646,4 +646,24 @@ CGRect swapWidthAndHeight(CGRect rect)
 }
 
 
+-(NSData *)compressMidQualityWithLengthLimit:(NSInteger)maxLength{
+    CGFloat compression = 1;
+    NSData *data = UIImageJPEGRepresentation(self, compression);
+    if (data.length < maxLength) return data;
+    CGFloat max = 1;
+    CGFloat min = 0;
+    for (int i = 0; i < 6; ++i) {
+        compression = (max + min) / 2;
+        data = UIImageJPEGRepresentation(self, compression);
+        if (data.length < maxLength * 0.9) {
+            min = compression;
+        } else if (data.length > maxLength) {
+            max = compression;
+        } else {
+            break;
+        }
+    }
+    return data;
+}
+
 @end
