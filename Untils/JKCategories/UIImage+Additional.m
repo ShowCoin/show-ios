43762b54e3bedc3e152dad9b666e13c3ed757ba3
chouYagami <zhouhua@show.one>
2018-12-28 13:34:37 +0800

    UIImage+Additional Method implementation

--- Untils/JKCategories/UIImage+Additional.m
+++ Untils/JKCategories/UIImage+Additional.m
@@ -488,5 +488,56 @@ CGRect swapWidthAndHeight(CGRect rect)
     
     return [UIImage imageWithCIImage:ciimage];
 }
+#pragma mark - 截屏
++ (UIImage *)screenshot {
+    
+    CGSize imageSize = CGSizeZero;
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    if (UIInterfaceOrientationIsPortrait(orientation))
+    {
+        imageSize = [UIScreen mainScreen].bounds.size;
+        
+    } else
+    {
+        imageSize = CGSizeMake([UIScreen mainScreen].bounds.size.height, [UIScreen mainScreen].bounds.size.width);
+    }
+    UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    for (UIWindow *window in [[UIApplication sharedApplication] windows])
+    {
+        CGContextSaveGState(context);
+        CGContextTranslateCTM(context, window.center.x, window.center.y);
+        CGContextConcatCTM(context, window.transform);
+        CGContextTranslateCTM(context, -window.bounds.size.width * window.layer.anchorPoint.x, -window.bounds.size.height * window.layer.anchorPoint.y);
+        if (orientation == UIInterfaceOrientationLandscapeLeft)
+        { CGContextRotateCTM(context, M_PI_2);
+            CGContextTranslateCTM(context, 0, -imageSize.width);
+            
+        } else if (orientation == UIInterfaceOrientationLandscapeRight)
+        {
+            CGContextRotateCTM(context, -M_PI_2);
+            CGContextTranslateCTM(context, -imageSize.height, 0);
+            
+        } else if (orientation == UIInterfaceOrientationPortraitUpsideDown)
+        {
+            CGContextRotateCTM(context, M_PI);
+            CGContextTranslateCTM(context, -imageSize.width, -imageSize.height);
+            
+        }
+        if ([window respondsToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)])
+        {
+            [window drawViewHierarchyInRect:window.bounds afterScreenUpdates:YES];
+            
+        } else {
+            [window.layer renderInContext:context];
+            
+        }
+        CGContextRestoreGState(context);
+        
+    }
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext(); return image;
+    
+}
 
 @end
