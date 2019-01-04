6778ae76109aeb4bf74feac3d2e6ee25cc977b4d
YagamiChow <zhouhua@show.one>
2019-01-04 14:29:39 +0800

    NSString+Validation method is complete

--- Untils/Exten/NSString+Validation.m
+++ Untils/Exten/NSString+Validation.m
@@ -155,32 +155,7 @@
     NSInteger chineseNum = 0;
     NSInteger zifuNum = 0;
     
-    for (int i = 0; i<[self length]; i++) {
-        //截取字符串中的每一个字符
-        NSString *s = [self substringWithRange:NSMakeRange(i, 1)];
-        if ([self validateChineseChar:s])
-        {
-            if (length + 2 > index)
-            {
-                return [self substringToIndex:chineseNum + zifuNum];
-            }
-            
-            length +=2;
-            
-            chineseNum +=1;
-        }
-        else
-        {
-            if (length +1 >index)
-            {
-                return [self substringToIndex:chineseNum + zifuNum];
-            }
-            length+=1;
-            
-            zifuNum +=1;
-        }
-    }
-    return [self substringToIndex:index];
+   
 }
 //检测中文或者中文符号
 - (BOOL)validateChineseChar:(NSString *)string
