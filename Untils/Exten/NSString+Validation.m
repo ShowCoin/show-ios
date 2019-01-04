68ad4a560401b34c0233658f30772f19ffbd6c37
YagamiChow <zhouhua@show.one>
2019-01-04 14:30:50 +0800

    NSString+Validation method is complete

--- Untils/Exten/NSString+Validation.m
+++ Untils/Exten/NSString+Validation.m
@@ -134,15 +134,7 @@
     NSInteger length = 0;
     for (int i = 0; i<[self length]; i++) {
         //截取字符串中的每一个字符
-        NSString *s = [self substringWithRange:NSMakeRange(i, 1)];
-        if ([self validateChineseChar:s]) {
-            
-            NSLog(@" s 打印信息:%@",s);
-            
-            length +=2;
-        }else{
-            length +=1;
-        }
+       
         
         NSLog(@" 打印信息:%@  %ld",s,(long)length);
     }
