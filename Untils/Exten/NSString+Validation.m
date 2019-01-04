9c93eeb9394a94105eee4ab2c9ff6f4af9daf338
YagamiChow <zhouhua@show.one>
2019-01-04 14:32:46 +0800

    NSString+Validation method is complete

--- Untils/Exten/NSString+Validation.m
+++ Untils/Exten/NSString+Validation.m
@@ -119,7 +119,12 @@
     NSMutableString *string = [NSMutableString stringWithString:num];
     NSMutableString *newstring = [NSMutableString string];
     while (count > 3) {
-        
+        count -= 3;
+        NSRange rang = NSMakeRange(string.length - 3, 3);
+        NSString *str = [string substringWithRange:rang];
+        [newstring insertString:str atIndex:0];
+        [newstring insertString:@"," atIndex:0];
+        [string deleteCharactersInRange:rang];
     }
     [newstring insertString:string atIndex:0];
     return newstring;
