d4b1f4c954359a0ae78e2c97afe322b9e45c2ef2
Gxshowone <gongxin@show.one>
2019-01-09 12:32:55 +0800

    equal color

--- Common/SLLinkLabel/UIColor+Equalable.m
+++ Common/SLLinkLabel/UIColor+Equalable.m
@@ -12,26 +12,7 @@
 @implementation UIColor (Equalable)
 
 - (BOOL)isEqualToColor:(UIColor *)otherColor {
-    CGColorSpaceRef colorSpaceRGB = CGColorSpaceCreateDeviceRGB();
-    
-    UIColor *(^convertColorToRGBSpace)(UIColor*) = ^(UIColor *color) {
-        if(CGColorSpaceGetModel(CGColorGetColorSpace(color.CGColor)) == kCGColorSpaceModelMonochrome) {
-            const CGFloat *oldComponents = CGColorGetComponents(color.CGColor);
-            CGFloat components[4] = {oldComponents[0], oldComponents[0], oldComponents[0], oldComponents[1]};
-            CGColorRef colorRef = CGColorCreate( colorSpaceRGB, components );
-            
-            UIColor *color = [UIColor colorWithCGColor:colorRef];
-            CGColorRelease(colorRef);
-            return color;
-        } else
-            return color;
-    };
-    
-    UIColor *selfColor = convertColorToRGBSpace(self);
-    otherColor = convertColorToRGBSpace(otherColor);
-    CGColorSpaceRelease(colorSpaceRGB);
-    
-    return [selfColor isEqual:otherColor];
+   
 }
 
 @end
