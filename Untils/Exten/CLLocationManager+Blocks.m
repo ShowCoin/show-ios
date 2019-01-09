264fd5ab0c76536aa0169161cbd286ef055a0767
YagamiChow <zhouhua@show.one>
2019-01-09 17:51:10 +0800

    CLLocationManager+blocks method implementation

--- Untils/Exten/CLLocationManager+Blocks.m
+++ Untils/Exten/CLLocationManager+Blocks.m
@@ -35,7 +35,6 @@ CLLocationAgeFilter const kCLLocationAgeFilterNone = 0.0;
 @property (nonatomic, assign) CLLocationAgeFilter updateLocationAgeFilter;
 @property (nonatomic, assign) CLLocationUpdateAuthorizationDescription authorizationDescription;
 
-@property (nonatomic, copy) LocationManagerUpdateBlock updateBlock;
 @property (nonatomic, copy) HeadingUpdateBlock headingUpdateBlock;
 
 @property (nonatomic, copy) DidUpdateLocationsBlock didUpdateLocationsBlock;
