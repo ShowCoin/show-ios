c235aa0785eb4a094b3f88c02c5df33f0860e812
YagamiChow <zhouhua@show.one>
2019-01-09 18:04:11 +0800

    CLLocationManager+blocks method implementation

--- Untils/Exten/CLLocationManager+Blocks.m
+++ Untils/Exten/CLLocationManager+Blocks.m
@@ -48,7 +48,7 @@ CLLocationAgeFilter const kCLLocationAgeFilterNone = 0.0;
 @property (nonatomic, copy) DidExitRegionBlock didExitRegionBlock;
 @property (nonatomic, copy) DidFailWithErrorBlock didFailWithErrorBlock;
 @property (nonatomic, copy) MonitoringDidFailForRegionWithBlock monitoringDidFailForRegionWithBlock;
-@property (nonatomic, copy) DidStartMonitoringForRegionWithBlock didStartMonitoringForRegionWithBlock;
+@property (nonatomic, copy) DidChangeAuthorizationStatusBlock didChangeAuthorizationStatusBlock;
 @property (nonatomic, copy) LocationManagerDidPauseLocationUpdatesBlock locationManagerDidPauseLocationUpdatesBlock;
 @property (nonatomic, copy) LocationManagerDidResumeLocationUpdatesBlock locationManagerDidResumeLocationUpdatesBlock;
 @property (nonatomic, copy) DidFinishDeferredUpdatesWithErrorBlock didFinishDeferredUpdatesWithErrorBlock;
