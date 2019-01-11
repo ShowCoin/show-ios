ac4f1a9026dd4c4560b39a8dd8a80504f91e25e3
Gxshowone <gongxin@show.one>
2019-01-11 12:35:29 +0800

    chat

--- Classes/Chat/singleChat/SLPrivateChatViewController.h
+++ Classes/Chat/singleChat/SLPrivateChatViewController.h
@@ -7,27 +7,6 @@
 //
 
 #import "BaseViewController.h"
-#import "ShowUserModel.h"
 
-static inline CGFloat _GetChatInputViewHeight(void){
-    return 50;
-}
-
-static inline CGFloat _GetChatInputMoreCardHeight(void){
-    return 154;
-}
-
-
-@interface SLPrivateChatViewController : BaseViewController
-/// Public 二者设置一个，即更新了另一个
-@property (nonatomic, strong) NSString *targetUid;
-//事件用户
-@property (nonatomic, strong) ShowUserModel *targetUser;
-//是否是活跃聊天室
-@property (nonatomic, assign) BOOL isLiveChatRoom;
-//是否系统消息
-@property (nonatomic, assign) BOOL isSYSTEMMessage;
-//基类
-@property(nonatomic,strong)BaseViewController * Controller;
 
 @end
