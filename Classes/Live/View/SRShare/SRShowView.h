8dcb00d13508a5736ae005047e3a9f2ae138b779
chenyh <chenyinghao@show.one>
2019-01-17 20:12:13 +0800

    SRShowView

---
+++ Classes/Live/View/SRShare/SRShowView.h
@@ -0,0 +1,19 @@
+//
+//  SRShowView.h
+//  ShowLive
+//
+//  Created by chenyh on 2019/1/15.
+//  Copyright © 2019 vning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SRShowView : UIView
+
+@property (nonatomic, strong) NSString *text;
+
+@end
+
+NS_ASSUME_NONNULL_END
