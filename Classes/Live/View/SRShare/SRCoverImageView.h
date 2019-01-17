e3f288d672b9169adefb47a01a541144f7c9734e
chenyh <chenyinghao@show.one>
2019-01-17 20:11:37 +0800

    SRCorverView

---
+++ Classes/Live/View/SRShare/SRCoverImageView.h
@@ -0,0 +1,19 @@
+//
+//  SRCoverImageView.h
+//  ShowLive
+//
+//  Created by chenyh on 2019/1/15.
+//  Copyright © 2019 vning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SRCoverImageView : UIImageView
+
+@property (nonatomic, assign) CGFloat corverAlpha;
+
+@end
+
+NS_ASSUME_NONNULL_END
