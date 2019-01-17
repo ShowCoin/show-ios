7f1eb525e0f82546ce74496ff646333f240e2609
chenyh <chenyinghao@show.one>
2019-01-17 20:12:42 +0800

    SRCorverView .m add

---
+++ Classes/Live/View/SRShare/SRCoverImageView.m
@@ -0,0 +1,46 @@
+//
+//  SRCoverImageView.m
+//  ShowLive
+//
+//  Created by chenyh on 2019/1/15.
+//  Copyright © 2019 vning. All rights reserved.
+//
+
+#import "SRCoverImageView.h"
+
+@interface SRCoverImageView ()
+
+@property (nonatomic, strong) UIView *corverView;
+
+@end
+
+@implementation SRCoverImageView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self addSubview:self.corverView];
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    self.corverView.frame = self.bounds;
+}
+
+- (void)setCorverAlpha:(CGFloat)corverAlpha {
+    self.corverView.backgroundColor = [UIColor colorWithWhite:0 alpha:0.4];
+}
+
+#pragma mark - lazy
+
+- (UIView *)corverView {
+    if (!_corverView) {
+        _corverView = [[UIView alloc] init];
+        _corverView.backgroundColor = [UIColor colorWithWhite:0 alpha:0.4];
+    }
+    return _corverView;
+}
+
+@end
