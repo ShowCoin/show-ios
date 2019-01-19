bc8b078234707175c48162997739d1000f842a2a
chenyh <chenyinghao@show.one>
2019-01-19 19:46:21 +0800

    easy

---
+++ Classes/Live/View/SLEasyTouchButton/SLEasyTouchButton.h
@@ -0,0 +1,18 @@
+//
+//  SLEasyTouchButton.h
+//  ShowLive
+//
+//  Created by gongxin on 2018/5/29.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLEasyTouchButton : UIButton
+
+@end
+
+
+@interface SLTakeButton : SLEasyTouchButton
+
+@end
