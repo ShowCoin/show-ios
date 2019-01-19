d621ab7175659953acfc56576c977666461cb541
Gxshowone <gongxin@show.one>
2019-01-19 19:35:26 +0800

    file

---
+++ Common/exten/NSSting+aes.h
@@ -0,0 +1,17 @@
+//
+//  NSSting+aes.h
+//  ShowLive
+//
+//  Created by gongxin on 2019/1/19.
+//  Copyright © 2019 vning. All rights reserved.
+//
+
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSSting (aes)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Common/exten/NSSting+aes.m
@@ -0,0 +1,13 @@
+//
+//  NSSting+aes.m
+//  ShowLive
+//
+//  Created by gongxin on 2019/1/19.
+//  Copyright © 2019 vning. All rights reserved.
+//
+
+#import "NSSting+aes.h"
+
+@implementation NSSting (aes)
+
+@end
