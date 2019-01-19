ecea14d700385393b91b7c75b589431842d6558b
YagamiChow <zhouhua@show.one>
2019-01-19 19:35:43 +0800

    Method implementation modification

--- Untils/Exten/NSData+JKPCM.m
+++ Untils/Exten/NSData+JKPCM.m
@@ -86,6 +86,7 @@
     char byteRate3 = *ptr++;
     char byteRate4 = *ptr++;
     wavHeader[0x1C] = byteRate1;
+    wavHeader[0x1D] = byteRate2;
     wavHeader[0x1E] = byteRate3;
     wavHeader[0x1F] = byteRate4;
     
