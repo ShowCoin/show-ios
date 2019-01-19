18fd510f0d6ab65651e86c58436e7fd4a8abd272
Gxshowone <gongxin@show.one>
2019-01-19 19:37:30 +0800

    data

--- Common/exten/NSSting+aes.m
+++ Common/exten/NSSting+aes.m
@@ -11,13 +11,128 @@
 @implementation NSSting (aes)
 
 
-/**< 加密方法 */
-- (NSString*)aci_encryptWithAES;
+- (NSString*)aci_encryptWithAES {
+    
+    NSData *data = [self dataUsingEncoding:NSUTF8StringEncoding];
+  
+}
 
-/**< 解密方法 */
-- (NSString*)aci_decryptWithAES;
+- (NSString*)aci_decryptWithAES {
+    
+    NSData *data = [self dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *baseData_GTM = [self decodeBase64Data:data];
+    NSData *baseData = [[NSData alloc]initWithBase64EncodedString:self options:0];
+    
+    NSData *AESData_GTM = [self AES128operation:kCCDecrypt
+                                           data:baseData_GTM
+                                            key:[NSString aesKey]
+                                             iv:[NSString aesIV]];
+    NSData *AESData = [self AES128operation:kCCDecrypt
+                                       data:baseData
+                                        key:[NSString aesKey]
+                                         iv:[NSString aesIV]];
+    
+    NSString *decStr_GTM = [[NSString alloc] initWithData:AESData_GTM encoding:NSUTF8StringEncoding];
+    NSString *decStr = [[NSString alloc] initWithData:AESData encoding:NSUTF8StringEncoding];
+    
+    
+    return decStr;
+}
+
+/**
+ *  AES加解密算法
+ *
+ *  @param operation kCCEncrypt（加密）kCCDecrypt（解密）
+ *  @param data      待操作Data数据
+ *  @param key       key
+ *  @param iv        向量
+ *
+ *  @return
+ */
+- (NSData *)AES128operation:(CCOperation)operation data:(NSData *)data key:(NSString *)key iv:(NSString *)iv {
+    char keyPtr[kCCKeySizeAES128 + 1];
+    bzero(keyPtr, sizeof(keyPtr));
+    [key getCString:keyPtr maxLength:sizeof(keyPtr) encoding:NSUTF8StringEncoding];
+    
+    // IV
+    char ivPtr[kCCBlockSizeAES128 + 1];
+    bzero(ivPtr, sizeof(ivPtr));
+    [iv getCString:ivPtr maxLength:sizeof(ivPtr) encoding:NSUTF8StringEncoding];
+    
+    size_t bufferSize = [data length] + kCCBlockSizeAES128;
+    void *buffer = malloc(bufferSize);
+    size_t numBytesEncrypted = 0;
+    
+    CCCryptorStatus cryptorStatus = CCCrypt(operation, kCCAlgorithmAES128, kCCOptionPKCS7Padding,
+                                            keyPtr, kCCKeySizeAES128,
+                                            ivPtr,
+                                            [data bytes], [data length],
+                                            buffer, bufferSize,
+                                            &numBytesEncrypted);
+    
+    if(cryptorStatus == kCCSuccess) {
+        NSLog(@"Success");
+        return [NSData dataWithBytesNoCopy:buffer length:numBytesEncrypted];
+        
+    } else {
+        NSLog(@"Error");
+    }
+    
+    free(buffer);
+    return nil;
+}
+
+/**< GTMBase64编码 */
+- (NSString*)encodeBase64Data:(NSData *)data {
+    data = [GTMBase64 encodeData:data];
+    NSString *base64String = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    return base64String;
+}
+
+/**< GTMBase64解码 */
+- (NSData*)decodeBase64Data:(NSData *)data {
+    data = [GTMBase64 decodeData:data];
+    return data;
+}
 
 
-//md5
-+ (NSString *) md5:(NSString *) str;
++(NSString*)aesKey
+{
+    NSString * md5key = [NSString getMD5Sting];
+    NSString * key = ([WYSession sharedSession].isLogin==NO)?PSW_AES_KEY:[md5key substringWithRange:NSMakeRange(0, 16)];
+    return key;
+}
+
++(NSString*)aesIV
+{
+    
+    NSString * md5iv = [NSString getMD5Sting];
+    NSString * iv = ([WYSession sharedSession].isLogin==NO)?AES_IV_PARAMETER:[md5iv substringWithRange:NSMakeRange(16, 16)];
+    return iv;
+}
+
+
++ (NSString *) md5:(NSString *) str
+{
+    //要进行UTF8的转码
+    const char* input = [str UTF8String];
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(input, (CC_LONG)strlen(input), result);
+    
+    NSMutableString *digest = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for (NSInteger i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [digest appendFormat:@"%02x", result[i]];
+    }
+    
+    return digest;
+}
+
++(NSString*)getMD5Sting
+{
+    NSString * string = [NSString stringWithFormat:@"%@%@%@",[WYSession sharedSession].phone,AES_SALT,[WYSession sharedSession].token];
+    NSString * md5String = [NSString md5:string];
+    return md5String;
+    
+}
+
 @end
