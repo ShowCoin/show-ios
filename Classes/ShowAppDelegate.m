059a8fcd305d09cf41bb71e48d733f87715158a2
Gxshowone <gongxin@show.one>
2019-01-26 21:10:42 +0800

    exchagne

--- Classes/ShowAppDelegate.m
+++ Classes/ShowAppDelegate.m
@@ -18,6 +18,30 @@
 
 @implementation ShowAppDelegate
 //入口函数
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary 
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    __weak typeof(self) weaks = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [weaks show_registerService:application didFinishLaunchingWithOptions:launchOptions];
+    });
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    [PageMgr setupWithWindow:self.window];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
+    [self.window makeKeyAndVisible];
+    
+    #if defined(DEBUG)
+        [[SLFPSStatus sharedInstance] open];
+    #endif
+    
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+}
+
+
 
 @end
