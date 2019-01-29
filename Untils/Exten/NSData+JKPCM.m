934070ad2cfef5b430bb22c7cc6491bf8902ec47
YagamiChow <zhouhua@show.one>
2019-01-29 16:23:59 +0800

    Partial method implementation modifications

--- Untils/Exten/NSData+JKPCM.m
+++ Untils/Exten/NSData+JKPCM.m
@@ -119,6 +119,7 @@
     [articleId]: 2
     [createTime]: 1545705336000
     [commentCount]: 3
+    // SubChunkSize = NumSamples * NumChannels * BitsPerSample/8. This is the number of bytes in the data.
     *((SInt32 *)(wavHeader + 0x28)) = pcmDataLength;
 
     NSMutableData *wavData = [NSMutableData dataWithBytes:wavHeader length:wavHeaderSize];
