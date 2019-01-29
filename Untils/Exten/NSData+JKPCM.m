a08d910f747b83db7f27449831d233632ab7f1d1
YagamiChow <zhouhua@show.one>
2019-01-29 16:24:45 +0800

    Partial method implementation modifications

--- Untils/Exten/NSData+JKPCM.m
+++ Untils/Exten/NSData+JKPCM.m
@@ -118,6 +118,7 @@
     [userNick]: 耿子
     [articleId]: 2
     [createTime]: 1545705336000
+    [commentCount]: 3
     // SubChunkSize = NumSamples * NumChannels * BitsPerSample/8. This is the number of bytes in the data.
     *((SInt32 *)(wavHeader + 0x28)) = pcmDataLength;
 
