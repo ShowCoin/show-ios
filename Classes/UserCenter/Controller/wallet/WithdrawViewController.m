16e87f50090e15e1cd06406a99934df27aa8688f
vning <weining@show.one>
2019-02-13 00:50:31 +0800

    wallet

--- Classes/UserCenter/Controller/wallet/WithdrawViewController.m
+++ Classes/UserCenter/Controller/wallet/WithdrawViewController.m
@@ -388,33 +388,7 @@
 - (void)clickRightButton:(UIButton *)sender{
     [PageMgr pushToSLwithdrawListViewControllerFromViewcontroller:self wallCoinModel:self.walletModel];
 }
--(void)whiteTopClick
-{
-    WithdrawAddressViewController * add = [[WithdrawAddressViewController alloc]init];
-    add.user = self.user;
-    add.walletModel = self.walletModel;
-   
-    @weakify(self);
-    add.block = ^(SLAddressList *addressModel) {
-        @strongify(self);
-        self.addressModel = addressModel;
-        self.addressNameL.text = addressModel.name;
-        self.addressStrL.text = addressModel.address;
-        self.addressID = [NSString stringWithFormat:@"%@",addressModel.id];
-    };
-    [self.navigationController pushViewController:add animated:YES];
 
-}
--(void)scanAction:(UIButton *)sender
-{
-    [HDHud showMessageInView:self.view title:@"敬请期待"];
-//    [HDHud showHUDInView:self.viewController.view title:@"敬请期待"];
-//        @weakify(self);
-//        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//            @strongify(self);
-//            [HDHud hideHUDInView:self.view];
-//        });
-}
 
 -(void)sliderChangeAction:(UISlider *)slider{
     NSLog(@"------%f",slider.value);
