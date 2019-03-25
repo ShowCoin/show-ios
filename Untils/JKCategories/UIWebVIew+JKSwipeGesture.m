01e6b11b0505140f1ada11a8b275bf43188b9fe6
gongxin <474746241@qq.com>
2019-03-25 11:52:53 +0800

    no message

---
+++ Untils/JKCategories/UIWebVIew+JKSwipeGesture.m
@@ -0,0 +1,48 @@
+//
+//  UIWebVIew+SwipeGesture.m
+//  SNSwipeGestureSample
+//
+//  Created by Noda Shimpei on 2013/05/18.
+//  Copyright (c) 2013年 Noda Shimpei. All rights reserved.
+//
+
+#import "UIWebVIew+JKSwipeGesture.h"
+
+@interface UIWebView () <UIGestureRecognizerDelegate>
+
+@end
+
+@implementation UIWebView (JKSwipeGesture)
+
+//webView的左滑，右滑
+- (void)jk_useSwipeGesture {
+    UISwipeGestureRecognizer *swipeRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(jk_swipeRight:)];
+    [swipeRight setDirection:UISwipeGestureRecognizerDirectionRight];
+    [swipeRight setNumberOfTouchesRequired:2];
+    [swipeRight setDelegate:self];
+    [self addGestureRecognizer:swipeRight];
+    
+    UISwipeGestureRecognizer *swipeLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(jk_swipeLeft:)];
+    [swipeLeft setDirection:UISwipeGestureRecognizerDirectionLeft];
+    [swipeLeft setNumberOfTouchesRequired:2];
+    [swipeLeft setDelegate:self];
+    [self addGestureRecognizer:swipeLeft];
+    
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] init];
+    [pan setMaximumNumberOfTouches:2];
+    [pan setMinimumNumberOfTouches:2];
+    [self addGestureRecognizer:pan];
+    
+    [pan requireGestureRecognizerToFail:swipeLeft];
+    [pan requireGestureRecognizerToFail:swipeRight];
+}
+//右滑事件
+- (void)jk_swipeRight:(UISwipeGestureRecognizer *)recognizer {
+    if([recognizer numberOfTouches] == 2 && [self canGoBack]) [self goBack];
+}
+//左滑事件
+- (void)jk_swipeLeft:(UISwipeGestureRecognizer *)recognizer {
+    if([recognizer numberOfTouches] == 2 && [self canGoForward]) [self goForward];
+}
+
+@end
