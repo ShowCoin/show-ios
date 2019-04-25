55f5ccf589d3431d33cb8a698c7bc28f04c758a7
gongxin <474746241@qq.com>
2019-04-25 13:07:06 +0800

    no message

---
+++ Base.lproj/SLConfigAction.h
@@ -0,0 +1,13 @@
+//
+//  SLConfigAction.h
+//  ShowLive
+//
+//  Created by iori_chou on 2018/4/12.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "ShowAction.h"
+
+@interface SLConfigAction : ShowAction
+
+@end
