ce945493ea27c8e9e949749ec6341169cf003504
gongxin <474746241@qq.com>
2019-05-11 12:43:31 +0800

    no message

---
+++ Untils/JKCategories/SLChatStatisticsAction.h
@@ -0,0 +1,15 @@
+//
+//  SLChatStatisticsAction.h
+//  ShowLive
+//
+//  Created by iori_chou on 2018/4/14.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import "ShowAction.h"
+
+@interface SLChatStatisticsAction : ShowAction
+//参数tid
+@property (nonatomic, copy) NSString *tid;
+
+@end
