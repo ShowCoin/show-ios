e1ff344468111e5ea6b5c8aa0faea2fc87f525fd
JokeSmileZhang <2558412960@qq.com>
2019-06-29 17:02:01 +0800

    interfacestate

---
+++ Untils/Exten/SLHelper/pop/UITableView+NIMScrollToBottom.h
@@ -0,0 +1,15 @@
+//
+//  UITableView+NTESScrollToBottom.h
+//  show
+//
+//  Created by show.
+//  Copyright (c) 2015年 show. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (NIMKit)
+
+- (void)show_scrollToBottom:(BOOL)animation;
+
+@end
