37c48e7e0adb6a2cd2f00906a71abf07547000eb
JokeSmileZhang <2558412960@qq.com>
2019-08-24 16:25:19 +0800

    取消问题

---
+++ Untils/Exten/SLJPushUntils.h
@@ -0,0 +1,17 @@
+//
+//  SLJPushUntils.h
+//  ShowLive
+//
+//  Created by iori_chou on 2018/3/21.
+//  Copyright © 2018年 vning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLJPushUntils : NSObject
+
++(void)loginJPush;
+
++(void)logoutJPush;
+
+@end
