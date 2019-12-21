9419896dd9dc8e48a8190dd7826308bf4ec0f696
chenyh <chenyinghao@show.one>
2019-12-21 19:56:18 +0800

    ignore pro

---
+++ Classes/Login/DeviceChange/Model/DCSafetyIDModel.m
@@ -0,0 +1,20 @@
+
+//
+//  DCSafetyIDModel.m
+//  ShowLive
+//
+//  Created by chenyh on 2019/1/28.
+//  Copyright © 2019 vning. All rights reserved.
+//
+
+#import "DCSafetyIDModel.h"
+
+@implementation DCSafetyIDModel
+
++ (NSArray *)mj_ignoredPropertyNames {
+    return @[@"codeType", @"text", @"detail"];
+}
+
+
+
+@end
