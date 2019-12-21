adcc926d36eb7990e051fd3f1b2e6a130a69ebb3
chenyh <chenyinghao@show.one>
2019-12-21 19:56:47 +0800

    convert type to text

--- Classes/Login/DeviceChange/Model/DCSafetyIDModel.m
+++ Classes/Login/DeviceChange/Model/DCSafetyIDModel.m
@@ -17,7 +17,18 @@
 
 - (NSString *)text {
     if (!_text) {
-        
+        if ([self.type isEqualToString:@"phone"]) {
+            self.codeType = DCCodeTypeSMS;
+            _text = @"短信验证";
+        }
+        if ([self.type isEqualToString:@"email"]) {
+            self.codeType = DCCodeTypeEmail;
+            _text = @"邮箱验证";
+        }
+        if ([self.type isEqualToString:@"googleCode"]) {
+            self.codeType = DCCodeTypeGoogle;
+            _text = @"谷歌验证";
+        }
     }
     return _text;
 }
