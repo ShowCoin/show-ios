52ba6eec3306420e250aeacae8ce527b68134fbc
chenyh <chenyinghao@show.one>
2019-12-21 19:47:29 +0800

    country cell reset

---
+++ Classes/Login/CountryCodeVC/View/SLCountryCell.m
@@ -0,0 +1,26 @@
+//
+//  SLCountryCell.m
+//  ShowLive
+//
+//  Created by chenyh on 2019/11/28.
+//  Copyright © 2019 vning. All rights reserved.
+//
+
+#import "SLCountryCell.h"
+
+@implementation SLCountryCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        self.backgroundColor = kWhiteSeparationColor1;
+        self.contentView.backgroundColor = UIColor.clearColor;
+        self.textLabel.textColor = OTCWhiteColor();
+        self.textLabel.font = [UIFont systemFontOfSize:16.0];
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        self.showBottomLine = YES;
+    }
+    return self;
+}
+
+@end
