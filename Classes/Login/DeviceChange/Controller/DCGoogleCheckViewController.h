a5c20ffaf2113846084ce47ec303eb6248b50de5
chenyh <chenyinghao@show.one>
2019-12-21 19:57:19 +0800

    DCGoogleCheckViewController

---
+++ Classes/Login/DeviceChange/Controller/DCGoogleCheckViewController.h
@@ -0,0 +1,23 @@
+//
+//  DCGoogleCheckViewController.h
+//  ShowLive
+//
+//  Created by chenyh on 2019/1/26.
+//  Copyright © 2019 vning. All rights reserved.
+//
+
+#import "BaseViewController.h"
+#import "DCSafetyIDModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DCGoogleCheckViewController : BaseViewController
+
+@property (nonatomic, strong) DCSafetyIDModel *model;
+@property (nonatomic, strong) NSDictionary *codeParam;
+
+@property (nonatomic, copy) SLSimpleBlock refreshBlock;
+
+@end
+
+NS_ASSUME_NONNULL_END
