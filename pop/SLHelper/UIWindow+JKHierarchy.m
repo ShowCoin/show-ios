84d52e6567c275153a44a5b40f96e91e5b3f7894
vning <weining@show.one>
2019-12-28 15:47:40 +0800

    V_FIX

--- pop/SLHelper/UIWindow+JKHierarchy.m
+++ pop/SLHelper/UIWindow+JKHierarchy.m
@@ -19,13 +19,5 @@
     return topController;
 }
 
-- (UIViewController*)jk_currentViewController;
-{
-    UIViewController *currentViewController = [self jk_topMostController];
-    
-    while ([currentViewController isKindOfClass:[UINavigationController class]] && [(UINavigationController*)currentViewController topViewController])
-        currentViewController = [(UINavigationController*)currentViewController topViewController];
-    
-    return currentViewController;
-}
+
 @end
